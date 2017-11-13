dde1cb1696af7a03eb7eff555734710c22591a0c
yang hee jung <puuurm@naver.com>
2017-11-13 20:06:53 +0900

    Create Milk instance on main

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,5 +8,17 @@
 
 import Foundation
 
-let strawBerryMilk = Milk(taste: .strawBerry, brand: "서울우유", weight: "200ml", price: "1000원", name: "날마다딸기우유", dateOfManufacture: "20171009")
+let strawBerryMilk = Milk(taste: .strawBerry,
+                          brand: "서울우유",
+                          weight: "200ml",
+                          price: "1000원",
+                          name: "날마다딸기우유",
+                          dateOfManufacture: "20171009")
+let bananaMilk = Milk(taste: .banana,
+                      brand: "서울우유",
+                      weight: "200ml",
+                      price: "1000원",
+                      name: "날마다딸기우유",
+                      dateOfManufacture: "20171012")
 strawBerryMilk.printDrinkInfo()
+bananaMilk.printDrinkInfo()
