779cf5a8ed92062b13eac6c7686cb361aa009a1e
yang hee jung <puuurm@naver.com>
2017-11-13 20:20:24 +0900

    Create SoftDrink class

---
+++ VendingMachine/VendingMachine/SoftDrink.swift
@@ -0,0 +1,37 @@
+//
+//  SoftDrink.swift
+//  VendingMachine
+//
+//  Created by yangpc on 2017. 11. 13..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+class SoftDrink: Drink {
+    var taste: Taste
+    enum Taste: String {
+        case coke = "콜라"
+        case cider = "사이다"
+        case fanta = "환타"
+    }
+    
+    init(taste: Taste,
+                  brand: String,
+                  weight: String,
+                  price: String,
+                  name: String,
+                  dateOfManufacture: String) {
+        self.taste = taste
+        super.init(brand: brand,
+                   weight: weight,
+                   price: price,
+                   name: name,
+                   dateOfManufacture: dateOfManufacture)
+    }
+    
+    override func printDrinkInfo() {
+        print("\(taste) - ", terminator: "")
+        super.printDrinkInfo()
+    }
+}
