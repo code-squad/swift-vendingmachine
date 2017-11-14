b5bce1ae8ca024687fe7d8830b68c7e1dd618ad2
yang hee jung <puuurm@naver.com>
2017-11-14 17:14:41 +0900

    Add properties on Coffee

--- VendingMachine/VendingMachine/Coffee.swift
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -8,7 +8,31 @@
 
 import Foundation
 
-class Coffee: Drink {}
+class Coffee: Drink {
+    var isHot: Bool
+    var amountOfCaffeine: String
+    var nameOfCoffeeBeans: String
+    
+    init(productTpye: String,
+         brand: String,
+         weight: String,
+         price: String,
+         name: String,
+         dateOfManufacture: String,
+         isHot: Bool,
+         amountOfCaffeine: String,
+         nameOfCoffeeBeans: String) {
+        self.isHot = isHot
+        self.amountOfCaffeine = amountOfCaffeine
+        self.nameOfCoffeeBeans = nameOfCoffeeBeans
+        super.init(productTpye: productTpye,
+                   brand: brand,
+                   weight: weight,
+                   price: price,
+                   name: name,
+                   dateOfManufacture: dateOfManufacture)
+    }
+}
 
 class Top: Coffee { }
 class Cantata: Coffee { }
