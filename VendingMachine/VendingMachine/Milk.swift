db6e43968531c7d4176af0b03545318a4787bfe1
yang hee jung <puuurm@naver.com>
2017-11-14 16:26:13 +0900

    Add new properties on Milk

--- VendingMachine/VendingMachine/Milk.swift
+++ VendingMachine/VendingMachine/Milk.swift
@@ -9,6 +9,33 @@
 import Foundation
 
 class Milk: Drink {
+    var farmCode: String
+    var ingredients: [String]
+    lazy var isEasyOfDigestion: () -> Bool = {
+        if self.ingredients.contains("lactos") {
+            return false
+        }
+        return true
+    }
+    
+    init(productTpye: String,
+         brand: String,
+         weight: String,
+         price: String,
+         name: String,
+         dateOfManufacture: String,
+         farmCode: String = "Unknown",
+         ingredients: [String] = [String]()) {
+        self.farmCode = farmCode
+        self.ingredients = ingredients
+        super.init(productTpye: productTpye,
+                   brand: brand,
+                   weight: weight,
+                   price: price,
+                   name: name,
+                   dateOfManufacture: dateOfManufacture)
+    }
+        
 }
 
 class StrawBerryMilk: Milk {}
