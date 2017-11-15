1c74edbc6b7222e1326034158d42958e7550b03a
yang hee jung <puuurm@naver.com>
2017-11-15 17:49:57 +0900

    Modify properties and methods

--- VendingMachine/VendingMachine/SoftDrink.swift
+++ VendingMachine/VendingMachine/SoftDrink.swift
@@ -8,7 +8,34 @@
 
 import Foundation
 
-class SoftDrink: Drink { }
+class SoftDrink: Drink {
+    var amountOfSugar: Int
+    init?(productTpye: String,
+          calorie: String,
+          brand: String,
+          weight: String,
+          price: String,
+          name: String,
+          dateOfManufacture: String,
+          amountOfSugar: String) {
+        let g = CharacterSet.init(charactersIn: "g")
+        let gString = amountOfSugar.trimmingCharacters(in: g)
+        guard let amountOfSugarNumber = Int(gString) else { return nil }
+        self.amountOfSugar = amountOfSugarNumber
+        super.init(productTpye: productTpye,
+                   calorie: calorie,
+                   brand: brand,
+                   weight: weight,
+                   price: price,
+                   name: name,
+                   dateOfManufacture: dateOfManufacture)
+
+    }
+    
+    func isSuitableAmountOfSugar() -> Bool {
+        return self.amountOfSugar < 50 ? true : false
+    }
+}
 
 class Coke: SoftDrink { }
 class Cider: SoftDrink { }
