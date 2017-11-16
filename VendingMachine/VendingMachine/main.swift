4245404815913a4d45bf358f76d8ce8964639d81
yang hee jung <puuurm@naver.com>
2017-11-16 17:35:32 +0900

    Separate Banana class

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,39 +8,39 @@
 
 import Foundation
 
-let strawBerryMilk = StrawBerryMilk(productTpye: "딸기우유",
-                                    brand: "서울우유",
-                                    weight: "200ml",
-                                    price: "1000원",
-                                    name: "날마다딸기우유",
-                                    dateOfManufacture: "20171009",
-                                    calorie: "300kcal")
-let bananaMilk = BananaMilk(productTpye: "바나나우유",
-                            brand: "서울우유",
-                            weight: "200ml",
-                            price: "1000원",
-                            name: "날마다딸기우유",
-                            dateOfManufacture: "20171012",
-                            calorie: "350kcal")
-let coke = Coke(productTpye: "콜라",
-                calorie: "200kcal",
-                brand: "팹시",
-                weight: "350ml",
-                price: "2000원",
-                name: "다이어트콜라",
-                dateOfManufacture: "20171005",
-                amountOfSugar: "50g")
-let coffee = Coffee(productTpye: "커피", calorie: "150kcal",
-                    brand: "맥심",
-                    weight: "400ml",
-                    price: "3000원",
-                    name: "TOP아메리카노",
-                    dateOfManufacture: "20171010",
-                    isHot: true,
-                    amountOfCaffeine: "30mg",
-                    nameOfCoffeeBeans: "Colombia")
-print(strawBerryMilk!)
-print(bananaMilk!)
-print(coke!)
-print(coffee!)
+//let strawBerryMilk = StrawBerryMilk(productTpye: "딸기우유",
+//                                    brand: "서울우유",
+//                                    weight: "200ml",
+//                                    price: "1000원",
+//                                    name: "날마다딸기우유",
+//                                    dateOfManufacture: "20171009",
+//                                    calorie: "300kcal")
+//let bananaMilk = BananaMilk(productTpye: "바나나우유",
+//                            brand: "서울우유",
+//                            weight: "200ml",
+//                            price: "1000원",
+//                            name: "날마다딸기우유",
+//                            dateOfManufacture: "20171012",
+//                            calorie: "350kcal")
+//let coke = Coke(productTpye: "콜라",
+//                calorie: "200kcal",
+//                brand: "팹시",
+//                weight: "350ml",
+//                price: "2000원",
+//                name: "다이어트콜라",
+//                dateOfManufacture: "20171005",
+//                amountOfSugar: "50g")
+//let coffee = Coffee(productTpye: "커피", calorie: "150kcal",
+//                    brand: "맥심",
+//                    weight: "400ml",
+//                    price: "3000원",
+//                    name: "TOP아메리카노",
+//                    dateOfManufacture: "20171010",
+//                    isHot: true,
+//                    amountOfCaffeine: "30mg",
+//                    nameOfCoffeeBeans: "Colombia")
+//print(strawBerryMilk!)
+//print(bananaMilk!)
+//print(coke!)
+//print(coffee!)
 
