8eeca6e624a079a8101211fd1202413cee471f2a
yang hee jung <puuurm@naver.com>
2017-11-17 12:06:12 +0900

    Create VendingMachine swift File

---
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -0,0 +1,13 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by yangpc on 2017. 11. 17..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct VendingMachine {
+    
+}
