a35f1b94c72855a08c6d192a9585d3095dfabcbe
yang hee jung <puuurm@naver.com>
2017-11-21 15:36:54 +0900

    Create Manager who has manager mode VendingMachine

---
+++ VendingMachine/VendingMachine/Manager.swift
@@ -0,0 +1,13 @@
+//
+//  Manager.swift
+//  VendingMachine
+//
+//  Created by yangpc on 2017. 11. 21..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Manager {
+    var delegate: ManagerMode?
+}
