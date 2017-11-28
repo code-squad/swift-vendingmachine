e86f4280129acedbe005c15b87284b681691185a
yang hee jung <puuurm@naver.com>
2017-11-28 12:01:26 +0900

    Move out of do catch

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -13,20 +13,24 @@ var inputView = InputView()
 let outputView = Outputview()
 
 while(true) {
-    do {
-        if !vendingMachine.hasMode {
+    if !vendingMachine.hasMode {
+        do {
             let mode = try inputView.readMode()
             try vendingMachine.assignMode(mode: mode)
+        } catch VendingMachine.ModeError.invalidNumber {
+            print(VendingMachine.ModeError.invalidNumber.rawValue)
+        } catch InputView.InputError.invalidFormat {
+            print(InputView.InputError.invalidFormat.rawValue)
         }
-        if let menuContents = vendingMachine.makeMenu() {
-            outputView.printMonitor(menuContents: menuContents)
-        }
+    }
+    if let menuContents = vendingMachine.makeMenu() {
+        outputView.printMonitor(menuContents: menuContents)
+    }
+    do {
         let inputAction = try inputView.read()
         vendingMachine.action(action: inputAction)
     } catch InputView.InputError.invalidFormat {
         print(InputView.InputError.invalidFormat.rawValue)
-    } catch VendingMachine.ModeError.invalidNumber {
-        print(VendingMachine.ModeError.invalidNumber.rawValue)
     }
     if let resultOfBuy = vendingMachine.makeResultOfOrder() {
         outputView.printPurchase(drink: resultOfBuy)
