d4cb00569e0bb62f79aa1b96f76a09028a20621f
ChocOZerO <nayalth@gmail.com>
2017-12-11 23:04:08 +0900

    [Update]메뉴에서 입력하는 동작을 자판기함수에 연결 #12
    업데이트 중에 Beverage의 price 프로퍼티의 getter 오픈

---
+++ VendingMachine/VendingMachine/InputView.swift
@@ -0,0 +1,31 @@
+//
+//  InputView.swift
+//  VendingMachine
+//
+//  Created by TaeHyeonLee on 2017. 12. 11..
+//  Copyright © 2017 ChocOZerO. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+
+    enum Errors: String, Error {
+        case invalidInput = "입력을 확인해주세요."
+        case quit = "자판기 사용을 종료합니다."
+        case notInMenu = "보기에서 고르세요."
+    }
+
+    func readInput() throws -> (Int, Int) {
+        let input = readLine() ?? ""
+        guard input != "q" && input != "quit" else {
+            throw Errors.quit
+        }
+        let inputs = input.split(separator: " ").flatMap { String($0) }.filter { Int($0) != nil }.map { Int($0)! }
+        guard inputs.count == 2 else {
+            throw Errors.invalidInput
+        }
+        return (inputs[0], inputs[1])
+    }
+
+}
