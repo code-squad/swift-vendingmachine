7c07e9f51391e484c067ca97544ea3a79ee786c3
undervine <undervine@naver.com>
2017-12-11 17:19:09 +0900

    Make menus of Milk, SoftDrink and Coffee respectively #4
    Each classes have 'selectedMenu' variables.
    Add initializers.
    Override description of Beverage class, which now prints menu description also.

--- VendingMachine/VendingMachine/SoftDrink.swift
+++ VendingMachine/VendingMachine/SoftDrink.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 class SoftDrink: Beverage {
-    
+    let selectedMenu: Menus
+    enum Menus: String, CustomStringConvertible {
+        case coke = "콜라"
+        case cider = "사이다"
+        case fanta = "환타"
+        var description: String {
+            return self.rawValue
+        }
+    }
+    init(selectedMenu: Menus, _ brand: String, _ volume: Int, _ price: Int, _ productName: String, _ manufacturedDate: Date) {
+        self.selectedMenu = selectedMenu
+        super.init(brand, volume, price, productName, manufacturedDate)
+    }
+
+    override var description: String {
+        return self.selectedMenu.description + "-" + super.description
+    }
 }
