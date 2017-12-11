0fb258be2c3e4bb921d6d0a3a26e2a53bc6b5af8
undervine <undervine@naver.com>
2017-12-11 16:36:15 +0900

    Make sub-classes of Beverage #2

---
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -0,0 +1,13 @@
+//
+//  Coffee.swift
+//  VendingMachine
+//
+//  Created by 심 승민 on 2017. 12. 11..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Beverage {
+    
+}
---
+++ VendingMachine/VendingMachine/Milk.swift
@@ -0,0 +1,13 @@
+//
+//  Milk.swift
+//  VendingMachine
+//
+//  Created by 심 승민 on 2017. 12. 11..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Milk: Beverage {
+    
+}
