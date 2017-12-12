727462b7896884d80fac022c2e749322ae255c63
DongJin Lee <comets195@gmail.com>
2017-12-12 14:05:18 +0900

    Add Soda Pop Class disinherit from Bevarage.

---
+++ VendingMachine/VendingMachine/SodaPop.swift
@@ -0,0 +1,21 @@
+//
+//  SodaPop.swift
+//  VendingMachine
+//
+//  Created by Mrlee on 2017. 12. 12..
+//  Copyright © 2017년 Napster. All rights reserved.
+//
+
+import Foundation
+
+class SodaPop: Beverage {
+    private let kind: String
+    init(kind:String, brand: String, volume: Int, price: Int, name: String, manufacturingDate: Date) {
+        self.kind = kind
+        super.init(brand: brand, volume: volume, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+    
+    override var description: String {
+        return "\(kind) - " + super.description
+    }
+}
