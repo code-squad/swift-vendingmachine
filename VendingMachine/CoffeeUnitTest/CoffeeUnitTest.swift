b2de6f2816b2d6bf38f16023edb80cd1a671801b
DongJin Lee <comets195@gmail.com>
2017-12-13 18:42:48 +0900

    Add Coffee Test, validate..

--- VendingMachine/CoffeeUnitTest/CoffeeUnitTest.swift
+++ VendingMachine/CoffeeUnitTest/CoffeeUnitTest.swift
@@ -7,29 +7,16 @@
 //
 
 import XCTest
+@testable import VendingMachine
 
 class CoffeeUnitTest: XCTestCase {
+    let starBucks = StarBucksCoffee(volume: 500, price: 5600, manufacturingDate: "20171213", caffeine: false)
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testDeCaffeineCoffee() {
+        XCTAssertFalse(starBucks.hasCaffeine())
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testValidDate() {
+        XCTAssertTrue(starBucks.validate(with: Date()))
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
