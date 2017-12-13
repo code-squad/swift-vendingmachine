0ec0d5acfd6aea8b34855b1f97d4c95bbf75214f
DongJin Lee <comets195@gmail.com>
2017-12-13 18:41:33 +0900

    Add Soda Pop test when made coke.

--- VendingMachine/SodaPopTest/SodaPopTest.swift
+++ VendingMachine/SodaPopTest/SodaPopTest.swift
@@ -7,29 +7,16 @@
 //
 
 import XCTest
+@testable import VendingMachine
 
 class SodaPopTest: XCTestCase {
+    let orignalCoke = Coke(brand: "코카콜라(주)", volume: 270, price: 700, name: "코카콜라", manufacturingDate: "20171213", sugarContent: 40, labelColor: "RED", forWhere: false)
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testCokeisLowCoke() {
+        XCTAssertFalse(orignalCoke.isLowCoke())
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testIsForBusiness() {
+        XCTAssertFalse(orignalCoke.isBusinessUse())
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
