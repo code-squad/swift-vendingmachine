b501d8cca945b004ed41ee3b04994a7610574757
ChocOZerO <nayalth@gmail.com>
2017-12-14 15:58:53 +0900

    [Refactoring]관리자와 구매하는 사용자 기능 분리 #19
    VendingMachine을 기본 상태만 남기고 각 기능을 관리자와 구매자로 구분
    VendingMachine을 class로 변경해서 참조타입으로 변경하여 사용
    user와 admin struct를 만들어 각각 다른 동작을 할 수 있도록 변경

--- VendingMachine/VendingMachineTests/VendingMachineTest.swift
+++ VendingMachine/VendingMachineTests/VendingMachineTest.swift
@@ -20,115 +20,21 @@ class VendingMachineTest: XCTestCase {
         super.tearDown()
     }
 
-    //    자판기 금액을 원하는 금액만큼 올리는 메소드
-    func testInsertCoins() {
+    func testVeningMachine_basic() {
         let vendingMachine: VendingMachine = VendingMachine.init()
-        vendingMachine.insertCoins(1000)
-        XCTAssertEqual(vendingMachine.getBalance(), 1000)
+        XCTAssertEqual(vendingMachine.description, "coins: 0원, inventory: [:]")
     }
 
-    //    특정 상품 인스턴스를 넘겨서 재고를 추가하는 메소드
-    func testAddProduct() {
-        let vendingMachine: VendingMachine = VendingMachine.init()
-        let dateOfManufacture: Date = "20171211".getDateFromString()
-        let validate: Date = "20171220".getDateFromString()
-        let strawberryMilk = StrawberryMilk.init(brand: "서울우유", weight: 200, price: 1000, name: "딸기우유", dateOfManufacture: dateOfManufacture, validate: validate)
-        XCTAssertEqual(vendingMachine.getInventory().count, 0)
-        vendingMachine.add(product: strawberryMilk)
-        XCTAssertEqual(vendingMachine.getInventory().count, 1)
-    }
-
-    //    현재 금액으로 구매가능한 음료수 목록을 리턴하는 메소드
-    func testGetBuyableProducts() {
-        let vendingMachine: VendingMachine = VendingMachine.init()
-        vendingMachine.insertCoins(1000)
+    func testVeningMachine_properties() {
         let dateOfManufacture: Date = "20171211".getDateFromString()
         let validate: Date = "20171220".getDateFromString()
         let strawberryMilk = StrawberryMilk.init(brand: "서울우유", weight: 200, price: 1000, name: "딸기우유", dateOfManufacture: dateOfManufacture, validate: validate)
         let pepciCoke = PepciCoke.init(brand: "펩시", weight: 350, price: 2000, name: "다이어트콜라", dateOfManufacture: dateOfManufacture, lowCalorie: true)
-        vendingMachine.add(product: strawberryMilk)
-        vendingMachine.add(product: pepciCoke)
-        XCTAssertEqual(vendingMachine.getBuyableProducts().count, 1)
-    }
-
-    //    음료수를 구매하는 메소드
-    func testBuy() {
-        let vendingMachine: VendingMachine = VendingMachine.init()
-        vendingMachine.insertCoins(1000)
-        let dateOfManufacture: Date = "20171211".getDateFromString()
-        let validate: Date = "20171220".getDateFromString()
-        let strawberryMilk = StrawberryMilk.init(brand: "서울우유", weight: 200, price: 1000, name: "딸기우유", dateOfManufacture: dateOfManufacture, validate: validate)
-        let pepciCoke = PepciCoke.init(brand: "펩시", weight: 350, price: 2000, name: "다이어트콜라", dateOfManufacture: dateOfManufacture, lowCalorie: true)
-        let strawBerryCategory = String(strawberryMilk.description.split(separator: "(")[0])
-        vendingMachine.add(product: strawberryMilk)
-        vendingMachine.add(product: pepciCoke)
-        vendingMachine.buy(category: strawBerryCategory)
-        XCTAssertEqual(vendingMachine.getInventory().count, 1)
-        XCTAssertEqual(vendingMachine.getBuyableProducts().count, 0)
-        XCTAssertEqual(vendingMachine.getBalance(), 0)
-    }
-
-    //    잔액을 확인하는 메소드
-    func testGetBalance() {
-        let vendingMachine: VendingMachine = VendingMachine.init()
-        XCTAssertEqual(vendingMachine.getBalance(), 0)
-        vendingMachine.insertCoins(1000)
-        XCTAssertEqual(vendingMachine.getBalance(), 1000)
-    }
-
-    //    전체 상품 재고를 (사전으로 표현하는) 종류별로 리턴하는 메소드
-    func testGetInventory() {
-        let vendingMachine: VendingMachine = VendingMachine.init()
-        let dateOfManufacture: Date = "20171211".getDateFromString()
-        let validate: Date = "20171220".getDateFromString()
-        let strawberryMilk = StrawberryMilk.init(brand: "서울우유", weight: 200, price: 1000, name: "딸기우유", dateOfManufacture: dateOfManufacture, validate: validate)
-        let pepciCoke = PepciCoke.init(brand: "펩시", weight: 350, price: 2000, name: "다이어트콜라", dateOfManufacture: dateOfManufacture, lowCalorie: true)
-        vendingMachine.add(product: strawberryMilk)
-        vendingMachine.add(product: pepciCoke)
-        XCTAssertEqual(vendingMachine.getInventory().count, 2)
-    }
-
-    //    유통기한이 지난 재고만 리턴하는 메소드
-    func testGetExpiredProducts() {
-        let vendingMachine: VendingMachine = VendingMachine.init()
-        var dateOfManufacture: Date = "20171211".getDateFromString()
-        let validate: Date = "20171220".getDateFromString()
-        let strawberryMilk = StrawberryMilk.init(brand: "서울우유", weight: 200, price: 1000, name: "딸기우유", dateOfManufacture: dateOfManufacture, validate: validate)
-
-        dateOfManufacture = "20171207".getDateFromString()
-        let expiredValidate: Date = "20171209".getDateFromString()
-        let bananaMilk = BananaMilk.init(brand: "서울우유", weight: 200, price: 1000, name: "날마다바나나우유", dateOfManufacture: dateOfManufacture, validate: expiredValidate)
-        vendingMachine.add(product: strawberryMilk)
-        vendingMachine.add(product: bananaMilk)
-        XCTAssertEqual(vendingMachine.getExpiredProducts(date: "20171210".getDateFromString()).count, 1)
-    }
-
-    //    따뜻한 음료만 리턴하는 메소드
-    func testGetHotProducts() {
-        let vendingMachine: VendingMachine = VendingMachine.init()
-        let dateOfManufacture: Date = "20171211".getDateFromString()
-        let topCoffee = TOPCoffee.init(brand: "맥심", weight: 400, price: 3000, name: "TOP아메리카노", dateOfManufacture: dateOfManufacture, hot: true)
-        let kantata = Kantata.init(brand: "Kantata", weight: 400, price: 3000, name: "칸타타", dateOfManufacture: dateOfManufacture, hot: true)
-        let georgia = Georgia.init(brand: "코카콜라", weight: 400, price: 3000, name: "죠지아", dateOfManufacture: dateOfManufacture, hot: false)
-        vendingMachine.add(product: topCoffee)
-        vendingMachine.add(product: kantata)
-        vendingMachine.add(product: georgia)
-        XCTAssertEqual(vendingMachine.getHotProducts().count, 2)
-    }
-
-    //    시작이후 구매 상품 이력을 배열로 리턴하는 메소드
-    func testGetSalesHistory() {
-        let vendingMachine: VendingMachine = VendingMachine.init()
-        let dateOfManufacture: Date = "20171211".getDateFromString()
-        let topCoffee = TOPCoffee.init(brand: "맥심", weight: 400, price: 3000, name: "TOP아메리카노", dateOfManufacture: dateOfManufacture, hot: true)
-        let kantata = Kantata.init(brand: "Kantata", weight: 400, price: 3000, name: "칸타타", dateOfManufacture: dateOfManufacture, hot: true)
-        let georgia = Georgia.init(brand: "코카콜라", weight: 400, price: 3000, name: "죠지아", dateOfManufacture: dateOfManufacture, hot: false)
-        let topCategory: String = String(topCoffee.description.split(separator: "(")[0])
-        vendingMachine.add(product: topCoffee)
-        vendingMachine.add(product: kantata)
-        vendingMachine.buy(category: topCategory)
-        vendingMachine.add(product: georgia)
-        XCTAssertEqual(vendingMachine.getSalesHistory().count, 1)
+        var inventory: Inventory = [:]
+        inventory["딸기우유"] = [strawberryMilk]
+        inventory["펩시콜라"] = [pepciCoke]
+        let vendingMachine: VendingMachine = VendingMachine.init(coins: 1000, inventory: inventory)
+        XCTAssertEqual(vendingMachine.description, "coins: 1000원, inventory: [\"딸기우유\": [딸기우유(StrawberryMilk) - 서울우유, 200ml, 1000원, 딸기우유, 20171211], \"펩시콜라\": [펩시콜라(PepciCoke) - 펩시, 350ml, 2000원, 다이어트콜라, 20171211]]")
     }
 
 }
