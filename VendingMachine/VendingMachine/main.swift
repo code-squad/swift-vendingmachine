478db0369db12e94eb39c7841dd07fcb2f7d79fc
ChocOZerO <nayalth@gmail.com>
2017-12-14 16:08:20 +0900

    [Update]Main에서 기존과 같이 동작하도록 변경 #20

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -21,16 +21,18 @@ let topCoffee = TOPCoffee.init(brand: "맥심", weight: 400, price: 3000, name: 
 let kantata = Kantata.init(brand: "Kantata", weight: 400, price: 3000, name: "칸타타", dateOfManufacture: dateOfManufacture, hot: true)
 let georgia = Georgia.init(brand: "코카콜라", weight: 400, price: 3000, name: "죠지아", dateOfManufacture: dateOfManufacture, hot: false)
 
-var vendingMachine = VendingMachine()
-vendingMachine.add(product: strawberryMilk)
-vendingMachine.add(product: chocolateMilk)
-vendingMachine.add(product: bananaMilk)
-vendingMachine.add(product: pepciCoke)
-vendingMachine.add(product: sprite)
-vendingMachine.add(product: fanta)
-vendingMachine.add(product: topCoffee)
-vendingMachine.add(product: kantata)
-vendingMachine.add(product: georgia)
+let vendingMachine: VendingMachine = VendingMachine.init()
+var user: VendingMachineUser = VendingMachineUser.init(vendingMachine: vendingMachine)
+var admin: VendingMachineAdmin = VendingMachineAdmin.init(vendingMachine: vendingMachine)
+admin.add(product: strawberryMilk)
+admin.add(product: chocolateMilk)
+admin.add(product: bananaMilk)
+admin.add(product: pepciCoke)
+admin.add(product: sprite)
+admin.add(product: fanta)
+admin.add(product: topCoffee)
+admin.add(product: kantata)
+admin.add(product: georgia)
 
 let outputView = OutputView()
 let inputView = InputView()
@@ -38,13 +40,13 @@ let validationChecker = ValidationChecker()
 var selector: (ValidationChecker.VendingMenu, Int) = (ValidationChecker.VendingMenu.none, 0)
 while selector.0 == ValidationChecker.VendingMenu.none {
     print("\n======= 자판기 =======")
-    let inventory: Inventory = vendingMachine.getInventory()
-    let buyableProducts: Array<Category> = vendingMachine.getBuyableProducts()
+    let inventory: Inventory = user.getInventory()
+    let buyableProducts: Array<Category> = user.getBuyableProducts()
     if buyableProducts.count == 0 {
         print("현재 금액으로 살 수 있는 음료가 없습니다.")
         outputView.show(inventory: inventory)
     } else {
-        outputView.show(balance: vendingMachine.getBalance())
+        outputView.show(balance: user.getBalance())
         outputView.show(inventory: inventory, buyableProducts: buyableProducts)
     }
     outputView.showMenu()
@@ -59,13 +61,13 @@ while selector.0 == ValidationChecker.VendingMenu.none {
     }
     switch selector.0 {
     case .insertCoins:
-        vendingMachine.insertCoins(selector.1)
+        user.insertCoins(selector.1)
     case .buyProduct where selector.1 <= buyableProducts.count:
-        vendingMachine.buy(category: buyableProducts[selector.1-1])
+        user.buy(category: buyableProducts[selector.1-1])
         print("\(buyableProducts[selector.1-1])를 구매하셨습니다. \(String(describing: inventory[buyableProducts[selector.1-1]]![0].price))원을 차감합니다.")
     case .salesHistory:
         print("현재까지 구매 내역입니다.")
-        outputView.show(products: vendingMachine.getSalesHistory())
+        outputView.show(products: user.purchaseList)
     default:
         continue
     }
