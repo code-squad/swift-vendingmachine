8548b418cbda269c69afad77690c2982ef95a439
undervine <undervine@naver.com>
2017-12-16 20:17:14 +0900

    Add protocol 'EnumCollection' and it's extension to get an array of cases in Enum type #12

--- VendingMachine/VendingMachine/Utilities.swift
+++ VendingMachine/VendingMachine/Utilities.swift
@@ -22,20 +22,34 @@ extension Dictionary where Value == Int {
     }
 }
 
-// 클래스 iterator
-class ClassIteratorOf<Type>: IteratorProtocol {
-    typealias Element = Type
-    private let elements: [Element]
-    private var nextIndex: Int
-    init(_ elements: [Element]) {
-        self.nextIndex = 0
-        self.elements = elements
+// EnumCollection을 채택한 타입은 Hashable도 구현해야 함. Enum은 이미 Hashable이므로 hashValue를 따로 구현해줄 필요 없음.
+protocol EnumCollection: Hashable {
+    // sequence를 array로 타입캐스팅하여 반환.
+    static var allValues: [Self] { get }
+}
+
+// enum 내부 값을 배열로 가져오는 확장 기능.
+// [출처](https://theswiftdev.com/2017/10/12/swift-enum-all-values/)
+extension EnumCollection {
+    // 시퀀스를 배열로 캐스팅 후 반환.
+    static var allValues: [Self] {
+        return Array(self.cases())
     }
 
-    func next() -> Element? {
-        defer { self.nextIndex += 1 }
-        guard self.nextIndex < self.elements.count else { return nil }
-        return self.elements[self.nextIndex]
+    // 내부 값의 시퀀스 반환.
+    private static func cases() -> AnySequence<Self> {
+        // 전체 요소를 시퀀스로 만들어 반환.
+        return AnySequence { () -> AnyIterator<Self> in
+            var hash = 0
+            return AnyIterator {
+                defer { hash += 1 }
+                let currentElement = withUnsafePointer(to: &hash) {
+                    $0.withMemoryRebound(to: Self.self, capacity: 1) { $0.pointee }
+                }
+                guard currentElement.hashValue == hash else { return nil }
+                return currentElement
+            }
+        }
     }
 
 }
