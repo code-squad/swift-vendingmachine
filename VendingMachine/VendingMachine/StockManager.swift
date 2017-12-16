51f2ec8a4dc3383ea015b4b486ad5b90544c407e
undervine <undervine@naver.com>
2017-12-16 20:15:18 +0900

    Add functions to support VendingMachine class #12

--- VendingMachine/VendingMachine/StockManager.swift
+++ VendingMachine/VendingMachine/StockManager.swift
@@ -9,25 +9,82 @@
 import Foundation
 
 class StockManager {
-    // 재고 리스트 반환.
-    private var inventoryHistory: [String:Stock]
-    init() {
-        self.inventoryHistory = [:]
+    private weak var vendingMachine: VendingMachine!
+    private var stock: [VendingMachine.Menu.RawValue:Stock]
+    private var purchasedHistory: [HistoryInfo]
+    init(_ vendingMachine: VendingMachine) {
+        self.vendingMachine = vendingMachine
+        self.stock = [:]
+        self.purchasedHistory = []
     }
 
-    func record(for beverage: VendingMachine.Menu) {
-        self.inventoryHistory.update(forKey: beverage.description)
+    // 인벤토리 상태에 따라 장부 업데이트.
+    func updateStock(_ oldStock: Set<Beverage>) {
+        // 인벤토리 상태의 차이를 장부에 기록.
+        record(for: oldStock.symmetricDifference(self.vendingMachine))
     }
 
+    // 변경된 부분을 장부에 기록.
+    private func record(for changes: Set<Beverage>) {
+        for change in changes {
+            self.stock.update(forKey: change.description)
+        }
+    }
+
+    // 품절 여부 반환.
     func isSoldOut(_ beverageType: String) -> Bool {
-        guard let stock = self.inventoryHistory[beverageType] else { return true }
+        guard let stock = self.stock[beverageType] else { return true }
         return stock < 1
+    }
+
+    func showStockList() -> [VendingMachine.Menu.RawValue:Stock] {
+        return self.stock
     }
 
     // 품절이 아닌 상품 리스트 반환.
     func showSellingList() -> [VendingMachine.Menu] {
-        return inventoryHistory.filter { return !isSoldOut($0.key) }.flatMap {
+        return stock.filter { return !isSoldOut($0.key) }.flatMap {
             return VendingMachine.Menu(rawValue: $0.key)
+        }
+    }
+
+    // 유통기한이 지난 재고 리스트 반환.
+    func showExpiredList() -> [VendingMachine.Menu.RawValue:Stock] {
+        var expiredList: [VendingMachine.Menu.RawValue:Stock] = [:]
+        let checkingDay = Date(timeIntervalSinceNow: 0)
+        // 현재 자판기 내 음료수 중
+        for beverage in self.vendingMachine {
+            // 유통기한이 현재 날짜 기준으로 지난 경우,
+            guard beverage.isExpired(with: checkingDay) else { break }
+            // 리스트에 해당 음료수의 이름과 개수 기록.
+            expiredList.update(forKey: beverage.description)
+        }
+        return expiredList
+    }
+
+    func showPurchasedHistory() -> [HistoryInfo] {
+        return self.purchasedHistory
+    }
+
+    func recordHistory(_ oldStock: Set<Beverage>) {
+        // 음료수를 빼먹은 경우.
+        if isPurchased(oldStock) {
+            recordHistoryOfEach(oldStock)
+        }
+    }
+
+    private func isPurchased(_ oldStock: Set<Beverage>) -> Bool {
+        // 이전 상태가 현재 상태를 포함하는 관계이면 음료수를 빼먹은 상황으로 판단. - self.vendingMachine을 Set 타입으로 직접 쓸 수 없어서 이렇게 사용함.
+        return oldStock.isSuperset(of: self.vendingMachine)
+    }
+
+    private func recordHistoryOfEach(_ oldStock: Set<Beverage>) {
+        // 빼먹은 음료수들을 돌면서
+        for beverage in oldStock.symmetricDifference(vendingMachine) {
+            // 빼먹은 음료수의 히스토리를 생성.
+            let purchasedInfo = HistoryInfo(purchasingDate: Date(timeIntervalSinceNow: 0), purchasedMenu: beverage.description, count: 1)
+            // 기록.
+            self.purchasedHistory.append(purchasedInfo)
         }
     }
 
