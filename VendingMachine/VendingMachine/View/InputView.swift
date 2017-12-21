163421d13f066454bb42bbdc8a021b9d0124643f
undervine <undervine@naver.com>
2017-12-21 11:56:41 +0900

    Add user menu to choose AdminMode or UserMode #21
    Add user menu to choose mode, and implement prompt() to return ActivateMode type instance according to the chosen mode.

--- VendingMachine/VendingMachine/View/InputView.swift
+++ VendingMachine/VendingMachine/View/InputView.swift
@@ -9,80 +9,37 @@
 import Foundation
 
 class InputView {
-    private let vendingMachine: VendingMachine
-    init(_ vendingMachine: VendingMachine) {
-        self.vendingMachine = vendingMachine
-    }
-
-    // 잔액이 0원인 경우, 디폴트 메뉴 표시.
-    private func showDefaultMenus() -> String {
-        var screen = "=> "
-        // 자판기 메뉴명, 재고만 표시.
-        for (beverageType, stock) in self.vendingMachine.checkTheStock() {
-            guard let product = VendingMachine.Menu.allValues.filter({ $0 == beverageType }).first else { return "" }
-            screen += "\(product.productName)(\(stock)개) "
-        }
-        screen += "\n"
-        return screen
-    }
-
-    // 잔액이 있는 경우, 모든 메뉴를 순서대로 가격과 함께 표시.
-    private func showMenusInOrder() -> String {
-        var screen = ""
-        for menu in VendingMachine.Menu.allValues {
-            // 현재 메뉴의 재고.
-            guard let stock = self.vendingMachine.checkTheStock().filter({ $0.key == menu })[menu] else { break }
-            // 현재 메뉴의 번호, 이름, 가격, 재고 표시.
-            screen += "\(menu.hashValue+1)) \(menu.productName) \(menu.price)원 (\(stock)개)\n"
-        }
-        return screen
-    }
-
-    // 잔액에 따라 메뉴 포맷 달라짐.
-    private func showMenus() -> String {
-        var screen = ""
-        if self.vendingMachine.showBalance() == 0 {
-            screen += showDefaultMenus()
-        }else {
-            screen += showMenusInOrder()
-        }
-        return screen
-    }
-
-    // 사용자 스크린 표시.
-    private func showScreen() {
-        var screen = "\n현재 투입한 금액이 \(self.vendingMachine.showBalance())원입니다. 다음과 같은 음료가 있습니다.\n "
-        screen += showMenus()
-        for inputMenu in InputMenu.allValues {
-            screen += "\n\(inputMenu.description)"
-        }
-        screen += "\n> "
-        print(screen, terminator: "")
-    }
-
-    // 사용자 메뉴 종류.
-    enum InputMenu: Int, CustomStringConvertible, EnumCollection {
-        case insert = 1
-        case buy
+    // 모드 메뉴 종류.
+    enum Menu: Int, CustomStringConvertible, EnumCollection {
+        case adminMode = 1
+        case userMode
         var description: String {
             var menuName = ""
             switch self {
-            case .insert: menuName = "금액추가"
-            case .buy: menuName = "음료구매"
+            case .adminMode: menuName = "관리자 모드"
+            case .userMode: menuName = "사용자 모드"
             }
-            return String(self.rawValue) + ". " + menuName
+            return String(rawValue) + ". " + menuName
         }
     }
 
-    // 사용자 스크린 표시 및 사용자 입력값(2개) 전처리 및 반환.
-    func prompt() -> (InputMenu, Int)? {
-        showScreen()
-        guard let userInput = readLine(), userInput != "q" || userInput != "quit" else { return nil }
-        let userInputs = userInput.split(separator: " ")
-        guard userInputs.count >= 2 else { return nil }
-        guard let userInputArgument1 = Int(userInputs[0]), let userInputArgument2 = Int(userInputs[1]) else { return nil }
-        guard let selectedMenu = InputMenu(rawValue: userInputArgument1) else { return nil }
-        return (selectedMenu, userInputArgument2)
+    private static func showScreen() -> String {
+        var screen = "자판기를 시작합니다.\n"
+        for menu in Menu.allValues {
+            screen += menu.description + "\n"
+        }
+        screen += "> "
+        return screen
     }
 
+    static func prompt<MachineType: Managable&UserServable>(_ machine: MachineType) -> ActivateMode? {
+        print(showScreen(), terminator: "")
+        guard let userInput = readLine(), let userMenuNumber = Int(userInput) else { return nil }
+        guard let inputMode = Menu(rawValue: userMenuNumber) else { return nil }
+        switch inputMode {
+        case .adminMode: return AdminMode(machine)
+        case .userMode: return UserMode(machine)
+        }
+    }
+    
 }
