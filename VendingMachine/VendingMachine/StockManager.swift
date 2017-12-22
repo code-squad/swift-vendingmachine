30933b10e6e1421274f74c1788cf065a16f23c92
undervine <undervine@naver.com>
2017-12-22 18:54:09 +0900

    Add 'Product' protocol to use classes like Beverage generally

--- VendingMachine/VendingMachine/StockManager.swift
+++ VendingMachine/VendingMachine/StockManager.swift
@@ -8,49 +8,49 @@
 
 import Foundation
 
-class StockManager {
-    private weak var vendingMachine: VendingMachine!
+class StockManager<MachineType: Machine, ProductType: Product> {
+    private var machine: MachineType
     // 자판기 메뉴별 남은 재고 기록.
-    private var stock: [VendingMachine.Menu:Stock]
+    private var stock: [ProductType.MenuType:Stock]
     // 구입이력 기록.
     private var purchasedHistory: [HistoryInfo]
-    init(_ vendingMachine: VendingMachine) {
-        self.vendingMachine = vendingMachine
+    init(_ machine: MachineType) {
+        self.machine = machine
         self.stock = [:]
         self.purchasedHistory = []
     }
 
     // 인벤토리 상태에 따라 장부 업데이트.
-    func updateStock(_ recentChanged: Beverage, isNewArrival: Bool) {
+    func updateStock(_ recentChanged: ProductType, isNewArrival: Bool) {
         stock = stock.update(forKey: recentChanged.menuType, isNewArrival)
     }
 
     // 품절 여부 반환.
-    func isSoldOut(_ beverageType: VendingMachine.Menu) -> Bool {
+    func isSoldOut(_ beverageType: ProductType.MenuType) -> Bool {
         guard let stock = stock[beverageType] else { return true }
         return stock < 1
     }
 
     // 재고 기록 반환.
-    func showStockList() -> [VendingMachine.Menu:Stock] {
+    func showStockList() -> [ProductType.MenuType:Stock] {
         return stock
     }
 
     // 품절이 아닌 메뉴 리스트 반환.
-    func showSellingList() -> [VendingMachine.Menu] {
+    func showSellingList() -> [ProductType.MenuType] {
         let notSoldOutList = stock.filter { !isSoldOut($0.key) }
         return notSoldOutList.flatMap { $0.key }
     }
 
     // 유통기한이 지난 재고 리스트 반환.
-    func showExpiredList(on checkingDay: Date) -> [VendingMachine.Menu:Stock] {
-        var expiredList: [VendingMachine.Menu:Stock] = [:]
+    func showExpiredList(on checkingDay: Date) -> [ProductType.MenuType:Stock] {
+        var expiredList: [ProductType.MenuType:Stock] = [:]
         // 현재 자판기 내 음료수 중
-        for beverage in vendingMachine {
+        for product in machine as! [ProductType] {
             // 유통기한이 현재 날짜 기준으로 지난 경우,
-            guard beverage.isExpired(with: checkingDay) else { continue }
+            guard product.isExpired(with: checkingDay) else { continue }
             // 리스트에 해당 음료수의 이름과 개수 기록.
-            expiredList = expiredList.update(forKey: beverage.menuType, true)
+            expiredList = expiredList.update(forKey: product.menuType, true)
         }
         return expiredList
     }
@@ -61,7 +61,7 @@ class StockManager {
     }
 
     // 구입 이력 기록.
-    func recordPurchasedHistory(_ recentChanged: Beverage, isPurchased: Bool) {
+    func recordPurchasedHistory(_ recentChanged: ProductType, isPurchased: Bool) {
         // 음료수를 빼먹은 경우, 구입 이력 생성 및 기록.
         if isPurchased {
             // 현재 구입된 음료수의 구입이력 생성.
