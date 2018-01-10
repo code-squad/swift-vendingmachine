c48763737829d343c21151ef90faa9f246f4bca2
dely2p <lhyal2p@gmail.com>
2018-01-11 01:55:57 +0900

    add menu property of enum type at each class #1
    Milk, Soda, Coffee Class 각각에 Menu라는 enum type의 menu 속성을 추가했다. enum에는 요구사항에서 예시로 주어진 각 음료들의 이름을 넣었다. 또 생성자로 Menu type을 생성자로 받도록 수정했다.

--- VendingMachine/VendingMachine/Soda.swift
+++ VendingMachine/VendingMachine/Soda.swift
@@ -9,13 +9,28 @@
 import Foundation
 
 class Soda: Beverage {
+    enum Menu: Int, CustomStringConvertible {
+        case Coke, Sprite, Fanta
+        var description: String {
+            switch self {
+            case .Coke:
+                return "콜라"
+            case .Sprite:
+                return "사이다"
+            case .Fanta:
+                return "환타"
+            }
+        }
+    }
+    var menu: Menu
     var brand: String
     var weight: Int
     var price: Int
     var name: String
     var manufactureDate: Date
     
-    init(brand: String, weight: Int, price: Int, name: String, manufactureDate: Date) {
+    init(menu: Menu, brand: String, weight: Int, price: Int, name: String, manufactureDate: Date) {
+        self.menu = menu
         self.brand = brand
         self.weight = weight
         self.price = price
