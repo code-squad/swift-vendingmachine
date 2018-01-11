8e017387216d4cd3c491cc4f324de00f578c3d6f
dely2p <lhyal2p@gmail.com>
2018-01-11 19:59:48 +0900

    create each subClass of Milk, Soda, Coffee Class #6
    음료수 클래스 계층을 3계층으로 만들기 위해 각각의 SubClass를 만들었다.

---
+++ VendingMachine/VendingMachine/ChocoMilk.swift
@@ -0,0 +1,13 @@
+//
+//  ChocoMilk.swift
+//  VendingMachine
+//
+//  Created by Eunjin Kim on 2018. 1. 11..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class ChocoMilk: Milk {
+    
+}
---
+++ VendingMachine/VendingMachine/Coke.swift
@@ -0,0 +1,13 @@
+//
+//  Coke.swift
+//  VendingMachine
+//
+//  Created by Eunjin Kim on 2018. 1. 11..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coke: Soda {
+    
+}
