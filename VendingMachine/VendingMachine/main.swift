22188549b2c476d0e243a230450c27a42aa1d8ac
dely2p <lhyal2p@gmail.com>
2018-01-11 12:14:50 +0900

    create a drink object instance by type in the main and output as a print() function #3
    Date format을 yyyyMMdd형태로 정하여 음료수 객체를 만들고 Beverage의 CustomStringConvertible protocol의 description을 override하여 출력문을 만들었다.

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+func run() {
+    //    딸기우유 - 서울우유, 200ml, 1000원, 날마다딸기우유, 20171009
+    //    바나나우유 - 서울우유, 200ml, 1000원, 날마다딸기우유, 20171012
+    //    콜라 - 팹시, 350ml, 2000원, 다이어트콜라, 20171005
+    //    커피 - 맥심, 400ml, 3000원, TOP아메리카노, 20171010
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyyMMdd"
+    
+    if let date = formatter.date(from: "20161009") {
+        let strawberryMilk = Milk(menu: .Strawberry, brand: "서울우유", weight: 200, price: 1000, name: "날마다딸기우유", manufactureDate: date)
+        print(strawberryMilk.description)
+    }
+    if let date = formatter.date(from: "20171012") {
+        let bananaMilk = Milk(menu: .Banana, brand: "서울우유", weight: 200, price: 1000, name: "날마다딸기우유", manufactureDate: date)
+        print(bananaMilk.description)
+    }
+//    if let date = formatter.date(from: "20161008") {
+//        let menu3 = Soda(menu: , brand: "서울우유", weight: 200, price: 1000, name: "날마다딸기우유", manufactureDate: date)
+//    }
+//    if let date = formatter.date(from: "20161008") {
+//        let menu4 = Coffee(menu: , brand: "서울우유", weight: 200, price: 1000, name: "날마다딸기우유", manufactureDate: date)
+//    }
+    
+}
+run()
