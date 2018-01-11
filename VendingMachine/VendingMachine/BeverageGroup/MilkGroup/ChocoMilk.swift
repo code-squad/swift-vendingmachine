2eb9198ef353c2b6655e34a461a28b8479156bac
dely2p <lhyal2p@gmail.com>
2018-01-11 20:52:21 +0900

    add property in Milk Class and each SubClasses #6
    Milk Class에 유통기한 속성과 초기화 구문을 넣었고, Strawberry에 딸기시럽함유량을, Choco에 초코파우더 함유량을, banana에 바나나시럽 함유량을 초기화 한 속성을 넣었다.

--- VendingMachine/VendingMachine/BeverageGroup/MilkGroup/ChocoMilk.swift
+++ VendingMachine/VendingMachine/BeverageGroup/MilkGroup/ChocoMilk.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class ChocoMilk: Milk {
     var kindOfMilk: String = "초코우유"
+    var cocoapowder:Double = 1.1
     override init(brand: String, weight: Int, price: Int, name: String, manufactureDate: Date) {
         super.init(brand: brand, weight: weight, price: price, name: name, manufactureDate: manufactureDate)
     }
