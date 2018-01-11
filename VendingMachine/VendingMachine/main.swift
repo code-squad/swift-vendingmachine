4b02d59f3955e912a7c0e07cb523e7957b503d44
YUAMING <miennes3@gmail.com>
2018-01-11 17:19:43 +0900

    Add main function

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,11 +8,10 @@
 
 import Foundation
 
-let today = DateUtility.today()
-let cocaCola = CocaCola(brand: "코카콜라", volume: 350, price: 2000, productName: "다이어트콜라", expiryDate: today, calorie: 100, carbonicAcid: "탄산낮음")
-let pepci = Pepci(brand: "펩시", volume: 350, price: 2000, productName: "펩시", expiryDate: today, calorie: 250, carbonicAcid: "탄산높음")
+func main() {
+    let printingData = InputView.makeIntances()
+    OutputView(data: printingData).printResult()
+}
 
-let printingData: [OutputPrintable] = [cocaCola, pepci]
-
-OutputView(data: printingData).printResult()
+main()
 
