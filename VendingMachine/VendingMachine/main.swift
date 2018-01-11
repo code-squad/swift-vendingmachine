95382dbf801c8062226171d4cdf0a23b51eea005
YUAMING <miennes3@gmail.com>
2018-01-11 16:05:31 +0900

    Modify main.swift

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,11 +9,10 @@
 import Foundation
 
 let today = DateUtility.today()
-let strawberryMilk = Milk(.strawberry, "서울우유", 200, 1000, "날마다딸기우유", today)
-let bananaMilk = Milk(.banana, "서울우유", 200, 1000, "날마다딸기우유", today)
-let georgia = Coffee(.georgia, "맥심", 400, 3000, "TOP아메리카노", today)
-let cola = CarbonatedDrink(.cola, "펩시", 350, 2000, "다이어트콜라", today)
+let cocaCola = CocaCola(brand: "코카콜라", volume: 350, price: 2000, productName: "다이어트콜라", expiryDate: today, calorie: 100, carbonicAcid: "탄산낮음")
+let pepci = Pepci(brand: "펩시", volume: 350, price: 2000, productName: "펩시", expiryDate: today, calorie: 250, carbonicAcid: "탄산높음")
 
-let printingData: [OutputPrintable] = [strawberryMilk, bananaMilk, georgia, cola]
+let printingData: [OutputPrintable] = [cocaCola, pepci]
 
 OutputView(data: printingData).printResult()
+
