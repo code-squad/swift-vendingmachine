4c3a5cffbd5ddf9ef396ea948857c397d888fc47
YUAMING <miennes3@gmail.com>
2018-01-11 12:23:48 +0900

    Add ResultPrintable protocol and OutputView struct

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -14,7 +14,6 @@ let bananaMilk = Milk(.banana, "서울우유", 200, 1000, "날마다딸기우유
 let georgia = Coffee(.georgia, "맥심", 400, 3000, "TOP아메리카노", today)
 let cola = CarbonatedDrink(.cola, "펩시", 350, 2000, "다이어트콜라", today)
 
-print(strawberryMilk.description)
-print(bananaMilk.description)
-print(georgia.description)
-print(cola.description)
+let printData: [ResultPrintable] = [strawberryMilk, bananaMilk, georgia, cola]
+
+OutputView(data: printData).printResult()
