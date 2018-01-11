d6ef42807bf802d89e7aad111d662c451a8da40e
dely2p <lhyal2p@gmail.com>
2018-01-11 20:12:17 +0900

    create UnitTestVendingMachine #9
    인터페이스 테스트코드 작성을 위해 UnitTest를 만들었다.

---
+++ VendingMachine/UnitTestVendingMachine/UnitTestVendingMachine.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestVendingMachine.swift
+//  UnitTestVendingMachine
+//
+//  Created by Eunjin Kim on 2018. 1. 11..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestVendingMachine: XCTestCase {
+    
+    func testExample() {
+        
+    }
+    
+}
