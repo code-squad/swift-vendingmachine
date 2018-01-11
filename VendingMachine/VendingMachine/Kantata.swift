9d36f64be287971c1591421d1fe2639dd67fad7c
dely2p <lhyal2p@gmail.com>
2018-01-11 19:59:48 +0900

    create each subClass of Milk, Soda, Coffee Class #6
    음료수 클래스 계층을 3계층으로 만들기 위해 각각의 SubClass를 만들었다.

---
+++ VendingMachine/VendingMachine/Kantata.swift
@@ -0,0 +1,13 @@
+//
+//  Kantata.swift
+//  VendingMachine
+//
+//  Created by Eunjin Kim on 2018. 1. 11..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Kantata: Coffee {
+    
+}
