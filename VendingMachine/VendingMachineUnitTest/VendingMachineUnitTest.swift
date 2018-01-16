c1fac2cfb8500d7d9ef828f8f842ae215e5f8a58
Jack <somedd@naver.com>
2018-01-16 16:08:03 +0900

    Add setUp function and reduce code

--- VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
+++ VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
@@ -9,27 +9,24 @@
 import XCTest
 
 class VendingMachineUnitTest: XCTestCase {
+    var form: DateFormatter!
+    override func setUp() {
+        super.setUp()
+        self.form = DateFormatter()
+        form.dateFormat = "yyyyMMdd"
+    }
     
     func test_우유객체생성여부() {
-        let form = DateFormatter()
-        form.dateFormat = "yyyyMMdd"
-        
         let strawberryMilk = Milk(kind: "딸기우유",brand: "서울우유", name: "날마다 딸기우유", volume: 200, price: 1000, date: form.date(from: "20170101") ?? Date())
         XCTAssertNotNil(strawberryMilk)
     }
     
     func test_탄산음료객체생성여부() {
-        let form = DateFormatter()
-        form.dateFormat = "yyyyMMdd"
-        
         let pepsi = SodaPop(kind: "콜라", brand: "팹시", name: "다이어트콜라", volume: 350, price: 700, date: form.date(from: "20160105") ?? Date())
         XCTAssertNotNil(pepsi)
     }
     
     func test_커피객체생성여부() {
-        let form = DateFormatter()
-        form.dateFormat = "yyyyMMdd"
-        
         let maxim = Coffee(kind: "커피", brand: "맥심", name: "TOP아메리카노", volume: 400, price: 3000, date: form.date(from: "20170110") ?? Date())
         XCTAssertNotNil(maxim)
     }
