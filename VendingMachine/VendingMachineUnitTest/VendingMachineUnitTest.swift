77bc8cabff4423a0a78f10c6d07a6da7bd9be8e2
Jack <somedd@naver.com>
2018-01-16 14:24:18 +0900

    Add UnitTests of Milk, Coffee

--- VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
+++ VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
@@ -10,18 +10,27 @@ import XCTest
 
 class VendingMachineUnitTest: XCTestCase {
     
-    func test_음료객체생성여부() {
+    func test_우유객체생성여부() {
         let form = DateFormatter()
         form.dateFormat = "yyyyMMdd"
         
-        let strawberryMilk = Milk(kind: "딸기우유",brand: "서울우유", name: "날마다 딸기우유", volume: 200, price: 1000, date: form.date(from: "20170101")!)
-        let bananaMilk = Milk(kind: "바나나우유", brand: "서울우유", name: "날마다 바나나우유", volume: 200, price: 1000, date: form.date(from: "20170105")!)
-        let pepsi = SodaPop(kind: "콜라", brand: "팹시", name: "다이어트콜라", volume: 350, price: 700, date: form.date(from: "20160105")!)
-        let maxim = Coffee(kind: "커피", brand: "맥심", name: "TOP아메리카노", volume: 400, price: 3000, date: form.date(from: "20170110")!)
-        
+        let strawberryMilk = Milk(kind: "딸기우유",brand: "서울우유", name: "날마다 딸기우유", volume: 200, price: 1000, date: form.date(from: "20170101") ?? Date())
         XCTAssertNotNil(strawberryMilk)
-        XCTAssertNotNil(bananaMilk)
+    }
+    
+    func test_탄산음료객체생성여부() {
+        let form = DateFormatter()
+        form.dateFormat = "yyyyMMdd"
+        
+        let pepsi = SodaPop(kind: "콜라", brand: "팹시", name: "다이어트콜라", volume: 350, price: 700, date: form.date(from: "20160105") ?? Date())
         XCTAssertNotNil(pepsi)
+    }
+    
+    func test_커피객체생성여부() {
+        let form = DateFormatter()
+        form.dateFormat = "yyyyMMdd"
+        
+        let maxim = Coffee(kind: "커피", brand: "맥심", name: "TOP아메리카노", volume: 400, price: 3000, date: form.date(from: "20170110") ?? Date())
         XCTAssertNotNil(maxim)
     }
     
