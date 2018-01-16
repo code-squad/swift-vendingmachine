541ea22c80ea9fabb7c549e4dc22d8112c7940c2
Jack <somedd@naver.com>
2018-01-16 11:37:13 +0900

    Generate Beverage class

---
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -0,0 +1,23 @@
+//
+//  Coffee.swift
+//  VendingMachine
+//
+//  Created by jack on 2018. 1. 16..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Beverage {
+    private var brand: String
+    private var kind: String
+    override var description: String {
+        return "\(self.kind) - \(self.brand), \(super.description)"
+    }
+    
+    init(kind: String, brand : String, name : String, volume : Int, price : Int, date : Date) {
+        self.brand = brand
+        self.kind = kind
+        super.init(name: name, volume: volume, price: price, date: date)
+    }
+}
--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -2,10 +2,22 @@
 //  main.swift
 //  VendingMachine
 //
-//  Created by JK on 11/10/2017.
+//  Created by Jack (2018/01/15)
 //  Copyright © 2017 JK. All rights reserved.
 //
 
 import Foundation
 
+let form = DateFormatter()
+form.dateFormat = "yyyyMMdd"
+
+let strawberryMilk = Milk(kind: "딸기우유",brand: "서울우유", name: "날마다 딸기우유", volume: 200, price: 1000, date: form.date(from: "20170101")!)
+let bananaMilk = Milk(kind: "바나나우유", brand: "서울우유", name: "날마다 바나나우유", volume: 200, price: 1000, date: form.date(from: "20170105")!)
+let pepsi = SodaPop(kind: "콜라", brand: "팹시", name: "다이어트콜라", volume: 350, price: 700, date: form.date(from: "20160105")!)
+let maxim = Coffee(kind: "커피", brand: "맥심", name: "TOP아메리카노", volume: 400, price: 3000, date: form.date(from: "20170110")!)
+
+print(strawberryMilk)
+print(bananaMilk)
+print(pepsi)
+print(maxim)
 
