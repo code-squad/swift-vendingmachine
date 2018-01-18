5814d1ff57a996b37d492a5db4bad631087e6a8f
YUAMING <miennes3@gmail.com>
2018-01-19 00:02:27 +0900

    Add error handling and rename from countQuantity to countBeverageQuantity

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -8,13 +8,35 @@
 
 import Foundation
 
-class VendingMachine: Userable {    
+class VendingMachine {
     private var money: Money
+    private var inventory: Inventory
     
-    init(money: Money) {
+    init(money: Money, inventory: Inventory) {
         self.money = money
+        self.inventory = inventory
     }
     
+    func insertBeverage(beverageMenu: BeverageMenu, quantity: Int = 1) {
+        inventory.insertBeverage(beverageMenu: beverageMenu, quantity: quantity)
+    }
+    
+    func deductBeverage(beverageMenu: BeverageMenu, quantity: Int = -1) throws {
+        try inventory.deductBeverage(beverageMenu: beverageMenu, quantity: quantity)
+    }
+    
+    func countBeverageQuantity(beverageMenu: BeverageMenu) -> Int {
+        return inventory.countBeverageQuantity(beverageMenu: beverageMenu)
+    }
+    
+//    func fetchPurchasableBeverages() {
+//        inventory.
+//            $0.beverageMenu.makeInstance().price > money.countChange()
+//        })
+//    }
+}
+
+extension VendingMachine: Userable {
     func insertMoney(coin: Money.Unit) {
         money.insert(coin: coin)
     }
