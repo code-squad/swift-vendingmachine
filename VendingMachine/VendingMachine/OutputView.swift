4c28e25c87baf930745720ccaa3881fc6bdc1a5a
dely2p <lhyal2p@gmail.com>
2018-01-19 02:33:39 +0900

    Improving the relationship between objects #20
    클래스 객체 간의 의존성을 줄이기 위해 main에서 객체를 생성하여 호출하고, OutputView에서 VendingMachine의 값을 가져와서 출력하도록 개선했다.

--- VendingMachine/VendingMachine/OutputView.swift
+++ VendingMachine/VendingMachine/OutputView.swift
@@ -9,21 +9,34 @@
 import Foundation
 
 class OutputView {
-    func printAddAmoutMenu(availableBeverage: [ObjectIdentifier : [Beverage]]) {
+    private var coins: Int
+    private var vendingMachine: VendingMachine
+    private var availableBeverage: [ObjectIdentifier:[Beverage]]
+    init(vendingMachine: VendingMachine) {
+        coins = 0
+        self.vendingMachine = vendingMachine
+        availableBeverage = [ObjectIdentifier:[Beverage]]()
+    }
+    
+    func printAddAmoutMenu() {
+        self.availableBeverage = vendingMachine.listOfDrinksAvailable()
         for (index, menu) in availableBeverage.enumerated() {
             print("\(index+1))\(menu.value[0].kindOf) \(menu.value[0].price)(\(menu.value.count)개)")
         }
     }
     
-    func printPurchaseBeverage(beverageInfo: Beverage?) {
-        print("\(beverageInfo?.kindOf ?? "")를 구매하셨습니다. \(beverageInfo?.price ?? 0)원을 차감합니다.")
+    func printPurchaseBeverage(choiceBeverage: Beverage) {
+        print("\(choiceBeverage.kindOf)를 구매하셨습니다. \(choiceBeverage.price)원을 차감합니다.")
+        self.coins -= choiceBeverage.price
     }
     
-    func printCurrentCoins(coins: Int) {
-        print("\n현재 투입한 금액이 \(coins)원입니다. 다음과 같은 음료가 있습니다.")
+    func printCurrentCoins() {
+        print("\n현재 투입한 금액이 \(self.coins)원입니다. 다음과 같은 음료가 있습니다.")
+        vendingMachine.putCoins(coins: coins)
     }
     
-    func printBeverageMenu(entireInventory: [ObjectIdentifier : [Beverage]]) {
+    func printBeverageMenu() {
+        let entireInventory = vendingMachine.showEntireInventory()
         for menu in entireInventory {
             let numberOfBeverage = menu.value.count
             print("\(menu.value[0].kindOf)(\(numberOfBeverage)개) ", terminator: "")
@@ -36,4 +49,40 @@ class OutputView {
         print("2. 음료구매")
         print("> ", terminator: "")
     }
+    
+    func inputValueClassification(separatedValues: [Int]) {
+        switch separatedValues[0] {
+            case 1:
+                incrementCurrentCoins(coins: separatedValues[1])
+            case 2:
+                menuOfPurchaseBeverage(menuNumber: separatedValues[1])
+            default: break
+        }
+    }
+    
+    func getBeverageKey(menuNumber: Int) throws -> ObjectIdentifier {
+        for (index, beverage) in availableBeverage.enumerated() {
+            if menuNumber == (index + 1) {
+                return beverage.key
+            }
+        }
+        throw InventoryBox.VendingMachinError.invalidBeverage
+    }
+    
+    func menuOfPurchaseBeverage(menuNumber: Int) {
+        do {
+            let choiceBeverageKey = try getBeverageKey(menuNumber: menuNumber)
+            if let choiceBeverage = availableBeverage[choiceBeverageKey]?.first {
+                printPurchaseBeverage(choiceBeverage: choiceBeverage)
+                vendingMachine.buyBeverage(beverageName: choiceBeverageKey)
+            }
+        }catch {
+            print("get Beverage error")
+        }
+    }
+    
+    func incrementCurrentCoins(coins: Int) {
+        self.coins = coins
+    }
+    
 }
