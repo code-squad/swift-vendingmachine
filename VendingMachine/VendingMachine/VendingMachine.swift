5049e3c5d5055e425addae6657c25d976ae106ac
YUAMING <miennes3@gmail.com>
2018-01-18 10:32:30 +0900

    Create VendingMachine class

---
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -0,0 +1,13 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by yuaming on 2018. 1. 18..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class VendingMachine {
+    
+}
