b4ce44d7eb9477cda66f6b1fd084c7bbfe7f7291
YUAMING <miennes3@gmail.com>
2018-01-18 21:23:36 +0900

    Add quantity variable to multiple classes

--- VendingMachine/VendingMachine/Model/BeverageMenu.swift
+++ VendingMachine/VendingMachine/Model/BeverageMenu.swift
@@ -20,17 +20,17 @@ enum BeverageMenu {
         let today = DateUtility.today()
         switch self {
         case .bananaMilk:
-            return BananaMilk(brand: "빙그레", volume: 250, price: 1500, productName: "바나나우유", expiryDate: today, calorie: 300)
+            return BananaMilk(brand: "빙그레", volume: 250, price: 1500, productName: "바나나우유", expiryDate: today, calorie: 300, quantity: 0)
         case .cocaCola:
-            return CocaCola(brand: "코카콜라", volume: 500, price: 1430, productName: "코카콜라", expiryDate: today, calorie: 250)
+            return CocaCola(brand: "코카콜라", volume: 500, price: 1430, productName: "코카콜라", expiryDate: today, calorie: 250, quantity: 0)
         case .georgia:
-            return Georgia(brand: "코카콜라", volume: 240, price: 1100, productName: "조지아 오리지널", expiryDate: today, calorie: 94)
+            return Georgia(brand: "코카콜라", volume: 240, price: 1100, productName: "조지아 오리지널", expiryDate: today, calorie: 94, quantity: 0)
         case .pepci:
-            return Pepci(brand: "펩시콜라", volume: 355, price: 1200, productName: "펩시", expiryDate: today, calorie: 275)
+            return Pepci(brand: "펩시콜라", volume: 355, price: 1200, productName: "펩시", expiryDate: today, calorie: 275, quantity: 0)
         case .strawberryMilk:
-            return StrawberryMilk(brand: "빙그레", volume: 355, price: 1200, productName: "딸기우유", expiryDate: today, calorie: 275)
+            return StrawberryMilk(brand: "빙그레", volume: 355, price: 1200, productName: "딸기우유", expiryDate: today, calorie: 275, quantity: 0)
         case .top:
-            return TOP(brand: "맥심", volume: 275, price: 1800, productName: "TOP 스위트아메리카노", expiryDate: today, calorie: 48)
+            return TOP(brand: "맥심", volume: 275, price: 1800, productName: "TOP 스위트아메리카노", expiryDate: today, calorie: 48, quantity: 0)
         }
     }
 }
