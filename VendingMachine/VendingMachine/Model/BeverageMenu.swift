cc8e45161ec615ab81a7868ed9ba3cb96da0c4a4
YUAMING <miennes3@gmail.com>
2018-01-19 01:14:05 +0900

    Add isHot computed properties

--- VendingMachine/VendingMachine/Model/BeverageMenu.swift
+++ VendingMachine/VendingMachine/Model/BeverageMenu.swift
@@ -28,13 +28,13 @@ enum BeverageMenu {
         case .cocaCola:
             return CocaCola(brand: "코카콜라", volume: 500, price: 1430, productName: "코카콜라", expiryDate: today, calorie: 250)
         case .georgia:
-            return Georgia(brand: "코카콜라", volume: 240, price: 1100, productName: "조지아 오리지널", expiryDate: today, calorie: 94)
+            return Georgia(brand: "코카콜라", volume: 240, price: 1100, productName: "조지아 오리지널", expiryDate: today, calorie: 94, isHot: false)
         case .pepsi:
             return Pepsi(brand: "펩시콜라", volume: 355, price: 1200, productName: "펩시", expiryDate: today, calorie: 275)
         case .strawberryMilk:
             return StrawberryMilk(brand: "빙그레", volume: 355, price: 1200, productName: "딸기우유", expiryDate: today, calorie: 275)
         case .top:
-            return TOP(brand: "맥심", volume: 275, price: 1800, productName: "TOP 스위트아메리카노", expiryDate: today, calorie: 48)
+            return TOP(brand: "맥심", volume: 275, price: 1800, productName: "TOP 스위트아메리카노", expiryDate: today, calorie: 48, isHot: true)
         }
     }
 }
--- VendingMachine/VendingMachine/Model/Beverages/Coffee.swift
+++ VendingMachine/VendingMachine/Model/Beverages/Coffee.swift
@@ -9,11 +9,18 @@
 import Foundation
 
 class Coffee: Beverage {
-    override init(brand: String, volume: Int, price: Int, productName: String, expiryDate: Date, calorie: Int) {
+    private let _isHot: Bool
+    
+    init(brand: String, volume: Int, price: Int, productName: String, expiryDate: Date, calorie: Int, isHot: Bool) {
+        self._isHot = isHot
         super.init(brand: brand, volume: volume, price: price, productName: productName, expiryDate: expiryDate, calorie: calorie)
     }
     
     override var description: String {
         return "조지아커피(\(Georgia.bringTypeName)) - \(super.description)"
+    }
+    
+    var isHot: Bool {
+        return self._isHot
     }
 }
