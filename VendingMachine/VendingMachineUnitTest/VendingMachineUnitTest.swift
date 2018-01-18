e1f407096f14bc9b293c3db998838d2ac82ead50
Jack <somedd@naver.com>
2018-01-18 18:00:47 +0900

    Add UnitTest function of VendingMachine. #6

--- VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
+++ VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
@@ -10,25 +10,35 @@ import XCTest
 
 class VendingMachineUnitTest: XCTestCase {
     var form: DateFormatter!
+    var vendingMachineForTest: VendingMachine!
+    
     override func setUp() {
         super.setUp()
         self.form = DateFormatter()
         form.dateFormat = "yyyyMMdd"
+        let productsBox = [
+            StrawberryMilk("서울우유", "날마다딸기우유", 200, 1000, form.date(from: "20180117") ?? Date()),
+            BananaMilk("서울우유", "날마다바나나우유", 200, 1000, form.date(from: "20170117") ?? Date()),
+            PepciCoke("코카콜라", "다이어트콜라", 350, 2000, form.date(from: "20170110") ?? Date(), lowCalorie: true),
+            Fanta("환타", "환타오렌지", 350, 2000, form.date(from: "20170109") ?? Date(), lowCalorie: false),
+            TOPCoffee("맥심", "TOP아메리카노", 400, 3000, form.date(from: "20170117") ?? Date(), hot: false),
+            Georgia("맥심", "조지아카페모카", 450, 3500, form.date(from: "20170119") ?? Date(), hot: true),
+            ]
+        vendingMachineForTest = VendingMachine(productsBox: productsBox)
     }
     
     func test_딸기우유객체생성여부() {
-        let strawberryMilk = StrawberryMilk(brand: "서울우유", name: "날마다딸기우유", volume: 200, price: 1000, manufacturedDate: form.date(from: "20170117") ?? Date())
+        let strawberryMilk = StrawberryMilk("서울우유", "날마다딸기우유", 200, 1000, form.date(from: "20170117") ?? Date())
         XCTAssertNotNil(strawberryMilk)
-//        XCTAssertFalse(strawberryMilk.validate(with: form.date(from: "20170203") ?? Date()))
     }
     
     func test_바나나우유객체생성여부() {
-        let bananaMilk = BananaMilk(brand: "서울우유", name: "날마다바나나우유", volume: 200, price: 1000, manufacturedDate: form.date(from: "20170116") ?? Date())
+        let bananaMilk = BananaMilk("서울우유", "날마다바나나우유", 200, 1000, form.date(from: "20170116") ?? Date())
         XCTAssertNotNil(bananaMilk)
     }
     
     func test_초코우유객체생성및메서드() {
-        let chocolateMilk = ChocolateMilk(brand: "서울우유", name: "날마다초코우유", volume: 200, price: 1000, manufacturedDate: form.date(from: "20170115") ?? Date())
+        let chocolateMilk = ChocolateMilk("서울우유", "날마다초코우유", 200, 1000, form.date(from: "20170115") ?? Date())
         XCTAssertNotNil(chocolateMilk)
     }
     
@@ -39,17 +49,17 @@ class VendingMachineUnitTest: XCTestCase {
     }
 
     func test_팹시콜라객체생성여부() {
-        let pepsi = PepciCoke(brand: "팹시", name: "다이어트콜라", volume: 350, price: 2000, manufacturedDate: form.date(from: "20170201") ?? Date(), lowCalorie: true)
+        let pepsi = PepciCoke("팹시", "다이어트콜라", 350, 2000, form.date(from: "20170201") ?? Date(), lowCalorie: true)
         XCTAssertNotNil(pepsi)
     }
     
     func test_스프라이트객체생성여부() {
-        let sprite = Sprite(brand: "코카콜라", name: "사이다", volume: 350, price: 2000, manufacturedDate: form.date(from: "20170202") ?? Date(), lowCalorie: false)
+        let sprite = Sprite("코카콜라", "사이다", 350, 2000, form.date(from: "20170202") ?? Date(), lowCalorie: false)
         XCTAssertNotNil(sprite)
     }
     
     func test_환타객체생성여부() {
-        let fanta = Fanta(brand: "환타", name: "환타오렌지", volume: 350, price: 2000, manufacturedDate: form.date(from: "20170203") ?? Date(), lowCalorie: true)
+        let fanta = Fanta("환타", "환타오렌지", 350, 2000, form.date(from: "20170203") ?? Date(), lowCalorie: true)
         XCTAssertNotNil(fanta)
     }
     
@@ -59,17 +69,17 @@ class VendingMachineUnitTest: XCTestCase {
     }
     
     func test_TOP커피객체생성여부() {
-        let topCoffee = TOPCoffee(brand: "맥심", name: "TOP아메리카노", volume: 400, price: 3000, manufacturedDate: form.date(from: "20170110") ?? Date(), hot : false)
+        let topCoffee = TOPCoffee("맥심", "TOP아메리카노", 400, 3000, form.date(from: "20170110") ?? Date(), hot : false)
         XCTAssertNotNil(topCoffee)
     }
     
     func test_칸타타객체생성여부() {
-        let cantataCoffee = Cantata(brand: "맥심", name: "칸타타카페모카", volume: 400, price: 3000, manufacturedDate: form.date(from: "20170110") ?? Date(), hot : true)
+        let cantataCoffee = Cantata("맥심", "칸타타카페모카", 400, 3000, form.date(from: "20170110") ?? Date(), hot : true)
         XCTAssertNotNil(cantataCoffee)
     }
     
     func test_조지아생성여부() {
-        let georgiaCoffee = Georgia(brand: "맥심", name: "조지아아메리카노", volume: 400, price: 3000, manufacturedDate: form.date(from: "20170110") ?? Date(), hot : true)
+        let georgiaCoffee = Georgia("맥심", "조지아아메리카노", 400, 3000, form.date(from: "20170110") ?? Date(), hot : true)
         XCTAssertNotNil(georgiaCoffee)
     }
     
@@ -77,5 +87,68 @@ class VendingMachineUnitTest: XCTestCase {
         let coffee = Coffee(brand: "더벤티", name: "아이스아메리카노", volume: 500, price: 1500, manufacturedDate: form.date(from: "20170303") ?? Date(), hot: false)
         XCTAssertFalse(coffee.isHot())
     }
+    
+    func test_자판기객체생성여부() {
+        let tempBox = [
+            StrawberryMilk("서울우유", "날마다딸기우유", 200, 1000, form.date(from: "20180117") ?? Date()),
+            BananaMilk("서울우유", "날마다바나나우유", 200, 1000, form.date(from: "20170117") ?? Date()),
+            PepciCoke("코카콜라", "다이어트콜라", 350, 2000, form.date(from: "20170110") ?? Date(), lowCalorie: true),
+            Fanta("환타", "환타오렌지", 350, 2000, form.date(from: "20170109") ?? Date(), lowCalorie: false),
+            TOPCoffee("맥심", "TOP아메리카노", 400, 3000, form.date(from: "20170117") ?? Date(), hot: false),
+            Georgia("맥심", "조지아카페모카", 450, 3500, form.date(from: "20170119") ?? Date(), hot: true),
+            ]
 
+        let vendingMachine = VendingMachine(productsBox: tempBox)
+        XCTAssertNotNil(vendingMachine)
+    }
+    
+    func test_자판기돈넣기메서드() {
+        vendingMachineForTest.addMoney(1000)
+        XCTAssertTrue(vendingMachineForTest.getBalance() == 1000)
+    }
+    
+    func test_자판기음료객체추가메서드() {
+        let baseCountOfStrawBerryMilk = vendingMachineForTest.getInventory()["딸기우유"]?.count ?? 0
+        let tempDrink = StrawberryMilk("서울우유", "날마다딸기우유", 200, 1000, form.date(from: "20180117") ?? Date())
+        vendingMachineForTest.addBeverage(tempDrink)
+        XCTAssertTrue(vendingMachineForTest.getInventory()["딸기우유"]?.count == baseCountOfStrawBerryMilk + 1)
+    }
+    
+    func test_자판기구매가능음료리스트메서드() {
+        vendingMachineForTest.addMoney(10000)
+        XCTAssertNotNil(vendingMachineForTest.generateListOfValidProduct())
+        XCTAssertTrue(vendingMachineForTest.generateListOfValidProduct().count == 6)
+    }
+    
+    func test_자판기음료구매메서드() {
+        vendingMachineForTest.addMoney(10000)
+        vendingMachineForTest.buy("딸기우유")
+        XCTAssertNil(vendingMachineForTest.getInventory()["딸기우유"])
+    }
+    
+    func test_자판기잔액확인메서드() {
+        vendingMachineForTest.addMoney(10000)
+        XCTAssertTrue(vendingMachineForTest.getBalance() == 10000)
+    }
+    
+    func test_자판기재고메서드() {
+        XCTAssertNotNil(vendingMachineForTest.getInventory())
+    }
+    
+    func test_자판기유통기간지난우유리스트생성메서드() {
+        XCTAssertNotNil(vendingMachineForTest.generateInvalidProducts())
+        XCTAssertTrue(vendingMachineForTest.generateInvalidProducts().count == 1) //바나나우유
+    }
+    
+    func test_자판기뜨거운커피우유리스트생성메서드() {
+        XCTAssertNotNil(vendingMachineForTest.generateHotProducts())
+        XCTAssertTrue(vendingMachineForTest.generateHotProducts().count == 1) //조지아
+    }
+    
+    func test_자판기구매내역리스트생성메서드() {
+        vendingMachineForTest.buy("딸기우유")
+        XCTAssertNotNil(vendingMachineForTest.generateListOfHistory())
+        XCTAssertTrue(vendingMachineForTest.generateListOfHistory().count == 1) //딸기우유
+    }
+    
 }
