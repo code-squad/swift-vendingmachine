a632d335e6cf3f392413aa443361725aa66093b8
YUAMING <miennes3@gmail.com>
2018-01-19 13:42:25 +0900

    Update InputView struct

--- VendingMachine/VendingMachine/VIews/InputView.swift
+++ VendingMachine/VendingMachine/VIews/InputView.swift
@@ -10,28 +10,28 @@ import Foundation
 
 struct InputView {
     private var vendingMachine: VendingMachine
+    private let allMenus = BeverageMenu.allValues
     
     init(vendingMachine: VendingMachine) {
         self.vendingMachine = vendingMachine
     }
     
-    func chooseAction() throws -> Mode{
+    func chooseAction() throws -> (Mode, Int)? {
         print(getCurrentState())
         
-        guard let answer = readLine() else {
-            throw BeverageErrors.invalidValue
-        }
+        guard let answer = readLine() else { throw BeverageErrors.invalidValue }
         
         return try act(answer)
     }
     
-    func act(_ action: String) throws -> Mode {
+    func act(_ action: String) throws -> (Mode, Int)? {
         let arguments = action.split(separator: " ")
         
+        if arguments[0] == "q" || arguments[0] == "quit" { return nil }
         guard arguments.count >= 2 else { throw BeverageErrors.invalidValue }
         guard let mode = Mode(rawValue: Int(arguments[0]) ?? 0) else { throw BeverageErrors.invalidValue }
         
-        return mode
+        return (mode, Int(arguments[1]) ?? 0)
     }
 }
 
@@ -53,15 +53,24 @@ extension InputView {
     private func getCurrentState() -> String {
         return """
         현재 투입한 금액이 \(vendingMachine.countChange())원입니다. 다음과 같은 음료가 있습니다.
-        \(viewCurrentInventory())
+        \(vendingMachine.countChange() == 0 ? viewCurrentInventory() : viewListOfCurrentBeverage())
         1. \(Mode.insertMoney.description)
         2. \(Mode.buyBeverage.description)
         """
     }
     
     private func viewCurrentInventory() -> String {
-        return vendingMachine.checkCurrentInventory().reduce("= >") {
+        return vendingMachine.checkCurrentInventory().reduce("=> ") {
             $0 + $1.beverageMenu.makeInstance().description + "(" + String($1.quantity) + "개) "
         }
+    }
+    
+    private func viewListOfCurrentBeverage() -> String {
+        var number = 0
+        return allMenus.map({
+            let beverage = $0.makeInstance()
+            number = number + 1
+            return String(number) + ") " + beverage.description + " " + String(beverage.price) + "원(" +  String(vendingMachine.countBeverageQuantity(beverageMenu: $0)) + "개)"
+        }).joined(separator: "\n")
     }
 }
