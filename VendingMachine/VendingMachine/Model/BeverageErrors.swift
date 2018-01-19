1f1a74df4bf5077926aaab44bae6776f4306dc83
YUAMING <miennes3@gmail.com>
2018-01-19 12:41:59 +0900

    Add error cases

--- VendingMachine/VendingMachine/Model/BeverageErrors.swift
+++ VendingMachine/VendingMachine/Model/BeverageErrors.swift
@@ -11,6 +11,7 @@ import Foundation
 enum BeverageErrors: Error {
     case outOfStock
     case notEnoughMoney
+    case invalidValue
     
     var localizedDescription: String {
         switch self {
@@ -18,6 +19,8 @@ enum BeverageErrors: Error {
             return "재고가 없습니다."
         case .notEnoughMoney:
             return "돈이 부족합니다."
+        case .invalidValue:
+            return "유효하지 않는 입력값입니다."
         }
     }
 }
