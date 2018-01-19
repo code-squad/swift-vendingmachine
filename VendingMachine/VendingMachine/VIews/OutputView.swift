5dcb60d1a10d64d7b7195f1c1c8881df12dcc8cf
YUAMING <miennes3@gmail.com>
2018-01-19 15:52:51 +0900

    Update OutputView struct

--- VendingMachine/VendingMachine/VIews/OutputView.swift
+++ VendingMachine/VendingMachine/VIews/OutputView.swift
@@ -9,30 +9,7 @@
 import Foundation
 
 struct OutputView {
-    private let vendingMachine: VendingMachine
-    
-    init(vendingMachine: VendingMachine) {
-        self.vendingMachine = vendingMachine
-    }
-    
-    func printResult(input: (InputView.Mode, Int)) throws {
-        switch input.0 {
-        case .insertMoney:
-            insertMoney(coin: input.1)
-        case .buyBeverage:
-            try printResultOfBeveragePurchase(index: input.1)
-        }
-    }
-    
-    private func insertMoney(coin: Int) {
-        vendingMachine.insertMoney(coin: coin)
-    }
-    
-    private func printResultOfBeveragePurchase(index: Int) throws {
-        let allMenus = BeverageMenu.allValues
-        let beverageMenu = allMenus[index-1]
-        let beverage = beverageMenu.makeInstance()
-        try vendingMachine.buyBeverage(beverageMenu: beverageMenu)
+    func printResult(beverage: Beverage) {
         print("\(beverage.description)를 구매하셨습니다. \(beverage.price)원을 차감합니다.", terminator: "\n")
     }
 }
