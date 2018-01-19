dee0477318c0da9f9a81dbceb357a21439dac65f
YUAMING <miennes3@gmail.com>
2018-01-19 15:53:13 +0900

    Create Mode struct

---
+++ VendingMachine/VendingMachine/Mode/Mode.swift
@@ -0,0 +1,23 @@
+//
+//  Mode.swift
+//  VendingMachine
+//
+//  Created by yuaming on 2018. 1. 19..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Mode {
+    func act(input: (InputView.Mode, Int), machine: Userable & VendingMachineManagerable) throws -> Userable & VendingMachineManagerable {
+        switch input.0 {
+        case .insertMoney:
+            machine.insertMoney(coin: input.1)
+        case .buyBeverage:
+            let beverageMenu = allMenus[input.1-1]
+            try machine.buyBeverage(beverageMenu: beverageMenu)
+        }
+        
+        return machine
+    }
+}
