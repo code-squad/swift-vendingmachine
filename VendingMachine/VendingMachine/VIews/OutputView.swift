b9369e89cba9bae65e8f32328a28077f78e7ce1d
YUAMING <miennes3@gmail.com>
2018-01-22 14:44:27 +0900

    Update OutputView struct

--- VendingMachine/VendingMachine/VIews/OutputView.swift
+++ VendingMachine/VendingMachine/VIews/OutputView.swift
@@ -9,7 +9,15 @@
 import Foundation
 
 struct OutputView {
-    func printResult(beverage: Beverage) {
-        print("\(beverage.description)를 구매하셨습니다. \(beverage.price)원을 차감합니다.", terminator: "\n")
+    func printResult(mode: ScreenHelper.Mode, answer: Int) {
+        guard mode == ScreenHelper.Mode.buyBeverage else { return }
+            
+        let beverage = makeBeverage(index: answer)
+        print("\(beverage.description)를 구매하셨습니다. \(beverage.price.countChange())원을 차감합니다.", terminator: "\n")
+    }
+    
+    private func makeBeverage(index: Int) -> Beverage {
+        return allMenus[index-1].makeInstance()
     }
 }
+
