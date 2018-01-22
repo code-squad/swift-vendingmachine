1a4a62c963117c4b92155e177d2592b22d153768
Jack <somedd@naver.com>
2018-01-22 16:15:42 +0900

    Add protocols(AdminMode,UserMode). #9

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -9,7 +9,31 @@
 import Foundation
 typealias productKind = String
 
-struct VendingMachine {
+protocol AdminMode {
+    mutating func addBeverage(_ product : Beverage)
+    mutating func removeProduct(_ product : String)
+    mutating func updateProductNumbersAndKinds()
+    func generateListOfHistory() -> [Beverage]
+    func getProductName(_ userProductNumber : InputView.ProductNumber) -> String
+    func getInventory() -> [String:[Beverage]]
+    func generateInvalidProducts() -> [Beverage]
+    func generateListOfValidProduct() -> [String]
+    func generateBeverageFromProductName(_ productName : String) -> Beverage?
+}
+
+protocol UserMode {
+    mutating func addMoney(_ userMoney : Int)
+    mutating func buy(_ product : String)
+    mutating func updateProductNumbersAndKinds()
+    func getProductName(_ userProductNumber : InputView.ProductNumber) -> String
+    func getInventory() -> [String:[Beverage]]
+    func generateInvalidProducts() -> [Beverage]
+    func generateListOfValidProduct() -> [String]
+    func getBalance() -> Int
+}
+
+struct VendingMachine : AdminMode, UserMode {
+    
     private var inventory : [String:[Beverage]] = [:]
     private var balance : Int = 0
     private var historyOfProductsSold : [Beverage] = []
@@ -22,12 +46,10 @@ struct VendingMachine {
         self.updateProductNumbersAndKinds()
     }
     
-    // 자판기 금액을 원하는 금액만큼 올리는 메소드
     mutating func addMoney(_ userMoney : Int) {
         self.balance += userMoney
     }
 
-    // 특정 상품 인스턴스를 넘겨서 재고를 추가하는 메소드
     mutating func addBeverage(_ product : Beverage) {
         if self.inventory.index(forKey: String(describing: type(of:product).self)) == nil {
             self.inventory.updateValue([product], forKey: String(describing: type(of:product).self))
@@ -36,12 +58,10 @@ struct VendingMachine {
         self.inventory[String(describing: type(of:product).self)]?.append(product)
     }
     
-    // 현재 금액으로 구매가능한 음료수 목록을 리턴하는 메소드
     func generateListOfValidProduct() -> [String] {
         return self.inventory.filter({$0.value.count > 0}).filter({($0.value[0].price) < self.balance}).map({$0.key})
     }
     
-    // 음료수를 구매하는 메소드
     mutating func buy(_ product : String) {
         let soldProduct = self.inventory[product]?.removeFirst()
         guard let product = soldProduct else { return }
@@ -49,17 +69,18 @@ struct VendingMachine {
         self.balance -= product.price
     }
     
-    // 잔액을 확인하는 메소드
     func getBalance() -> Int {
         return self.balance
     }
     
-    // 전체 상품 재고를 (사전으로 표현하는) 종류별로 리턴하는 메소드
+    mutating func removeProduct(_ product : String) {
+        self.inventory[product]?.removeFirst()
+    }
+    
     func getInventory() -> [String:[Beverage]] {
         return self.inventory.filter({$0.value.count > 0})
     }
     
-    // 유통기한이 지난 재고만 리턴하는 메소드
     func generateInvalidProducts() -> [Beverage] {
         let kindsOfMilk : [ObjectIdentifier] = [StrawberryMilk.getKind(),BananaMilk.getKind(),ChocolateMilk.getKind()]
         let milkProducts = self.inventory.map({$0.value.filter({kindsOfMilk.contains(ObjectIdentifier(type(of:$0)))})})
@@ -67,7 +88,6 @@ struct VendingMachine {
         return Array(invalidProducts.filter({!$0.isEmpty}).joined())
     }
     
-    // 따뜻한 음료만 리턴하는 메소드
     func generateHotProducts() -> [Beverage] {
         let kindsOfCoffee : [ObjectIdentifier] = [TOPCoffee.getKind(),Cantata.getKind(),Georgia.getKind()]
         let coffeeProducts = self.inventory.map({$0.value.filter({kindsOfCoffee.contains(ObjectIdentifier(type(of:$0)))})})
@@ -75,7 +95,6 @@ struct VendingMachine {
         return Array(hotProducts.filter({$0.isEmpty == false}).joined())
     }
     
-    // 시작이후 구매 상품 이력을 배열로 리턴하는 메소드
     func generateListOfHistory() -> [Beverage] {
         return self.historyOfProductsSold
     }
@@ -90,6 +109,20 @@ struct VendingMachine {
             self.productNumbersAndKinds.updateValue(oneProduct, forKey: productNumber)
             productNumber += 1
         }
+    }
+    
+    func generateBeverageFromProductName(_ productName : String) -> Beverage? {
+        switch productName {
+        case String(describing: type(of:StrawberryMilk().self)) : return StrawberryMilk()
+        case String(describing: type(of:BananaMilk().self)) : return BananaMilk()
+        case String(describing: type(of:PepciCoke().self)) : return PepciCoke()
+        case String(describing: type(of:Fanta().self)) : return Fanta()
+        case String(describing: type(of:TOPCoffee().self)) : return TOPCoffee()
+        case String(describing: type(of:Georgia().self)) : return Georgia()
+        default:
+            break
+        }
+        return nil
     }
     
 }
--- VendingMachine/VendingMachine/VendingMachineUser.swift
+++ VendingMachine/VendingMachine/VendingMachineUser.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+struct VendingMachineUser : UserMode{
+    private var vendingMachine : UserMode
+    
+    init(_ machine : UserMode) {
+        self.vendingMachine = machine
+    }
+    
+    mutating func addMoney(_ userMoney: Int) {
+        self.vendingMachine.addMoney(userMoney)
+    }
+    
+    mutating func buy(_ product: String) {
+        self.vendingMachine.buy(product)
+    }
+    
+    mutating func updateProductNumbersAndKinds() {
+        self.vendingMachine.updateProductNumbersAndKinds()
+    }
+    
+    func getProductName(_ userProductNumber: InputView.ProductNumber) -> String {
+        return self.vendingMachine.getProductName(userProductNumber)
+    }
+    
+    func getInventory() -> [String : [Beverage]] {
+        return self.vendingMachine.getInventory()
+    }
+    
+    func generateInvalidProducts() -> [Beverage] {
+        return self.vendingMachine.generateInvalidProducts()
+    }
+    
+    func generateListOfValidProduct() -> [String] {
+        return self.vendingMachine.generateListOfValidProduct()
+    }
+    
+    func getBalance() -> Int {
+        return self.vendingMachine.getBalance()
+    }
+
+}
