1793a6ea920a1ea5d0ab8e98da43b218c584b942
Jack <somedd@naver.com>
2018-01-22 16:17:05 +0900

    Add UserController for executing UserMode. #10

--- VendingMachine/VendingMachine/UserController.swift
+++ VendingMachine/VendingMachine/UserController.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+
+struct UserContorller {
+    private var vendingMachine : UserMode
+    private var runUserMode = true
+
+    
+    init(_ vendingMachineUser : UserMode) {
+        self.vendingMachine = vendingMachineUser
+    }
+    
+    mutating func executeUser() {
+        while runUserMode {
+        outputView.printUserModeBaseMessages(self.vendingMachine)
+            let userInput = inputView.readUserMenu()
+            if userInput == .invalidUserMenu {
+                outputView.printUserModeMessage(.invalidMenu)
+                continue
+            }
+            guard userInput != .exit else { return }
+            self.executeMenu(userInput)
+        }
+    }
+    
+    mutating func executeMenu(_ userInput : InputView.userMenu) {
+        switch userInput {
+        case .addMoney :
+            outputView.printUserModeMessage(.addMoney)
+            vendingMachine.addMoney(inputView.readMoney())
+        case .buyDrink :
+            vendingMachine.updateProductNumbersAndKinds()
+            outputView.printUserModeMessage(.chooseProduct)
+            let userProductNumber = inputView.readProductNumber()
+            if userProductNumber == .invalidNumber {
+                outputView.printUserModeMessage(.invalidMenu)
+            }
+            let userProductName = vendingMachine.getProductName(userProductNumber)
+            let userProduct = vendingMachine.getInventory()[userProductName]?.first
+            if vendingMachine.generateListOfValidProduct().contains(userProductName) {
+                vendingMachine.buy(userProductName)
+                outputView.printBuyProduct(userProductName, productPrice: userProduct?.price ?? 0)
+                return
+            }
+            outputView.printUserModeMessage(.shortOfMoney)
+        case .exit :
+        break
+        default :
+            break
+        }
+    }
+    
+}
