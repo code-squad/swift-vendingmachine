d3acd2b71cad18fd4490acf6a086bef2dfe6b8c0
dely2p <lhyal2p@gmail.com>
2018-01-23 16:56:37 +0900

    add AdminDTO, UserDTO to make data transfer object #29
    하드코딩으로 배열의 값에 접근하지 않도록 AdminDTO와 UserDTO를 만들었다.

---
+++ VendingMachine/VendingMachine/AdminDTO.swift
@@ -0,0 +1,15 @@
+//
+//  AdminDTO.swift
+//  VendingMachine
+//
+//  Created by Eunjin Kim on 2018. 1. 23..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct AdminDTO {
+    var inventoryAdminDriver: Int
+    var menuNumber: Int
+    var numberOfTheBeverage: Int
+}
---
+++ VendingMachine/VendingMachine/UserDTO.swift
@@ -0,0 +1,14 @@
+//
+//  UserDTO.swift
+//  VendingMachine
+//
+//  Created by Eunjin Kim on 2018. 1. 23..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct UserDTO {
+    var inventoryUserDriver: Int
+    var coinsOrBeverageMenu: Int
+}
