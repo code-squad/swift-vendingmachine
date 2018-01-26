16302a7e9f8291c7d96865c7c63e99987d5b1807
youth27 <mayihelpu27@gmail.com>
2018-01-26 16:32:20 +0900

    Step1 -beverage class

---
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -0,0 +1,20 @@
+//
+//  Coffee.swift
+//  VendingMachine
+//
+//  Created by YOUTH on 2018. 1. 26..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Beverage {
+    override var type: String {
+        return "커피"
+    }
+    override init(brand: String, weight: Int, price: Int, name: String, MFDDate: Date) {
+        super.init(brand: brand, weight: weight, price: price, name: name, MFDDate: MFDDate)
+    }
+
+}
+
---
+++ VendingMachine/VendingMachine/OutputView.swift
@@ -0,0 +1,17 @@
+//
+//  OutputView.swift
+//  VendingMachine
+//
+//  Created by YOUTH on 2018. 1. 26..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+
+    func showResult(_ text: CustomStringConvertible) {
+        print(text.description)
+    }
+
+}
