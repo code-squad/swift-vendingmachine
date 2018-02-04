c920d85ece1fcb40c1f25f7358e518c8c64d7531
youth27 <mayihelpu27@gmail.com>
2018-02-04 22:29:41 +0900

    Make Stock, StockController- Dictionary feature is in StockController

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,17 +8,4 @@
 
 import Foundation
 
-let outputView = OutputView()
-
-let strawberryMilk = Milk(flavor: "딸기", brand: "서울우유", weight: 200, price: 1000, name: "날마다딸기우유", manufactured: "20171009")
-
-let bananaMilk = Milk(flavor: "바나나", brand: "서울우유", weight: 200, price: 1000, name: "날마다바나나우유", manufactured: "20171012")
-
-let coke = SoftDrink(brand: "펩시", weight: 350, price: 2000, name: "다이어트콜라", manufactured: "20171005")
-let coffee = Coffee(brand: "맥심", weight: 400, price: 3000, name: "TOP아메리카노", manufactured: "20171010")
-
-outputView.showResult(strawberryMilk)
-outputView.showResult(bananaMilk)
-outputView.showResult(coke)
-outputView.showResult(coffee)
-
+Controller().run()
