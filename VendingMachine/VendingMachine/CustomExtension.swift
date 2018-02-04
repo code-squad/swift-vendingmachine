46b57d7d9201cfdc9032b3fa8ee82d77eb037666
youth27 <mayihelpu27@gmail.com>
2018-02-04 22:23:06 +0900

    Add Extension - Dictionary element update

--- VendingMachine/VendingMachine/CustomExtension.swift
+++ VendingMachine/VendingMachine/CustomExtension.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 
-extension Date {
+// MARK: Extension Date type
 
+extension Date {
     // 지정한 형태로 출력하기위한 description 속성
     var description: String {
         let dateFormatter = DateFormatter()
@@ -36,8 +37,45 @@ extension Date {
         // 사용기한 + 제조일자 > 오늘날짜 = true
         return currentDate < expiration
     }
-
-
 }
+
+// MARK: Extension Dictionary
+
+extension Dictionary {
+    mutating func update(other:Dictionary) {
+        for (key,value) in other {
+            self.updateValue(value, forKey:key)
+        }
+    }
+}
+
+// MARK: EnumCollection protocol
+
+public protocol EnumCollection: Hashable {
+    static func cases() -> AnySequence<Self>
+    static var allValues: [Self] { get }
+}
+
+public extension EnumCollection {
+
+    public static func cases() -> AnySequence<Self> {
+        return AnySequence { () -> AnyIterator<Self> in
+            var raw = 0
+            return AnyIterator {
+                let current: Self = withUnsafePointer(to: &raw) { $0.withMemoryRebound(to: self, capacity: 1) { $0.pointee } }
+                guard current.hashValue == raw else {
+                    return nil
+                }
+                raw += 1
+                return current
+            }
+        }
+    }
+
+    public static var allValues: [Self] {
+        return Array(self.cases())
+    }
+}
+
 
 
