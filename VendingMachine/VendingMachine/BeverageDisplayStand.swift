53258ed3734bc25d5f758f40c188645f7d3d982c
youth27 <mayihelpu27@gmail.com>
2018-02-05 15:57:55 +0900

    Add Stock, ShowCase struct - BeverageDisplayStand protocol

---
+++ VendingMachine/VendingMachine/BeverageDisplayStand.swift
@@ -0,0 +1,14 @@
+//
+//  BeverageDisplayStand.swift
+//  VendingMachine
+//
+//  Created by YOUTH on 2018. 2. 5..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol BeverageDisplayStand: CustomStringConvertible {
+    var description: String { get }
+
+}
