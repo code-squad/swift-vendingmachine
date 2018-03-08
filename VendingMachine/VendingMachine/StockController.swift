50945833c42f7a3e96c8a997f939cacbb498999d
Mijin Jeon <mayihelpu27@gmail.com>
2018-03-08 16:01:48 +0900

    Add Stock object

--- VendingMachine/VendingMachine/StockController.swift
+++ VendingMachine/VendingMachine/StockController.swift
@@ -8,134 +8,64 @@
 
 import Foundation
 
-struct StockController {
+class StockController {
 
-    private(set) var inventory = [ObjectIdentifier: [Beverage]]()
     private var history = History()
     private(set) var shelf = Shelf()
+    private var stock = Stock()
 
     init (items: [Beverage]) {
         items.forEach{ item in self.history.addSupplyLog(item)}
         let stockSets = items.reduce(into: [ObjectIdentifier: [Beverage]]()) {
             $0[ObjectIdentifier(type(of: $1)), default:[]].append($1)
         }
-        self.inventory = stockSets
-        self.shelf = Shelf(items: self.inventory)
+        self.shelf = Shelf(items: stockSets)
+        self.stock = Stock(sets: stockSets)
     }
 
-    func setAsDictionary (_ beverages: [Beverage]) -> [ObjectIdentifier: [Beverage]] {
-        let stockSets = beverages.reduce(into: [ObjectIdentifier: [Beverage]]()) {
-            $0[ObjectIdentifier(type(of: $1)), default:[]].append($1)
-        }
-    return stockSets
-    }
-
-    mutating func buy(itemCode: Int, balance: Int) throws -> Beverage {
+    func buy(itemCode: Int, balance: Int) throws -> Beverage {
         let key = try shelf.matchCode(option: itemCode)
-        let item = self.inventory[key]![0]
-        history.addPurchaseLog(item)
-        self.shelf = shelf.update(newItems: self.inventory)
+        let item = stock.buy(itemCode: key)
+        self.history.addPurchaseLog(item)
         return item
     }
 
     // remove한 뒤에 리턴해버리니까 음료수가 한개만 남은상태에서
     // 구매하려고하면 range에러뜨는 상황 발생해서 두 메소드로 나눔
-    mutating func removeItem(_ itemCode: Int) throws {
+    func removeItem(_ itemCode: Int) throws {
         let key = try shelf.matchCode(option: itemCode)
-        self.inventory[key]!.remove(at: 0)
-        self.shelf = shelf.update(newItems: self.inventory)
+        self.stock.removeItem(key)
     }
 
-    mutating func addItem(item: Beverage) {
-        for set in self.inventory {
-            if var arr = self.inventory[ObjectIdentifier(type(of:item))] {
-                arr.append(item)
-                self.inventory[ObjectIdentifier(type(of:item))] = arr
-                break
-            } else if set.key != ObjectIdentifier(type(of:item)) {
-                let newItemSet = [ObjectIdentifier(type(of:item)) : [item]]
-                self.inventory = self.inventory.update(other: newItemSet)
-            }
-        }
+    func addItem(item: Beverage) {
+        self.stock.addItem(item)
         self.history.addSupplyLog(item)
-        self.shelf = shelf.update(newItems: self.inventory)
-    }
-
-
-    func priceOfItem(_ itemCode: Int) throws -> Int {
-        let key = try self.shelf.matchCode(option: itemCode)
-        return self.inventory[key]![0].price()
     }
 
     func findHotBeverage() -> [ObjectIdentifier: [Beverage]] {
-        var available = [Beverage]()
-        for set in self.inventory {
-            for item in set.value {
-                if item.isHot() {
-                    available.append(item)
-                }
-            }
-        }
-        return self.setAsDictionary(available)
+        return stock.findHotBeverage()
     }
 
     func menu() -> String {
-        var result = ""
-        var index = 0
-        let itemCodes = self.shelf.itemTags
-
-        for itemCode in itemCodes where self.inventory[itemCode]!.count > 0 {
-            let item = self.inventory[itemCode]![0]
-            index += 1
-            result += "\(index)) \(item.type) : \(item.price())원 | \(self.inventory[itemCode]!.count)개 \n"
-        }
-        return result
+        return stock.menu()
     }
 
     func stockSummary() -> String {
-        var result = ""
-        for set in self.inventory where set.value.count > 0 {
-            result += "\(set.value[0].type) (\(set.value.count)개) | "
-        }
-        return result
+        return stock.summary()
     }
 
     // 유통기한 지난 음료
     func findDiscardBeverage() -> [ObjectIdentifier: [Beverage]] {
-        var discards = [Beverage]()
-        for set in self.inventory {
-            for product in set.value {
-                if !product.isValid() {
-                    discards.append(product)
-                }
-            }
-        }
-        return self.setAsDictionary(discards)
+        return stock.findDiscardBeverage()
     }
 
     // 유통기한 내의 음료
     func findValidBeverage() -> [ObjectIdentifier: [Beverage]] {
-        var valid = [Beverage]()
-        for set in self.inventory {
-            for product in set.value {
-                if product.isValid() {
-                    valid.append(product)
-                }
-            }
-        }
-        return self.setAsDictionary(valid)
+        return stock.findValidBeverage()
     }
 
     func finditemsCheaper(than balance: Money) -> [ObjectIdentifier: [Beverage]] {
-        var valid = [Beverage]()
-        for set in self.inventory {
-            for product in set.value {
-                if balance.isAffordable(item: product) {
-                    valid.append(product)
-                }
-            }
-        }
-         return self.setAsDictionary(valid)
+        return stock.finditemsCheaper(than: balance)
     }
 
     func showHistory() -> String {
@@ -144,14 +74,7 @@ struct StockController {
 
     // 가장 저렴한 금액의 음료 가격 리턴
     func cheapestPrice() -> Int {
-        var price = [Int]()
-        for set in self.inventory {
-            for product in set.value {
-                price.append(product.price())
-            }
-        }
-        let cheapest = price.sorted()[0]
-        return cheapest
+        return stock.cheapestPrice()
     }
 
 
