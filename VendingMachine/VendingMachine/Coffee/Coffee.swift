920f4cd00917dbd7e02ff096ea5d4e552abe080b
waitNseE <yellow.cjh@gmail.com>
2018-03-08 16:38:32 +0900

    음료 최상위 클래스 선언

---
+++ VendingMachine/VendingMachine/Coffee/Coffee.swift
@@ -0,0 +1,9 @@
+//
+//  Coffee.swift
+//  VendingMachine
+//
+//  Created by Choi Jeong Hoon on 2018. 3. 8..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
---
+++ VendingMachine/VendingMachine/Coffee/DutchCoffeeStory.swift
@@ -0,0 +1,9 @@
+//
+//  DutchCoffeeStory.swift
+//  VendingMachine
+//
+//  Created by Choi Jeong Hoon on 2018. 3. 8..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
