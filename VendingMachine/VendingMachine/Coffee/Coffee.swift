d2947bd6736c7e1e1f9ffa03e065b1026c36c7fd
waitNseE <yellow.cjh@gmail.com>
2018-03-08 16:42:14 +0900

    커피 클래스 및 하위클래스 선언 필요하위속성 및 메서드 포함

--- VendingMachine/VendingMachine/Coffee/Coffee.swift
+++ VendingMachine/VendingMachine/Coffee/Coffee.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+class Coffee: Beverage {
+    private var sizeType: CoffeeSize = .short
+    init(_ brand: String, _ weight: Int, _ price: Int, _ name: String, _ manufactured_date: Date, _ sizeType: CoffeeSize) {
+        self.sizeType = .short
+        super.init(brand, weight, price, name, manufactured_date)
+        self.sizeType = getSize(weight)
+    }
+    
+    func getSize(_ weight: Int) -> CoffeeSize {
+        return CoffeeSize(rawValue: weight) ?? .short
+    }
+}
+
+enum CoffeeTastes: CustomStringConvertible {
+    case americano
+    case top
+    case dutchCoffeeStory
+    case letsBe
+    var description: String {
+        switch self {
+        case .americano: return "아메리카노"
+        case .top: return "티오피"
+        case .dutchCoffeeStory: return "더치커피스토리"
+        case .letsBe: return "레츠비"
+        }
+    }
+}
+
+enum CoffeeSize: Int {
+    case short = 240, tall = 350, grande = 470, venti = 590
+}
