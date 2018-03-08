b3fa9da9562c9582d53ccc6d2d820678cd4f88a2
waitNseE <yellow.cjh@gmail.com>
2018-03-08 16:39:42 +0900

    soda클래스 및 하위 탄산음료 클래스 선언

--- VendingMachine/VendingMachine/Soda/Welchs.swift
+++ VendingMachine/VendingMachine/Soda/Welchs.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+class Welchs: Soda, CustomStringConvertible, Printable {
+    private var kindOfWelchs: KindOfWelchs = .grape
+    init(_ brand: String, _ weight: Int, _ price: Int, _ name: String, _ manufactured_date: Date, _ amountOfSugar: Float, _ kindOfWelchs: KindOfWelchs) {
+        self.kindOfWelchs = kindOfWelchs
+        super.init(brand, weight, price, name, manufactured_date, amountOfSugar)
+    }
+    var description: String {
+        return SodaTastes.coke.description + Welchs.className + self.beverageDescription()
+    }
+    
+    func printBeverage() {
+        print(self.description)
+    }
+}
+
+enum KindOfWelchs {
+    case grape, whiteGrape, strawberry
+}
