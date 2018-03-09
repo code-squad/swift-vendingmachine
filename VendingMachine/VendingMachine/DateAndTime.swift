5fbd8fdc4fc1b942a3557f3756c390c6a8eeb825
waitNseE <yellow.cjh@gmail.com>
2018-03-09 16:52:29 +0900

    날짜와시간관련 내용 분리

---
+++ VendingMachine/VendingMachine/DateAndTime.swift
@@ -0,0 +1,22 @@
+//
+//  Date.swift
+//  VendingMachine
+//
+//  Created by Choi Jeong Hoon on 2018. 3. 9..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+struct DateAndTime {
+    static let twoWeeksAsSeconds: Double = 1209600
+    static let fourtyFiveDaysAsSeconds: Double = 9720000
+    static let sixMonthAsSeconds: Double = 10368000
+    static let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        formatter.timeZone = TimeZone(abbreviation: "GMT+0:00")
+        return formatter
+    }()
+
+}
