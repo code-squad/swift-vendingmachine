8e9124a96c01362aa84f7221751d8110980710e8
waitNseE <yellow.cjh@gmail.com>
2018-03-15 20:27:54 +0900

    음료추가 및 간소화

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -12,7 +12,8 @@ var welchs = Welchs("톡쏘는정훈", 400, 1500, "웰치스", "20171105", 40, .
 var chocoFlavoredMilk = ChocoFlavoredMilk("달콤한정훈", 250, 1700, "맛좋은초코유유", "20180301", .second_Grade, 200, 1)
 var top = Top("분위기있는정훈", 200, 1500, "TOP", "20171225", .grande, .basic)
 var dutchCoffeeStory = DutchCoffeeStory("분위기있는정훈", 300, 3000, "더치커피스토리", "20171005", .short, .forGiftUse)
-var beverages: [Beverage] = [chocoFlavoredMilk, top, dutchCoffeeStory, welchs]
+let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20160301", .second_Grade, 72, 0.7)
+var beverages: [Beverage] = [chocoFlavoredMilk, top, dutchCoffeeStory, welchs, bananaFlavord]
 
 var controller = Controller()
 beverages.forEach {
@@ -20,7 +21,6 @@ beverages.forEach {
 }
 let outputView = Outputview(controller)
 outputView.printMonitor()
-
 while true {
     guard let input = InputView.readMenu() else {
         print("올바른 메뉴번호를 입력해주세요")
