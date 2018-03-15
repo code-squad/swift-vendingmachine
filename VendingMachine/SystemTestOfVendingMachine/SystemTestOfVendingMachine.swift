02969a04ca18cdf1f1b787f69ff38a27c5f78038
waitNseE <yellow.cjh@gmail.com>
2018-03-15 20:24:29 +0900

    controller 및 통합테스트 추가

--- VendingMachine/SystemTestOfVendingMachine/SystemTestOfVendingMachine.swift
+++ VendingMachine/SystemTestOfVendingMachine/SystemTestOfVendingMachine.swift
@@ -9,27 +9,121 @@
 import XCTest
 
 class SystemTestOfVendingMachine: XCTestCase {
+    var controller = Controller()
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_AddMoney() {
+        controller.add(money: 500)
+        XCTAssertEqual(controller.checkUserBalance(), 500)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func test_withdrawlBalance() {
+        controller.add(money: 10000)
+        controller.withdrawlBalance()
+        XCTAssertEqual(controller.checkUserBalance(), 0)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_Success_Add_Product() {
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180305", .second_Grade, 72, 0.7)
+        controller.add(product: bananaFlavord)
+        var list = controller.listOfInventory()
+       XCTAssertEqual(list.count, 1)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_Fail_Add_Product() {
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180305", .second_Grade, 72, 0.7)
+        let chocoFlavored = ChocoFlavoredMilk("신선한정훈", 250, 1500, "맛좋은초코우유", "20180307", .first_Grade, 62, 1.6)
+        controller.add(product: chocoFlavored)
+        var list = controller.listOfInventory()
+        XCTAssertNotEqual(list.count, 1)
+    }
+    
+    func test_잔액이부족한경우_구매가능한리스트확인() {
+        controller.add(money: 500)
+        let welchs = Welchs("톡쏘는정훈", 200, 1500, "맛좋은웰취스", "20171001", 110, .strawberry)
+        let coke = Coke("톡쏘는정훈", 250, 1000, "맛좋은코크", "20170907", 100, .original)
+        controller.add(product: welchs)
+        controller.add(product: coke)
+        let shopping = controller.showListOfBuyableBeverage()
+        XCTAssertEqual(shopping.count, 0)
+    }
+    
+    func test_잔액충분한데유통기한이지난경우_구매가능한리스트확인() {
+        controller.add(money: 3500)
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20170305", .second_Grade, 72, 0.7)
+        let chocoFlavored = ChocoFlavoredMilk("신선한정훈", 250, 1500, "맛좋은초코우유", "20170307", .first_Grade, 62, 1.6)
+        controller.add(product: bananaFlavord)
+        controller.add(product: chocoFlavored)
+        let shopping = controller.showListOfBuyableBeverage()
+        XCTAssertEqual(shopping.count, 0)
+    }
+    
+    func test_잔액충분_유통기한지나지않은경우_구매가능리스트확인() {
+        controller.add(money: 3500)
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180301", .second_Grade, 72, 0.7)
+        let chocoFlavored = ChocoFlavoredMilk("신선한정훈", 250, 1500, "맛좋은초코우유", "20180301", .first_Grade, 62, 1.6)
+        controller.add(product: bananaFlavord)
+        controller.add(product: chocoFlavored)
+        let shopping = controller.showListOfBuyableBeverage()
+        XCTAssertEqual(shopping.count, 2)
+    }
+    
+    func test_구매성공여부확인() {
+        controller.add(money: 3500)
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180301", .second_Grade, 72, 0.7)
+        controller.add(product: bananaFlavord)
+        let beverage = controller.buy(productIndex: 1)!
+        XCTAssertEqual(beverage, bananaFlavord)
+    }
+    
+    func test_구매실패여부확인_없는상품_구매하려고할때() {
+        controller.add(money: 3500)
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180301", .second_Grade, 72, 0.7)
+        let chocoFlavored = ChocoFlavoredMilk("신선한정훈", 250, 1500, "맛좋은초코우유", "20180301", .first_Grade, 62, 1.6)
+        controller.add(product: chocoFlavored)
+        let beverage = controller.buy(productIndex: 1)!
+        XCTAssertNotEqual(beverage, bananaFlavord)
+    }
+    
+    func test_유통기한만료된음료만_리스트_정상확인되는지_확인() {
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20160301", .second_Grade, 72, 0.7)
+        let chocoFlavored = ChocoFlavoredMilk("신선한정훈", 250, 1500, "맛좋은초코우유", "20160301", .first_Grade, 62, 1.6)
+        let welchs = Welchs("톡쏘는정훈", 200, 1500, "맛좋은웰취스", "20171001", 110, .strawberry)
+        controller.add(product: bananaFlavord)
+        controller.add(product: chocoFlavored)
+        controller.add(product: welchs)
+        let list = controller.checkListOfOverExpirationDate()
+        XCTAssertEqual(list, [bananaFlavord,chocoFlavored])
+    }
+    
+    func test_구매이력_정상확인되는지_여부() {
+        controller.add(money: 3500)
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180301", .second_Grade, 72, 0.7)
+        controller.add(product: bananaFlavord)
+        let beverage = controller.buy(productIndex: 1)!
+        let history = controller.showShoppingHistory()
+        XCTAssertEqual(beverage, history[0].key)
+    }
+    
+    func test_자판기_정상작동여부_통합테스트() {
+        var welchs = Welchs("톡쏘는정훈", 400, 1500, "웰치스", "20171105", 40, .grape)
+        var chocoFlavoredMilk = ChocoFlavoredMilk("달콤한정훈", 250, 1700, "맛좋은초코유유", "20180301", .second_Grade, 200, 1)
+        var top = Top("분위기있는정훈", 200, 1500, "TOP", "20171225", .grande, .basic)
+        var dutchCoffeeStory = DutchCoffeeStory("분위기있는정훈", 300, 3000, "더치커피스토리", "20171005", .short, .forGiftUse)
+        let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20160301", .second_Grade, 72, 0.7)
+        var beverages: [Beverage] = [chocoFlavoredMilk, top, dutchCoffeeStory, welchs, bananaFlavord]
+        beverages.forEach {
+            controller.add(product: $0)
         }
+        XCTAssertEqual(controller.listOfInventory().count, 5)
+        controller.add(money: 10000)
+        XCTAssertEqual(controller.checkUserBalance(), 10000)
+        let buyableBeverage = controller.showListOfBuyableBeverage()
+        XCTAssertEqual(buyableBeverage.count, 4)
+        let purchasedBeverage = controller.buy(welchs)
+        XCTAssertEqual(controller.checkUserBalance(), 8500)
+        XCTAssertEqual(controller.showShoppingHistory()[0].key, purchasedBeverage)
+        controller.withdrawlBalance()
+        XCTAssertEqual(controller.checkUserBalance(), 0)
     }
     
 }
