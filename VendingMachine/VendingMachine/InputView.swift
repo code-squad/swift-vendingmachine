01d5fa262557fd7315900f0f084c491296d56be0
waitNseE <yellow.cjh@gmail.com>
2018-03-21 16:03:23 +0900

    관리,유저모드 추가로 인풋메서드 분리

--- VendingMachine/VendingMachine/InputView.swift
+++ VendingMachine/VendingMachine/InputView.swift
@@ -9,32 +9,25 @@
 import Foundation
 
 struct InputView {
-    static func readMenu() -> (menuOption: Menu, detail: Int)? {
-        let menu: String = """
-    1. 금액추가
-    2. 음료구입
-    3. 잔돈출금
-    >
-    """
-        print(menu, terminator: " ")
+    
+    static func read() -> Int? {
+        let menu: String = "1. 관리자모드\n2. 사용자모드"
+        print(menu)
+        return Int(readLine() ?? "Error")
+        
+    }
+    static func readMenu() -> (menuOption: Action, detail: Int)? {
         var realMenu = [Int]()
         if let menu = readLine() {
             guard menu != "q" else { return nil }
             if menu == "3" {
-                return (menuOption: .withdrawlBalance, -1)
+                return (menuOption: .exit, -1)
             }
             let tempMenu = menu.split(separator: " ").flatMap{ Int($0) }
             realMenu = tempMenu
         }
         if realMenu.count != 2 { return nil }
-        
-        guard let menuOption = Menu(rawValue: realMenu[0]) else { return nil }
+        guard let menuOption = Action(rawValue: realMenu[0]) else { return nil }
         return (menuOption, realMenu[1])
-    }
-}
-
-extension InputView {
-    enum Menu: Int {
-        case putCoin = 1, buyBeverage, withdrawlBalance
     }
 }
