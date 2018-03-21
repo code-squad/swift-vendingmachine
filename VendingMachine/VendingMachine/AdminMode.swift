9cb3cbfb255014834492f60983d84b1b060a2333
waitNseE <yellow.cjh@gmail.com>
2018-03-21 15:27:45 +0900

    사용자 및 관리자모드 공통기능 추상화한 프로토콜

---
+++ VendingMachine/VendingMachine/AdminMode.swift
@@ -0,0 +1,9 @@
+//
+//  AdminMode.swift
+//  VendingMachine
+//
+//  Created by Choi Jeong Hoon on 2018. 3. 19..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
---
+++ VendingMachine/VendingMachine/ControllerCore.swift
@@ -0,0 +1,9 @@
+//
+//  ControllerCore.swift
+//  VendingMachine
+//
+//  Created by Choi Jeong Hoon on 2018. 3. 20..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
