543bf91c1bb18e06eecf4e7fc3318adeca5797f5
waitNseE <yellow.cjh@gmail.com>
2018-03-22 21:14:18 +0900

    모드에 따른 메서드 구현부가 Inventory로 이관후 함수 호출로 로직 사용

--- VendingMachine/VendingMachine/ControllerCore.swift
+++ VendingMachine/VendingMachine/ControllerCore.swift
@@ -9,52 +9,40 @@
 import Foundation
 
 class ControllerCore {
-    private var inventory = [Beverage]()
-    private var shoppingLists = [Beverage]()
-    private var userMoney: Int = 0
-    private var income: Int = 0
-    
+    private var inventory: Inventory
+    private var sshoppingHistory: ShoppingLists
     static let welchs = Welchs("톡쏘는정훈", 400, 1500, "웰치스", "20171105", 40, .grape)
     static let chocoFlavoredMilk = ChocoFlavoredMilk("달콤한정훈", 250, 1700, "맛좋은초코유유", "20180301", .second_Grade, 200, 1)
     static let top = Top("분위기있는정훈", 200, 1500, "TOP", "20171225", .grande, .basic)
     static let dutchCoffeeStory = DutchCoffeeStory("분위기있는정훈", 300, 3000, "더치커피스토리", "20171005", .short, .forGiftUse)
-    static let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180320", .second_Grade, 72, 0.7)
+    static let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180321", .second_Grade, 72, 0.7)
     static let beverages: [Beverage] = [chocoFlavoredMilk, top, dutchCoffeeStory, welchs, bananaFlavord]
+    init() {
+        self.inventory = Inventory()
+        self.sshoppingHistory = ShoppingLists()
+    
+    }
 
 }
 
 
-extension ControllerCore: AdminModeCore {
+extension ControllerCore: AdminModable {
     func add(productIndex: Int) throws {
-        let listOfDrink = drinkLists()
-        guard listOfDrink.indices.contains(productIndex-1)  else {
-            throw stockError.invalidProductNumber
-        }
-        inventory.append(listOfDrink[productIndex-1])
+        try inventory.add(productIndex: productIndex)
     }
     
     func subtract(productIndex: Int) throws -> Beverage {
-        let listOfDrink = drinkLists()
-        guard listOfDrink.indices.contains(productIndex - 1) else {
-            throw stockError.invalidProductNumber
-        }
-        let deleteDrink = listOfDrink[productIndex - 1]
-        for drink in inventory.enumerated() {
-            if drink.element === deleteDrink {
-                inventory.remove(at: drink.offset)
-                return drink.element
-            }
-        }
-        throw stockError.empty
+        return try inventory.subtract(productIndex: productIndex)
+        
     }
     
     func checkIncome() -> Int {
-        return income
+        return inventory.checkIncome()
     }
     
     func listOfInventory() -> [Beverage : Int] {
         var countDictionary = [Beverage : Int]()
-        for beverage in inventory {
+        for beverage in inventory.beverageLists {
             let count = countDictionary[beverage] ?? 0
             countDictionary[beverage] = count + 1
         }
@@ -62,61 +50,41 @@ extension ControllerCore: AdminModeCore {
     }
     
     func checkListOfOverExpirationDate() -> [Beverage] {
-        return inventory.filter { $0.isValidate() == false }
+        return inventory.beverageLists.filter { $0.isValidate() == false }
     }
     
     func drinkLists() -> [Beverage] {
-        return ControllerCore.beverages
+        return inventory.drinkLists()
     }
-    
     
 }
 
-extension ControllerCore: UserModeCore {
+extension ControllerCore: UserModable {
+    func buy(productIndex: Int) throws -> Beverage {
+        
+       return try self.inventory.buy(productIndex: productIndex)
+    }
+    
     func insertMoney(userMoney: Int) {
-        self.userMoney = userMoney
+        inventory.insertMoney(userMoney: userMoney)
     }
     
     func buyableBeverages() -> [Beverage] {
-        let listOfCanBuy = inventory.filter { $0.isValidate() &&
-                                                                                        $0.price <= self.userMoney
-                                                                                     }
+        let listOfCanBuy = inventory.beverageLists.filter { $0.isValidate() && $0.price <= inventory.userBalance() }
         return listOfCanBuy
     }
     
-    func buy(productIndex: Int) throws -> Beverage {
-        let drinks = buyableBeverages()
-        guard drinks.indices.contains(productIndex - 1) else {
-            throw stockError.invalidProductNumber
-        }
-        let purchasedBeverage = drinks[productIndex - 1]
-        for beverage in inventory.enumerated() {
-            if beverage.element == purchasedBeverage {
-                self.userMoney -= purchasedBeverage.price
-                self.income += purchasedBeverage.price
-                self.inventory.remove(at: beverage.offset)
-                self.shoppingLists.append(purchasedBeverage)
-                return purchasedBeverage
-            }
-        }
-        throw stockError.soldOut
-    }
-    
     func userBalance() -> Int {
-        return self.userMoney
+        return inventory.userBalance()
     }
     
     func shoppingHistory() -> [Beverage] {
-        return self.inventory
+        return self.inventory.shoppingHistory()
     }
     
     func withdrawlBalance() -> Int {
-        let balance = self.userMoney
-        userMoney = 0
-        return balance
+        return inventory.withdrawlBalance()
     }
-    
-    
 }
 
 extension ControllerCore {
