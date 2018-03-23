3d33755827bb780fe848351b885077efa3a3700f
waitnsee <yellow.cjh@gmail.com>
2018-03-23 17:21:26 +0900

    인벤토리는 해당역할만하게 하고 나머지 역할은 모두 ControllerCore가 하도록함, ControllerCore는 인벤토리, 머니, 구매목록 객체를 연결

--- VendingMachine/VendingMachine/ControllerCore.swift
+++ VendingMachine/VendingMachine/ControllerCore.swift
@@ -9,19 +9,10 @@
 import Foundation
 
 class ControllerCore {
-    private var inventory: Inventory
-    static let welchs = Welchs("톡쏘는정훈", 400, 1500, "웰치스", "20171105", 40, .grape)
-    static let chocoFlavoredMilk = ChocoFlavoredMilk("달콤한정훈", 250, 1700, "맛좋은초코유유", "20180301", .second_Grade, 200, 1)
-    static let top = Top("분위기있는정훈", 200, 1500, "TOP", "20171225", .grande, .basic)
-    static let dutchCoffeeStory = DutchCoffeeStory("분위기있는정훈", 300, 3000, "더치커피스토리", "20171005", .short, .forGiftUse)
-    static let bananaFlavord = BananaFlavoredMilk("신선한정훈", 250, 1500, "맛좋은바나나우유", "20180321", .second_Grade, 72, 0.7)
-    static let beverages: [Beverage] = [chocoFlavoredMilk, top, dutchCoffeeStory, welchs, bananaFlavord]
-    init() {
-        self.inventory = Inventory()
-    }
-
+    private var inventory = Inventory()
+    private var shoppingLists = ShoppingLists()
+    private var money = Money()
 }
-
 
 extension ControllerCore: AdminModable, AdminIncome {
     func add(productIndex: Int) throws {
@@ -30,11 +21,10 @@ extension ControllerCore: AdminModable, AdminIncome {
     
     func subtract(productIndex: Int) throws -> Beverage {
         return try inventory.subtract(productIndex: productIndex)
-        
     }
     
     func checkIncome() -> Int {
-        return inventory.checkIncome()
+        return money.checkIncome()
     }
     
     func listOfInventory() -> [Beverage : Int] {
@@ -53,37 +43,60 @@ extension ControllerCore: AdminModable, AdminIncome {
 
 extension ControllerCore: UserModable, UserMoney {
     func buy(productIndex: Int) throws -> Beverage {
-        
-       return try self.inventory.buy(productIndex: productIndex)
+        let drinks = buyableBeverages()
+        guard drinks.indices.contains(productIndex - 1) else {
+            throw stockError.invalidProductNumber
+        }
+        let purchasedBeverage = drinks[productIndex - 1]
+        for beverage in inventory.beverages.enumerated() {
+            if beverage.element == purchasedBeverage {
+                self.money.userMoney -= purchasedBeverage.price
+                money.vendingMachineIncome += purchasedBeverage.price
+                self.money.vendingMachineIncome += purchasedBeverage.price
+                _ = try self.inventory.subtract(productIndex: beverage.offset)
+                self.shoppingLists.buy(purchasedBeverage)
+                return purchasedBeverage
+            }
+        }
+        throw stockError.soldOut
     }
     
     func insertMoney(userMoney: Int) {
-        inventory.insertMoney(userMoney: userMoney)
+        money.insertMoney(userMoney: userMoney)
     }
     
     func buyableBeverages() -> [Beverage] {
-        return inventory.buyableBeverages()
+        let listOfCanBuy = inventory.beverages.filter { $0.isValidate() &&
+            $0.price <= userBalance()
+        }
+        return listOfCanBuy
     }
     
     func userBalance() -> Int {
-        return inventory.userBalance()
+        return money.userBalance()
     }
     
-    func shoppingHistory() -> [Beverage] {
-        return self.inventory.shoppingHistory()
+    func shoppingHistory() -> ShoppingLists {
+        return self.shoppingLists.shoppingHistory()
     }
     
     func withdrawlBalance() -> Int {
-        return inventory.withdrawlBalance()
+        return money.withdrawlBalance()
     }
 }
 
 extension ControllerCore {
-    enum stockError: String, Error {
-        case soldOut = "해당 음료수는 품절되었습니다."
-        case invalidProductNumber = "유효하지 않은 음료수 번호 입니다."
-        case empty = "재고가 하나도 없습니다."
+    enum stockError: CustomStringConvertible, Error {
+        case soldOut
+        case invalidProductNumber
+        case empty
+        var description: String {
+            switch self {
+            case .soldOut: return "해당 음료수는 품절되었습니다."
+            case .invalidProductNumber : return "유효하지 않은 음료수 번호 입니다."
+            case .empty : return "재고가 하나도 없습니다."
+            }
+        }
     }
-    
 }
 
