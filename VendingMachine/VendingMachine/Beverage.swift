33b5850de12290d98004f21015e5c4bfac13801b
Moon <myssun0325@gmail.com>
2018-06-05 21:49:34 +0900

    Add name property

--- VendingMachine/VendingMachine/Beverage.swift
+++ VendingMachine/VendingMachine/Beverage.swift
@@ -9,21 +9,24 @@
 import Foundation
 
 class Beverage {
+    
     private let brand: String
     private let capacity: Int
     private let price: Int
+    private let name: String
     private let date: Date
     
-    init(brand: String, capacity: Int, price: Int, date: Date) {
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date) {
         self.brand = brand
         self.capacity = capacity
         self.price = price
+        self.name = name
         self.date = date
     }
 }
 
 extension Beverage: CustomStringConvertible {
     var description: String {
-        return "\(self.brand), \(self.capacity)ml, \(self.price)원, \(self.date)"
+        return "\(self.brand), \(self.capacity)ml, \(self.price)원, \(self.name), \(self.date)"
     }
 }
