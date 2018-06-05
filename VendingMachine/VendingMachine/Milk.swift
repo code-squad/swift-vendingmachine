47afff1b120c5c7731b7ea7d348adbe147000148
Moon <myssun0325@gmail.com>
2018-06-05 20:52:41 +0900

    Create Milk class

---
+++ VendingMachine/VendingMachine/Milk.swift
@@ -0,0 +1,13 @@
+//
+//  Milk.swift
+//  VendingMachine
+//
+//  Created by moon on 2018. 6. 5..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Milk: Beverage {
+    
+}
