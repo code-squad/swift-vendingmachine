6be37d21b83ec26ee4c06ce2bd137390eb156819
Moon <myssun0325@gmail.com>
2018-06-05 21:18:28 +0900

    Create Coffee class

---
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -0,0 +1,13 @@
+//
+//  Coffee.swift
+//  VendingMachine
+//
+//  Created by moon on 2018. 6. 5..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Beverage {
+    
+}
