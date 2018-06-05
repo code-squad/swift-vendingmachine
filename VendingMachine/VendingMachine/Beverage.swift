24ad5ff23bdf77fc61eebb615e8c952505a50bca
Moon <myssun0325@gmail.com>
2018-06-05 17:00:22 +0900

    Add Beverage class

---
+++ VendingMachine/VendingMachine/Beverage.swift
@@ -0,0 +1,13 @@
+//
+//  Beverage.swift
+//  VendingMachine
+//
+//  Created by moon on 2018. 6. 5..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Beverage {
+    
+}
