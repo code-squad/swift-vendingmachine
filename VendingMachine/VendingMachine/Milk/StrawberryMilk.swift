398df7e61716f68b047e3b2d02b4cccb3f0d50d8
Moon <myssun0325@gmail.com>
2018-06-07 17:03:42 +0900

    Create Mlik's subclasses

---
+++ VendingMachine/VendingMachine/Milk/StrawberryMilk.swift
@@ -0,0 +1,13 @@
+//
+//  StrawberryMilk.swift
+//  VendingMachine
+//
+//  Created by moon on 2018. 6. 7..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class StrawberryMilk {
+    
+}
