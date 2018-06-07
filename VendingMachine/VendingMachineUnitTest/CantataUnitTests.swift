de6387ba39207776542dfc60c7b82e9e6e89a8d6
Moon <myssun0325@gmail.com>
2018-06-07 21:05:44 +0900

    Add Cantata unit test case
    Insantiate success test

---
+++ VendingMachine/VendingMachineUnitTest/CantataUnitTests.swift
@@ -0,0 +1,19 @@
+//
+//  CantataUnittests.swift
+//  VendingMachineUnitTest
+//
+//  Created by moon on 2018. 6. 7..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import XCTest
+
+@testable import VendingMachine
+
+class CantataUnitTest: XCTestCase {
+    
+    func testCantataInstantiate_success() {
+        XCTAssertNotNil(Cantata(brand: "칸타타", capacity: 200, price: 1000, name: "칸타타블렌디드", manufacturedDate: "20180101", caffeineContent: 120, flavor: "블렌디드"))
+    }
+}
+
