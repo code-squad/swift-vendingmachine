df73bf231ba0195cbc2d303216b90ca363fb9c15
Moon <myssun0325@gmail.com>
2018-06-07 21:21:23 +0900

    Add Strawberry unit test case
    Instantiate success test

---
+++ VendingMachine/VendingMachineUnitTest/StrawberryMilkUnitTest.swift
@@ -0,0 +1,18 @@
+//
+//  StrawberryMilkUnitTest.swift
+//  VendingMachineUnitTest
+//
+//  Created by moon on 2018. 6. 7..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import XCTest
+
+@testable import VendingMachine
+
+class StrawberryMilkUnitTest: XCTestCase {
+
+    func testStrawberryMilkInstantiate_success() {
+        XCTAssertNotNil(StrawberryMilk(brand: "서울우유", capacity: 500, price: 1300, name: "딸기맛우유", manufacturedDate: "20180101", containRealStrawberry: true))
+    }
+}
