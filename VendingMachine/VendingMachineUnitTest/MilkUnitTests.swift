e6e4790f5edc6d2f77d46d151192814ed43f78b4
Moon <myssun0325@gmail.com>
2018-06-07 21:17:47 +0900

    Add Milk unit tests
    check expired date is over or not

--- VendingMachine/VendingMachineUnitTest/MilkUnitTests.swift
+++ VendingMachine/VendingMachineUnitTest/MilkUnitTests.swift
@@ -12,8 +12,25 @@ import XCTest
 
 class MilkUnitTest: XCTestCase {
     
+    var milk: Milk!
+    
+    override func setUp() {
+        super.setUp()
+        self.milk = Milk(brand: "서울우유", capacity: 200, price: 1000, name: "흰우유", manufacturedDate: "20180303")
+    }
+    
     func testMilkInstantiate_success() {
         XCTAssertNotNil(Milk(brand: "서울우유", capacity: 200, price: 1000, name: "흰우유", manufacturedDate: "20180303"))
+    }
+    
+    func testExpiredDateIsOver_withCheckDate() {
+        let checkDate: Date = "20180314".customDateFormat
+        XCTAssertTrue(self.milk.isOverExpired(with: checkDate))
+    }
+    
+    func testExpiredDateIsNotOver_withCheckDate() {
+        let checkDate: Date = "20180309".customDateFormat
+        XCTAssertFalse(self.milk.isOverExpired(with: checkDate))
     }
 }
 
