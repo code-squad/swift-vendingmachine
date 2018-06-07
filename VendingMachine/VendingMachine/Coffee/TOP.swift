f75cec90b5cfb3dacc3503d64e44504c69d99a66
Moon <myssun0325@gmail.com>
2018-06-07 16:55:55 +0900

    Add canColor property in TOP

--- VendingMachine/VendingMachine/Coffee/TOP.swift
+++ VendingMachine/VendingMachine/Coffee/TOP.swift
@@ -10,4 +10,15 @@ import Foundation
 
 class TOP: Coffee {
     
+    enum CanColor {
+        case yellow
+        case black
+    }
+    
+    private let canColor: CanColor
+    
+    init(brand: String, capacity: Int, price: Int, name: String, manufacturedDate: String, caffeineContent: Double, canColor: CanColor) {
+        self.canColor = canColor
+        super.init(brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate, caffeineContent: caffeineContent)
+    }
 }
