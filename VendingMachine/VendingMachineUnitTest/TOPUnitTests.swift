6c1f7fa190b183784c34aa1c69382061a43dbc26
Moon <myssun0325@gmail.com>
2018-06-07 21:01:57 +0900

    Add TOP unit test case
    TOP instantiate success test

---
+++ VendingMachine/VendingMachineUnitTest/TOPUnitTests.swift
@@ -0,0 +1,18 @@
+//
+//  TOPUnitTests.swift
+//  VendingMachineUnitTest
+//
+//  Created by moon on 2018. 6. 7..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import XCTest
+
+@testable import VendingMachine
+
+class TOPUnitTest: XCTestCase {
+    
+    func testTOPInstatiate_success() {
+        XCTAssertNotNil(TOP(brand: "맥심", capacity: 400, price: 300, name: "TOP아메리카노", manufacturedDate: "20171010", caffeineContent: 100, canColor: .black))
+    }
+}
