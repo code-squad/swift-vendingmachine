7ac44f0c8027c32f7e3962af7dce9094167071e1
Moon <myssun0325@gmail.com>
2018-06-07 15:01:38 +0900

    Create Coffee's subclasses

---
+++ VendingMachine/VendingMachine/Coffee/Cantata.swift
@@ -0,0 +1,13 @@
+//
+//  Cantata.swift
+//  VendingMachine
+//
+//  Created by moon on 2018. 6. 7..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Cantata: Coffee {
+    
+}
