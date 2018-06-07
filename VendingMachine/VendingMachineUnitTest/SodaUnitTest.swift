da0fe516db1d76414e2c2df8395a28fd3aa40084
Moon <myssun0325@gmail.com>
2018-06-07 21:34:10 +0900

    Add Soda unit test cases
    test isZeroCalories method

--- VendingMachine/VendingMachineUnitTest/SodaUnitTest.swift
+++ VendingMachine/VendingMachineUnitTest/SodaUnitTest.swift
@@ -30,4 +30,25 @@ class SodaUnitTest: XCTestCase {
         XCTAssertFalse(soda.isLowCalories())
     }
     
+    func testMinus1Calories_SodaIsNotZeroCalories() {
+        let testCalories = -1
+        let soda = Soda(brand: "동아오츠카", capacity: 350, price: 2000, name: "포카리", manufacturedDate: "20180101", calories: testCalories)
+        
+        XCTAssertFalse(soda.isZeroCalories())
+    }
+    
+    func test0Calories_SodaIsZeroCalories() {
+        let testCalories = 0
+        let soda = Soda(brand: "동아오츠카", capacity: 350, price: 2000, name: "포카리", manufacturedDate: "20180101", calories: testCalories)
+        
+        XCTAssertTrue(soda.isZeroCalories())
+    }
+    
+    func test1Calories_SodaIsNotZeroCalories() {
+        let testCalories = 1
+        let soda = Soda(brand: "동아오츠카", capacity: 350, price: 2000, name: "포카리", manufacturedDate: "20180101", calories: testCalories)
+        
+        XCTAssertFalse(soda.isZeroCalories())
+    }
+    
 }
