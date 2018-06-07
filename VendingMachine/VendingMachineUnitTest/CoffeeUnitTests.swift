ffd01ea92cb8d844d27b8e5fba47acf78a93fd58
Moon <myssun0325@gmail.com>
2018-06-07 20:55:07 +0900

    Add Coffee unit test cases
    isNonCaffeine method test

--- VendingMachine/VendingMachineUnitTest/CoffeeUnitTests.swift
+++ VendingMachine/VendingMachineUnitTest/CoffeeUnitTests.swift
@@ -12,8 +12,25 @@ import XCTest
 
 class CoffeeUnitTests: XCTestCase {
     
+    var coffee: Coffee!
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.coffee = Coffee(brand: "맥심", capacity: 400, price: 3000, name: "그냥커피", manufacturedDate: "20180606", caffeineContent: 150)
+    }
+    
     func testCoffeeInstantiate_success() {
         let coffee: Coffee = Coffee(brand: "맥심", capacity: 400, price: 3000, name: "그냥커피", manufacturedDate: "20180606", caffeineContent: 150)
         XCTAssertNotNil(coffee)
+    }
+    
+    func testCoffeeIsNonCaffeine() {
+        let coffee: Coffee = Coffee(brand: "맥심", capacity: 400, price: 3000, name: "그냥커피", manufacturedDate: "20180606", caffeineContent: 0)
+        XCTAssertTrue(coffee.isNonCaffeine())
+    }
+    
+    func testCoffeeContainCaffeine() {
+        XCTAssertFalse(self.coffee.isNonCaffeine())
     }
 }
