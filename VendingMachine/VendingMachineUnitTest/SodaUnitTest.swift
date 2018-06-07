f21705980361da668d324a1f6c8c76f5cd3059c0
Moon <myssun0325@gmail.com>
2018-06-07 21:30:37 +0900

    Add Soda unit test cases
    test isLowerCalories method

--- VendingMachine/VendingMachineUnitTest/SodaUnitTest.swift
+++ VendingMachine/VendingMachineUnitTest/SodaUnitTest.swift
@@ -11,8 +11,23 @@ import XCTest
 @testable import VendingMachine
 
 class SodaUnitTest: XCTestCase {
-
+    
     func testSodaInstantiate_success() {
         XCTAssertNotNil(Soda(brand: "동아오츠카", capacity: 350, price: 2000, name: "포카리", manufacturedDate: "20180101", calories: 20))
     }
+    
+    func test99Calories_SodaIsLowCalories() {
+        let testCalories = 99
+        let soda = Soda(brand: "동아오츠카", capacity: 350, price: 2000, name: "포카리", manufacturedDate: "20180101", calories: testCalories)
+        
+        XCTAssertTrue(soda.isLowCalories())
+    }
+    
+    func test100Calories_SodaIsNotLowCalories() {
+        let testCalories = 100
+        let soda = Soda(brand: "동아오츠카", capacity: 350, price: 2000, name: "포카리", manufacturedDate: "20180101", calories: testCalories)
+        
+        XCTAssertFalse(soda.isLowCalories())
+    }
+    
 }
