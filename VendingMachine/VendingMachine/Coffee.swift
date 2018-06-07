25938d28cf6aadabc0cf0c95bc903ac339a7bdb0
Moon <myssun0325@gmail.com>
2018-06-07 14:45:11 +0900

    Add caffeineContent property in Coffee

--- VendingMachine/VendingMachine/Coffee.swift
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -9,4 +9,10 @@
 import Foundation
 
 class Coffee: Beverage {
+    private let caffeineContent: Double
+    
+    init(brand: String, capacity: Int, price: Int, name: String, manufacturedDate: String, caffeineContent: Double) {
+        self.caffeineContent = caffeineContent
+        super.init(brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate)
+    }
 }
