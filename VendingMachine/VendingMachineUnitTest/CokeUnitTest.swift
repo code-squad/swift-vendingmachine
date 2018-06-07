0414efadb94009c06d258b2c0dc1af153e7d161e
Moon <myssun0325@gmail.com>
2018-06-07 21:36:14 +0900

    Add Coke Unit test case
    Instantiate success test

---
+++ VendingMachine/VendingMachineUnitTest/CokeUnitTest.swift
@@ -0,0 +1,18 @@
+//
+//  CokeUnitTest.swift
+//  VendingMachineUnitTest
+//
+//  Created by moon on 2018. 6. 7..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import XCTest
+
+@testable import VendingMachine
+
+class CokeUnitTest: XCTestCase {
+
+    func testCokeInstantiate_success() {
+        XCTAssertNotNil(Coke(brand: "코카콜라", capacity: 250, price: 1000, name: "코카콜라", manufacturedDate: "20180101", calories: 100, hasIce: false))
+    }
+}
