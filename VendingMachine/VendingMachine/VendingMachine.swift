bb0d979711691843924f1e071d57494763a1b2db
Moon <myssun0325@gmail.com>
2018-06-09 20:30:30 +0900

    코드구현: 특정 음료를 자판기에 넣어 재고를 추가하는  메서드 작성

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -11,8 +11,13 @@ import Foundation
 struct VendingMachine {
     
     var insertedMoney: Int = 0
+    var beverageStock: [Beverage] = [Beverage]()
     
     mutating func insertMoney(_ money: Int) {
         self.insertedMoney += money
+    }
+    
+    mutating func addBeverageStock(_ beverage: Beverage) {
+        self.beverageStock.append(beverage)
     }
 }
