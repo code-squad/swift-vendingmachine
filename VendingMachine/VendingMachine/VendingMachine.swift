63759364c1cf018501680e2a5533333c770d37b5
Moon <myssun0325@gmail.com>
2018-06-09 20:51:40 +0900

    Beverage배열을 객체화 -> BeverageList

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -11,13 +11,26 @@ import Foundation
 struct VendingMachine {
     
     var insertedMoney: Int = 0
-    var beverageStock: [Beverage] = [Beverage]()
+    var beverageStock: BeverageList = BeverageList()
     
     mutating func insertMoney(_ money: Int) {
         self.insertedMoney += money
     }
     
     mutating func addBeverageStock(_ beverage: Beverage) {
-        self.beverageStock.append(beverage)
+        self.beverageStock.addBeverage(beverage)
+    }
+    
+    func showPurchasableBeverages() -> String {
+        return ""
+    }
+}
+
+struct BeverageList {
+    
+    private var beverageList: [Beverage] = [Beverage]()
+    
+    mutating func addBeverage(_ beverage: Beverage) {
+        self.beverageList.append(beverage)
     }
 }
