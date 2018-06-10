7243c17a1f266f6ef26a2a049f53586da2402b77
Moon <myssun0325@gmail.com>
2018-06-11 01:06:19 +0900

    실패테스트 작성
    자판기에 음료수를 추가하면 자판기의 재고가 변한다.

--- VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
+++ VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
@@ -35,4 +35,13 @@ class VendingMachineUnitTest: XCTestCase {
         let expected: BeverageStock = BeverageStock()
         XCTAssertEqual(vendingMachine.beverageStock, expected)
     }
+    
+    func testAddBeverage() {
+        let coke: Coke = Coke(brand: "코카콜라", capacity: 200, price: 800, name: "제로콜라", manufacturedDate: "20180101", calories: 0, hasIce: false)
+        var beverageList = BeverageList()
+        beverageList.beverageList.append(coke)
+        let expected = BeverageStock(beverageStock: [BeverageName.coke:beverageList])
+        vendingMachine.add(beverage: coke)
+        XCTAssertEqual(vendingMachine.beverageStock, expected)
+    }
 }
