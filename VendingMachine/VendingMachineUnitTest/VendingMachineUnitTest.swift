f51a31237aee42e5c895c0b514f5e08a66464bed
Moon <myssun0325@gmail.com>
2018-06-11 01:49:23 +0900

    테스트코드 버그 수정

--- VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
+++ VendingMachine/VendingMachineUnitTest/VendingMachineUnitTest.swift
@@ -40,7 +40,7 @@ class VendingMachineUnitTest: XCTestCase {
         let coke: Coke = Coke(brand: "코카콜라", capacity: 200, price: 800, name: "제로콜라", manufacturedDate: "20180101", calories: 0, hasIce: false)
         var beverageList = BeverageList()
         beverageList.beverageList.append(coke)
-        let expected = BeverageStock(beverageStock: [BeverageName.coke:beverageList])
+        let expected = BeverageStock(beverageStock: [BeverageName.Coke:beverageList])
         XCTAssertNoThrow(try vendingMachine.add(beverage: coke), "추가실패")
         XCTAssertEqual(vendingMachine.beverageStock, expected)
     }
@@ -48,12 +48,14 @@ class VendingMachineUnitTest: XCTestCase {
     func testAddBeverage_addMultiBeverages() {
         let coke: Coke = Coke(brand: "코카콜라", capacity: 200, price: 800, name: "제로콜라", manufacturedDate: "20180101", calories: 0, hasIce: false)
         let sprite: Sprite = Sprite(brand: "코카콜라", capacity: 200, price: 800, name: "스프라이트", manufacturedDate: "20180505", calories: 70, hasOneMoreOnCap: false)
-        var beverageList = BeverageList()
-        beverageList.beverageList.append(coke)
-        beverageList.beverageList.append(sprite)
-        let expected = BeverageStock(beverageStock: [BeverageName.coke:beverageList])
+        var cokeBeverageList = BeverageList()
+        var spriteBeverageList = BeverageList()
+        cokeBeverageList.beverageList.append(coke)
+        spriteBeverageList.beverageList.append(sprite)
+        var expected = BeverageStock(beverageStock: [BeverageName.Coke:cokeBeverageList])
+        expected.beverageStock[BeverageName.Sprite] = spriteBeverageList
         XCTAssertNoThrow(try vendingMachine.add(beverage: coke), "콜라 추가실패")
-        XCTAssertNoThrow(try vendingMachine.add(beverage: sprite), "사이다 추가실패") 
+        XCTAssertNoThrow(try vendingMachine.add(beverage: sprite), "사이다 추가실패")
         XCTAssertEqual(vendingMachine.beverageStock, expected)
     }
 }
