b248d80c64cef20d77c61362715b51fe42ca17b1
Moon <myssun0325@gmail.com>
2018-06-12 01:27:40 +0900

    ProductType enum 이동

---
+++ VendingMachine/VendingMachine/ProductType.swift
@@ -0,0 +1,19 @@
+//
+//  ProductType.swift
+//  VendingMachine
+//
+//  Created by moon on 2018. 6. 12..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+enum ProductType: String {
+    case Coke
+    case Sprite
+    case TOP
+    case Cantata
+    case Georgia
+    case StrawberryMilk
+    case ChocoMilk
+}
