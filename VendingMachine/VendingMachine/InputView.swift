26183f2f5fc18c90563d2461500133f8548fecd9
Moon <myssun0325@gmail.com>
2018-06-19 19:17:03 +0900

    자판기 시작 및 모드 입력 구현
    InputView에 모드를 입력받는 메서드 구현
    OutputView에 시작 메시지 출력 메서드 구현

--- VendingMachine/VendingMachine/InputView.swift
+++ VendingMachine/VendingMachine/InputView.swift
@@ -10,6 +10,17 @@ import Foundation
 
 struct InputView {
     
+    enum Error: Swift.Error {
+        case modeSelectError
+        
+        var errorMessage: String {
+            switch self {
+            case.modeSelectError:
+                return "올바른 모드를 선택해주세요."
+            }
+        }
+    }
+    
     static func selectMenu() -> (Int, Int) {
         let input = (readLine() ?? "").split(separator: " ").compactMap { Int($0) }
         return (input.first!, input.last!)
@@ -17,5 +28,21 @@ struct InputView {
     
     static func readCoin() -> Int {
         return Int(readLine()!) ?? 0
+    }
+    
+    static func selectMode() throws -> Mode {
+        while true {
+            let input = readLine() ?? ""
+            switch input {
+            case "1":
+                return .administrator
+            case "2":
+                return .user
+            case "q":
+                return .quit
+            default:
+                throw Error.modeSelectError
+            }
+        }
     }
 }
