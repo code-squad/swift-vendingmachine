4e9bfd02e0f27784685d40066b74202ac1772896
DrakeYang <drake.the.programmer@gmail.com>
2018-07-19 17:19:00 +0900

    음료,커피,탄산음료,우유 클래스 추가

--- VendingMachine/VendingMachine/Drink.swift
+++ VendingMachine/VendingMachine/Drink.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+class Drink {
+    let brand : String
+    let size : Int
+    let price : Int
+    let name : String
+    let manufacturingDate : Date
+    
+    init(barnd:String, size:Int,price:Int, name:String, manufacturingDate:Date) {
+        self.brand = barnd
+        self.size = size
+        self.price = price
+        self.name = name
+        self.manufacturingDate = manufacturingDate
+    }
+    
+    
+}
+
+class Milk : Drink {
+    enum Flavor {
+        case strawberry
+        case choco
+        case banana
+    }
+    
+    let flavor : Flavor
+    
+    init(flavor:Flavor,barnd:String, size:Int,price:Int, name:String, manufacturingDate:Date){
+        self.flavor = flavor
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+}
+
+class Soda : Drink {
+    enum Flavor{
+        case coke
+        case cider
+        case fanta
+    }
+    
+    let flavor : Flavor
+    
+    init(flavor:Flavor,barnd:String, size:Int,price:Int, name:String, manufacturingDate:Date){
+        self.flavor = flavor
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+}
+
+class Coffee : Drink{
+    enum Flavor {
+        case top
+        case cantata
+        case georgia
+    }
+    
+    let flavor : Flavor
+    
+    init(flavor:Flavor,barnd:String, size:Int,price:Int, name:String, manufacturingDate:Date){
+        self.flavor = flavor
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+}
+
+
+
