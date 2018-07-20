211490edd9182286b2035f9d5aa15e5384760145
DrakeYang <drake.the.programmer@gmail.com>
2018-07-20 11:42:31 +0900

    flavor 가 제거된 생성자에 맞게 수정.

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -10,17 +10,17 @@ import Foundation
 
 func main(){
     guard
-    let milk1 : CustomStringConvertible = Milk(flavor: Milk.Flavor.strawberry, barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171009"),
-    let milk2 : CustomStringConvertible = Milk(flavor: Milk.Flavor.strawberry, barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171012"),
-    let soda1 : CustomStringConvertible = Soda(flavor: Soda.Flavor.coke, barnd: "팹시", size: 350, price: 2000, name: "다이어트콜라", manufacturingDate: "20171005"),
-        let coffe1 : CustomStringConvertible = Coffee(flavor: Coffee.Flavor.top, barnd: "맥심", size: 400, price: 3000, name: "TOP아메리카노", manufacturingDate: "20171010") else {
+    let milk1  = ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171009"),
+    let milk2 = ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171012"),
+    let soda1 = Coke(barnd: "팹시", size: 350, price: 2000, name: "다이어트콜라", manufacturingDate: "20171005"),
+    let coffe1 = TopCoffee(barnd: "맥심", size: 400, price: 3000, name: "TOP아메리카노", manufacturingDate: "20171010") else {
             return 
     }
     
-    print(milk1.getDetail())
-    print(milk2.getDetail())
-    print(soda1.getDetail())
-    print(coffe1.getDetail())
+    print(milk1)
+    print(milk2)
+    print(soda1)
+    print(coffe1)
     
 }
 
