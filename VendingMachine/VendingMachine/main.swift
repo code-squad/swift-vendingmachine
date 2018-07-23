26d27845e3b9d69fe8ccbd23abade193846fe1bb
DrakeYang <drake.the.programmer@gmail.com>
2018-07-23 17:16:12 +0900

    에너지드링크 변수명 변경 적용 : 노카페인 -> 제로카페인

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -10,19 +10,23 @@ import Foundation
 
 func main(){
     guard
-    let milk1  = ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171009"),
-    let milk2 = ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171012"),
-    let soda1 = Coke(barnd: "팹시", size: 350, price: 2000, name: "다이어트콜라", manufacturingDate: "20171005"),
-    let coffe1 = TopCoffee(barnd: "맥심", size: 400, price: 3000, name: "TOP아메리카노", manufacturingDate: "20171010") else {
-            return 
+        let chocoMilk  = ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171009", lowFat: true, lowSugar: true)
+            ,
+        let hot6 = EnergyDrink(
+            barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171012", zeroCaffeine: true),
+            let coke = Coke(barnd: "팹시", size: 350, price: 2000, name: "다이어트콜라", manufacturingDate: "20171005", PET: false, zeroCalorie: false),
+            let topCoffee = TopCoffee(barnd: "맥심", size: 400, price: 3000, name: "TOP아메리카노", manufacturingDate: "20171010", hot: false, zeroSugar: false)
+        else {
+            return ()
     }
     
-    print(milk1)
-    print(milk2)
-    print(soda1)
-    print(coffe1)
+    print(chocoMilk)
+        print(hot6)
+        print(coke)
+        print(topCoffee)
     
 }
 
 
 main()
+
