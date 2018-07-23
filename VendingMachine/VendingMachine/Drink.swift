d027e04d21ed1ce9655a80b7cf94476875fb59c3
DrakeYang <drake.the.programmer@gmail.com>
2018-07-23 15:41:55 +0900

    각 하위 클래스에 추자로 속성 생성. 우유 클래스 : 무지방, 초코우유 : 저과당, 탄산클래스 : 페트병, 콜라 : 무칼로리, 커피:핫, TOP:무설탕,에너지음료:무카페인

--- VendingMachine/VendingMachine/Drink.swift
+++ VendingMachine/VendingMachine/Drink.swift
@@ -54,22 +54,111 @@ class Drink : CustomStringConvertible {
     }
 }
 
+/// lowFat 체크 프로토콜
+protocol CheckLowFat {
+    func isLowFat() -> Bool
+}
 /// 우유 클래스
-class Milk : Drink {
-}
-class ChocoMilk : Milk {
+class Milk : Drink, CheckLowFat {
+    private let lowFat : Bool
+    
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDate: String, lowFat: Bool) {
+        self.lowFat = lowFat
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+    func isLowFat() -> Bool {
+        return self.lowFat
+    }
 }
 
+/// lowSugar 체크 프로토콜
+protocol CheckLowSugar {
+    func isLowSugar()->Bool
+}
+class ChocoMilk : Milk , CheckLowSugar{
+    let lowSugar : Bool
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDate: String, lowFat: Bool, lowSugar:Bool) {
+        self.lowSugar = lowSugar
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate, lowFat: lowFat)
+    }
+    func isLowSugar() -> Bool {
+        return self.lowSugar
+    }
+}
+/// isPET 체크 프로토콜
+protocol CheckPET {
+    func isPET()->Bool
+}
 /// 탄산음료 클래스
-class Soda : Drink {
+class Soda : Drink, CheckPET {
+    private let PET : Bool
+    
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDate: String, PET: Bool) {
+        self.PET = PET
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+    func isPET() -> Bool {
+        return self.PET
+    }
 }
-class Coke : Soda {
+/// zeroCalorie 체크 프로토콜
+protocol CheckZeroCalorie {
+    func isZeroCalorie()->Bool
+}
+class Coke : Soda, CheckZeroCalorie {
+    private let zeroCalorie : Bool
+    
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDate: String, PET: Bool,zeroCalorie:Bool) {
+        self.zeroCalorie = zeroCalorie
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate, PET: PET)
+    }
+    func isZeroCalorie() -> Bool {
+        return self.zeroCalorie
+    }
 }
 
+/// hot 체크 프로토콜
+protocol CheckHot {
+    func isHot()->Bool
+}
 /// 커피 클래스
-class Coffee : Drink{
+class Coffee : Drink, CheckHot{
+    private let hot : Bool
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDate: String, hot: Bool) {
+        self.hot = hot
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+    func isHot() -> Bool {
+        return self.hot
+    }
 }
-class TopCoffee : Coffee{
+/// zeroSugar 체크 프로토콜
+protocol CheckZeroSugar {
+    func isZeroSugar()->Bool
 }
-
+class TopCoffee : Coffee, CheckZeroSugar{
+    private let zeroSugar : Bool
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDate: String,hot: Bool, zeroSugar: Bool) {
+        self.zeroSugar = zeroSugar
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate,hot: hot)
+    }
+    func isZeroSugar() -> Bool {
+        return self.zeroSugar
+    }
+}
+/// noCaffeine 체크 프로토콜
+protocol CheckNoCaffeine {
+    func isNoCaffeine()->Bool
+}
+/// 에너지 드링크 클래스
+class EnergyDrink : Drink, CheckNoCaffeine {
+    private let noCaffeine : Bool
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDate: String, noCaffeine: Bool) {
+        self.noCaffeine = noCaffeine
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+    func isNoCaffeine() -> Bool {
+        return self.noCaffeine
+    }
+}
 
