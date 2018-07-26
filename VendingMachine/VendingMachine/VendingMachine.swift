6841730b4be1cc63cecde81bf0076b22cfb61647
DrakeYang <drake.the.programmer@gmail.com>
2018-07-26 17:18:17 +0900

    음료별 재고 배열 추가, 배열에 음료 추가하는 함수 추가

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 class VendingMachine {
+    /// init
+    init(){}
+    
     /// 자판기에 들어있는 금액
     private var insertedMoney = 0
     
@@ -21,5 +24,68 @@ class VendingMachine {
         self.insertedMoney -= money
     }
     
+  
     
+    /// 초코우유 재고
+    private var lowSugarChocoMilkInventory : [ChocoMilk] = []
+    private var chocoMilkInventory : [ChocoMilk] = []
+    
+    /// 콜라 재고
+    private var cokeInventory : [Coke] = []
+    private var zeroCalorieCokeInventory : [Coke] = []
+    
+    /// 커피 재고
+    private var hotTopCoffeeInventory : [TopCoffee] = []
+    
+    /// 에너지드링크 재고
+    private var energyDrinkInventory : [EnergyDrink] = []
+    
+    // 재고 추가 함수
+    /// 저과당 우유인지 아닌지 분류 후 추가
+    func addChocoMilk(chocoMilk: ChocoMilk){
+        if chocoMilk.isLowSugar() {
+            lowSugarChocoMilkInventory.append(chocoMilk)
+        } else {
+            chocoMilkInventory.append(chocoMilk)
+        }
+    }
+    /// 제로칼로리 콜라인지 아닌지 분류 후 추가
+    func addCoke(coke: Coke) {
+        if coke.isZeroCalorie() {
+            zeroCalorieCokeInventory.append(coke)
+        } else {
+            cokeInventory.append(coke)
+        }
+    }
+    /// 무과당 Top 커피인지 체크 후 추가
+    func addTopCoffee(topCoffee:TopCoffee)->()?{
+        if topCoffee.isZeroSugar() {
+            hotTopCoffeeInventory.append(topCoffee)
+            return ()
+        } else {
+            return nil
+        }
+    }
+    /// 디카페인인지 체크 후 추가
+    func addEnergyDrink(energyDrink: EnergyDrink)->()?{
+        if energyDrink.isNoCaffeine() {
+            return nil
+        } else {
+            energyDrinkInventory.append(energyDrink)
+        }
+        return ()
+    }
+    
+    /// 음료수 객체를 받아서 추가
+    func addInventory(drink:Any)->()?{
+        switch drink {
+        case is ChocoMilk : addChocoMilk(chocoMilk: drink as! ChocoMilk)
+        case is Coke : addCoke(coke: drink as! Coke)
+        case is TopCoffee : addTopCoffee(topCoffee: drink as! TopCoffee)
+        case is EnergyDrink : addEnergyDrink(energyDrink: drink as! EnergyDrink)
+        default : return nil
+        }
+        return ()
+    }
 }
+
