2186925cc2bbc42bd96105f962c50f4126fb465d
DrakeYang <drake.the.programmer@gmail.com>
2018-08-09 21:05:02 +0900

    음료수 생성 및 생성된 음료 자판기로 추가

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,21 +9,30 @@
 import Foundation
 
 func main(){
+    // 자판기 생성
+    var vendingMachine = VendingMachine()
+    
+    // 음료수 생성
     guard
-        let chocoMilk  = ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171009", lowFat: true, lowSugar: true)
-            ,
+        let lowSugarChocoMilk  = ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "저과당초코우유", manufacturingDate: "20171009", lowFat: true, lowSugar: true)
+        ,
         let hot6 = EnergyDrink(
-            barnd: "서울우유", size: 200, price: 1000, name: "날마다딸기우유", manufacturingDate: "20171012", zeroCaffeine: true),
-            let coke = Coke(barnd: "팹시", size: 350, price: 2000, name: "다이어트콜라", manufacturingDate: "20171005", usingPET: false, zeroCalorie: false),
-            let topCoffee = TopCoffee(barnd: "맥심", size: 400, price: 3000, name: "TOP아메리카노", manufacturingDate: "20171010", hot: false, zeroSugar: false)
+            barnd: "핫식스", size: 200, price: 1000, name: "핫식스", manufacturingDate: "20171012", zeroCaffeine: false),
+        let coke = Coke(barnd: "팹시", size: 350, price: 2000, name: "다이어트콜라", manufacturingDate: "20171005", usingPET: false, zeroCalorie: false),
+        let topCoffee = TopCoffee(barnd: "맥심", size: 400, price: 3000, name: "TOP아메리카노", manufacturingDate: "20171010", hot: false, zeroSugar: false),
+        let chocoMilk  = ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "그냥초코우유", manufacturingDate: "20171009", lowFat: true, lowSugar: false)
         else {
             return ()
     }
+    // 음료수 추가
+    vendingMachine.addDrink(drink: lowSugarChocoMilk)
+    vendingMachine.addDrink(drink: hot6)
+    vendingMachine.addDrink(drink: coke)
+    vendingMachine.addDrink(drink: topCoffee)
+    vendingMachine.addDrink(drink: chocoMilk)
     
-    print(chocoMilk)
-        print(hot6)
-        print(coke)
-        print(topCoffee)
+    
+    
     
 }
 
