e6a4583426dfe19249ed0c6815a5392aefe4d636
DrakeYang <drake.the.programmer@gmail.com>
2018-08-10 14:26:06 +0900

    문자열을 받아서 양의 정수로 치환가능한지 체크하는 함수 추가

--- VendingMachine/VendingMachine/Checker.swift
+++ VendingMachine/VendingMachine/Checker.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct Checker {
+    
+    /// 문자열을 받아서 양의 정수로 치환 가능한지 체크. 가능하면 치환, 안되면 닐 리턴
+    static func checkChangePlusInt(text:String)->Int?{
+        guard let number = Int(text) else {
+            return nil
+        }
+        guard number > 0 else {
+            return nil
+        }
+        return number
+    }
+}
