5e873fcfbd5e4738e0d14ab70b0ed27e37dc25b0
DrakeYang <drake.the.programmer@gmail.com>
2018-08-12 16:23:44 +0900

    문자열을 받아서 숫자 1~maxDrinkNumber 사이인지 체크하는 함수 추가

--- VendingMachine/VendingMachine/Checker.swift
+++ VendingMachine/VendingMachine/Checker.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 struct Checker {
+    /// 제공하는 음료의 종류 가지수
+    static let maxDrinkNumber = 6
     
     /// 문자열을 받아서 양의 정수로 치환 가능한지 체크. 가능하면 치환, 안되면 닐 리턴
     static func checkChangePlusInt(_ text:String)->Int?{
@@ -20,4 +22,13 @@ struct Checker {
         }
         return number
     }
+    
+    /// 문자열을 받아서 숫자 1~maxDrinkNumber 사이인지 체크
+    static func isRightDrinkNumber(_ text: String)->Int?{
+        guard let number = Int(text), number > 0, number <= Checker.maxDrinkNumber else {
+            return nil
+        }
+        return number
+    }
+    
 }
