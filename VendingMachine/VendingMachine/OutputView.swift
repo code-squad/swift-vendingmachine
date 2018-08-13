1d1714eec3d094cd0824c340df8d322634269fba
DrakeYang <drake.the.programmer@gmail.com>
2018-08-13 14:40:51 +0900

    OutputView 의 메세지출력 함수들 print -> return 으로 수정

--- VendingMachine/VendingMachine/OutputView.swift
+++ VendingMachine/VendingMachine/OutputView.swift
@@ -10,62 +10,66 @@ import Foundation
 
 struct OutputView {
     /// 시작 화면
-    func mainMenu(){
-        print("자판기 메인메뉴 입니다.")
+    func mainMenu()->String{
+        return("자판기 메인메뉴 입니다.")
     }
     /// 자판기 금액을 리턴한다
-    func printMoney(money:Int){
-        print("현재 자판기에 입력된 금액은 \(money)원 입니다.")
+    func returnMoney(money:Int)->String{
+        return("현재 자판기에 입력된 금액은 \(money)원 입니다.")
     }
     
     /// 재고 출력 메세지
-    func printGettableDrink(drinks:[InventoryDetail?]){
+    func returnGettableDrink(drinks:[InventoryDetail?])->String{
+        // 결과출력을 위한 변수
+        var result = ""
         // 메뉴번호를 출력하기 위한 변수
         var menuNumber = 1
-        print("---현재 구매가능한 음료수---")
+        result += ("---현재 구매가능한 음료수---\n")
         for drink in drinks {
             if drink != nil {
-            print("\(menuNumber). \(drink!.getDrinkName())-\(drink!.getDrinkPrice())원-\(drink!.getDrinkCount())개")
+            result += ("\(menuNumber). \(drink!.getDrinkName())-\(drink!.getDrinkPrice())원-\(drink!.getDrinkCount())개")
             }
             else {
-                print("\(menuNumber). 재고 없음")
+                result += ("\(menuNumber). 재고 없음")
             }
-            menuNumber += 1 
+            menuNumber += 1
+            result += "\n"
         }
-        print("----------------------")
+        result += ("----------------------")
+        return result
     }
     
     /// 잘못된 금액 입력시 출력메세지
-    func wrongMoney(){
-        print( "잘못된 금액입니다.")
+    func wrongMoney()->String{
+        return( "잘못된 금액입니다.")
     }
     
     /// 잘못된 메뉴 선택시 출력메세지
-    func wrongMenu(){
-        print("잘못된 메뉴입니다")
+    func wrongMenu()->String{
+        return("잘못된 메뉴입니다")
     }
     
     /// 종료시 출력메세지
-    func quitMessage(){
-        print("자판기를 종료합니다.")
+    func quitMessage()->String{
+        return("자판기를 종료합니다.")
     }
     
     /// 재고가 없는 물건 주문시 출력메세지
-    func notEnoughDrink(){
-        print("음료 재고가 없습니다.")
+    func notEnoughDrink()->String{
+        return("음료 재고가 없습니다.")
     }
     
     /// 숫자를 입력해야되는데 숫자가 아닌경우
-    func notNumeric(){
-        print("정수가 아닙니다.")
+    func notNumeric()->String{
+        return("정수가 아닙니다.")
     }
     
     /// 음료수 구매성공 메세지
-    func buyingSuccessMessage(dirnkName:String,drinkCount:Int,drinkPirce:Int){
-        print("\(dirnkName) \(drinkCount)개를 \(drinkPirce)원에 구매하였습니다.")
+    func buyingSuccessMessage(dirnkName:String,drinkCount:Int,drinkPirce:Int)->String{
+        return("\(dirnkName) \(drinkCount)개를 \(drinkPirce)원에 구매하였습니다.")
     }
     /// 주문금액이 잔고보다 클 경우
-    func notEnoughMoney(){
-        print("잔액이 부족합니다.")
+    func notEnoughMoney()->String{
+        return("잔액이 부족합니다.")
     }
 }
