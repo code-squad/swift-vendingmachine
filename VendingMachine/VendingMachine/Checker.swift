2edffce6c4b90561e73ba5f5c85babddf0bb92b4
DrakeYang <drake.the.programmer@gmail.com>
2018-08-17 20:40:43 +0900

    maxDrinkCount 삭제. isRightDrinkNumber 함수 수정 : 입력인자에 InventoryDetail 추가. 사용자 입력값에 재고개수와 직접비교

--- VendingMachine/VendingMachine/Checker.swift
+++ VendingMachine/VendingMachine/Checker.swift
@@ -9,24 +9,19 @@
 import Foundation
 
 struct Checker {
-    /// 제공하는 음료의 종류 가지수
-    static let maxDrinkNumber = 6
     
     /// 문자열을 받아서 양의 정수로 치환 가능한지 체크. 가능하면 치환, 안되면 닐 리턴
     static func checkChangePlusInt(_ text:String)->Int?{
-        guard let number = Int(text) else {
-            return nil
-        }
-        guard number > 0 else {
+        guard let number = Int(text),number > 0 else {
             return nil
         }
         return number
     }
     
     /// 문자열을 받아서 숫자 1~maxDrinkNumber 사이인지 체크
-    static func isRightDrinkNumber(_ text: String)->Int?{
-        guard let number = Int(text), number > 0, number <= Checker.maxDrinkNumber else {
-            return nil
+    static func isRightDrinkNumber(_ text: String,inventoryDetail:InventoryDetail)throws->Int{
+        guard let number = Int(text), number > 0, number <= inventoryDetail.storedDrinksDetail.count+1 else {
+            throw OutputView.errorMessage.wrongDrink
         }
         return number
     }
