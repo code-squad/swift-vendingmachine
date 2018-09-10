e1b4ce80db0ee72585b0d1e800b25d52f4431f83
DrakeYang <drake.the.programmer@gmail.com>
2018-09-10 17:38:55 +0900

    제네릭 적용된 음료슬롯 수정부분 적용. 음료슬롯을 배열로 처리하도록 변경. 스위치 함수 제거

--- VendingMachine/VendingMachine/DrinkInventory.swift
+++ VendingMachine/VendingMachine/DrinkInventory.swift
@@ -1,5 +1,5 @@
 //
-//  DrinkInventory.swift
+//  swift
 //  VendingMachine
 //
 //  Created by Yoda Codd on 2018. 8. 10..
@@ -18,31 +18,30 @@ protocol OrderedDrinkInventory {
 class DrinkInventory {
     /// init
     init(){
+        self.drinkSlots.append(DrinkSlot(drinkType: DrinkType.chocoMilk))
+        self.drinkSlots.append(DrinkSlot(drinkType: DrinkType.lowSugarChocoMilk))
+        self.drinkSlots.append(DrinkSlot(drinkType: DrinkType.coke))
+        self.drinkSlots.append(DrinkSlot(drinkType: DrinkType.zeroCalorieCoke))
+        self.drinkSlots.append(DrinkSlot(drinkType: DrinkType.hotTopCoffee))
+        self.drinkSlots.append(DrinkSlot(drinkType: DrinkType.energyDrink))
     }
-    /// 음료 종류
-    enum DrinkType{
-        case
-        lowSugarChocoMilk
-        ,chocoMilk
-        ,coke
-        ,zeroCalorieCoke
-        ,hotTopCoffee
-        ,energyDrink
-        ,none
-    }
+    
+    /// 슬롯들을 모아놓는 변수 선언
+    var drinkSlots : [DrinkSlot<Drink>] = []
+    
     /// 초코우유 재고
-    private let lowSugarChocoMilkInventory = DrinkSlot(drinkType: DrinkInventory.DrinkType.lowSugarChocoMilk)
-    private let chocoMilkInventory = DrinkSlot(drinkType: DrinkInventory.DrinkType.chocoMilk)
+    private let lowSugarChocoMilkInventory = DrinkSlot(drinkType: DrinkType.lowSugarChocoMilk)
+    private let chocoMilkInventory = DrinkSlot(drinkType: DrinkType.chocoMilk)
     
     /// 콜라 재고
-    private let cokeInventory = DrinkSlot(drinkType: DrinkInventory.DrinkType.coke)
-    private let zeroCalorieCokeInventory = DrinkSlot(drinkType: DrinkInventory.DrinkType.zeroCalorieCoke)
+    private let cokeInventory = DrinkSlot(drinkType: DrinkType.coke)
+    private let zeroCalorieCokeInventory = DrinkSlot(drinkType: DrinkType.zeroCalorieCoke)
     
     /// 커피 재고
-    private let hotTopCoffeeInventory = DrinkSlot(drinkType: DrinkInventory.DrinkType.hotTopCoffee)
+    private let hotTopCoffeeInventory = DrinkSlot(drinkType: DrinkType.hotTopCoffee)
     
     /// 에너지드링크 재고
-    private let energyDrinkInventory = DrinkSlot(drinkType: DrinkInventory.DrinkType.energyDrink)
+    private let energyDrinkInventory = DrinkSlot(drinkType: DrinkType.energyDrink)
     
     /// 음료객체를 받아서 재고정보로 출력
     func getDrinkDetail(drink: Drink)-> StoredDrinkDetail?{
@@ -50,71 +49,66 @@ class DrinkInventory {
         return inventoryDetail
     }
     
-    /// 입력받은 타입의 음료를 복사해서 1개 추가한다
-    private func duplicate(drinkType:DrinkType)throws{
-        switch drinkType {
-        case .chocoMilk : try chocoMilkInventory.duplicate()
-        case .lowSugarChocoMilk : try lowSugarChocoMilkInventory.duplicate()
-        case .coke : try cokeInventory.duplicate()
-        case .zeroCalorieCoke : try zeroCalorieCokeInventory.duplicate()
-        case .hotTopCoffee : try hotTopCoffeeInventory.duplicate()
-        case .energyDrink : try energyDrinkInventory.duplicate()
-        case .none : throw OutputView.errorMessage.wrongDrink
-        }
-    }
-    
     /// 해당타입의 음료를 여러번 추가한다
     func addDrinkSelfDuplicate(drinkType:DrinkType,drinkCount:Int)throws{
-        for _ in 1...drinkCount {
-            try duplicate(drinkType: drinkType)
+        // 음료슬롯 전부를 체크한다
+        for drinkSlot in drinkSlots {
+            // 음료슬롯의 음료타입이 맞으면
+            if drinkSlot.drinkType == drinkType {
+                // 입력개수 만큼 증가
+                for _ in 1...drinkCount {
+                    try drinkSlot.duplicate()
+                }
+                // 증가 후 반복문 종료
+                break
+            }
         }
+        // 만약 모든 음료슬롯에 해당 타입이 없다면 잘못된음료 에러 리턴
+        throw OutputView.errorMessage.wrongDrink
     }
     
     /// 음료수 객체를 받아서 추가
     func addInventory(undefinedDrink:Drink)throws->StoredDrinkDetail?{
-        switch undefinedDrink.drinkType {
-        case .chocoMilk : try chocoMilkInventory.addDrink(drink: undefinedDrink)
-        case .lowSugarChocoMilk : try lowSugarChocoMilkInventory.addDrink(drink: undefinedDrink)
-        case .coke : try cokeInventory.addDrink(drink: undefinedDrink)
-        case .zeroCalorieCoke : try zeroCalorieCokeInventory.addDrink(drink: undefinedDrink)
-        case .hotTopCoffee : try hotTopCoffeeInventory.addDrink(drink: undefinedDrink)
-        case .energyDrink : try energyDrinkInventory.addDrink(drink: undefinedDrink)
-        // 추가할수 없는 종류의 경우
-        case .none : return nil
+        // 음료슬롯 전부를 체크한다
+        for drinkSlot in drinkSlots {
+            // 음료슬롯의 음료타입이 맞으면
+            if drinkSlot.drinkType == undefinedDrink.drinkType {
+                // 입력개수 만큼 증가
+                try drinkSlot.addDrink(drink: undefinedDrink)
+                // 증가 후 반복문 종료
+                return StoredDrinkDetail(drink:undefinedDrink ,drinkCount: 1)
+            }
         }
-        return StoredDrinkDetail(drink:undefinedDrink ,drinkCount: 1)
+        // 만약 모든 음료슬롯에 해당 타입이 없다면 잘못된음료 에러 리턴
+        return nil
     }
     
     /// 전체 재고 출력 함수
     func getTotalDrinkDetail()->InventoryDetail{
         // 결과 출력용 변수
         var result = InventoryDetail()
-        result.takeDrinkDetail(drinkInventory: lowSugarChocoMilkInventory.getDrinkDetail())
-        result.takeDrinkDetail(drinkInventory: chocoMilkInventory.getDrinkDetail())
-        result.takeDrinkDetail(drinkInventory: cokeInventory.getDrinkDetail())
-        result.takeDrinkDetail(drinkInventory: zeroCalorieCokeInventory.getDrinkDetail())
-        result.takeDrinkDetail(drinkInventory: hotTopCoffeeInventory.getDrinkDetail())
-        result.takeDrinkDetail(drinkInventory: energyDrinkInventory.getDrinkDetail())
+        // 음료슬롯 전부 반복
+        for drinkSlot in drinkSlots {
+            // 각 음료슬롯의 정보를 추가
+            result.takeDrinkDetail(drinkInventory: drinkSlot.getDrinkDetail())
+        }
+        // 결과 리턴
         return result
     }
     
-    /// 음료 출력
-    private func popOneDrink(drinkType:DrinkType)throws->Drink{
-        switch drinkType {
-        case .chocoMilk : return try chocoMilkInventory.popDrink()
-        case .lowSugarChocoMilk : return try lowSugarChocoMilkInventory.popDrink()
-        case .coke : return try cokeInventory.popDrink()
-        case .zeroCalorieCoke : return try zeroCalorieCokeInventory.popDrink()
-        case .hotTopCoffee : return try hotTopCoffeeInventory.popDrink()
-        case .energyDrink : return try energyDrinkInventory.popDrink()
-        case .none : throw OutputView.errorMessage.wrongDrink
-        }
-    }
-    /// 음료 여러개 출력
-    func popDrinks(drinkType:DrinkType,drinkCount:Int)throws->DrinkSlot{
+    /// 음료 여러개 추출
+    func popDrinks(drinkType:DrinkType,drinkCount:Int)throws->DrinkSlot<Drink>{
+        // 결과 출력용 음료슬롯 생성
         let drinks = DrinkSlot(drinkType: drinkType)
-        for _ in 1...drinkCount {
-            try drinks.addDrink(drink: popOneDrink(drinkType: drinkType))
+        // 음료슬롯 전부를 체크한다
+        for drinkSlot in drinkSlots {
+            // 음료슬롯의 음료타입이 맞으면
+            if drinkSlot.drinkType == drinkType {
+                // 입력개수 만큼 기존 음료슬롯에서 음료를 빼서 결과변수에 추가
+                for _ in 1...drinkCount {
+                    try drinks.addDrink(drink: drinkSlot.popDrink())
+                }
+            }
         }
         return drinks
     }    
