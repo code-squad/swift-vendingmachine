69b5ddb465f06ac583cdfc665ea169aa0d6f482e
oingbong <oingbong@gmail.com>
2018-09-19 17:33:48 +0900

    Divided func in main

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -10,6 +10,11 @@ import Foundation
 
 struct VendingMachine {
     static func run(){
+        let inventory = VendingMachine.inventory()
+        _ = VendingMachine.information(beverage: inventory)
+    }
+    
+    static func inventory() -> [Beverage] {
         let strawBerryDate = Date(timeIntervalSinceReferenceDate: 545000000)
         let cokeDate = Date(timeIntervalSinceReferenceDate: 545500000)
         let topCoffeeDate = Date(timeIntervalSinceReferenceDate: 545850000)
@@ -18,9 +23,18 @@ struct VendingMachine {
         let coke = Soda(sodium: 120, brand: "펩시", capacity: 350, price: 1500, name: "다이어트콜라", dateOfManufacture: cokeDate, manufacturer: "펩시")
         let topCoffee = Coffee(caffeine: 300, brand: "맥심", capacity: 500, price: 2000, name: "TOP커피", dateOfManufacture: topCoffeeDate, manufacturer: "맥심")
         
-        print(strawberryMilk.description)
-        print(coke.description)
-        print(topCoffee.description)
+        var beverage = [Beverage]()
+        beverage.append(strawberryMilk)
+        beverage.append(coke)
+        beverage.append(topCoffee)
+        
+        return beverage
+    }
+    
+    static func information(beverage : [Beverage]){
+        for item in beverage {
+            print(item.description)
+        }
     }
 }
 
