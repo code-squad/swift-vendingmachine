801c257f50f0d3e75713e273ff7f68cb8b410775
oingbong <oingbong@gmail.com>
2018-09-20 14:37:53 +0900

    Add className print

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -19,15 +19,14 @@ struct VendingMachine {
         let cokeDate = Date(timeIntervalSinceNow: -convertSeconds(15))
         let topCoffeeDate = Date(timeIntervalSinceNow: -convertSeconds(20))
         
-        let strawberryMilk = Milk(fat: 0.2, brand: "매일우유", capacity: 125, price: 1200, name: "상하목장유기농딸기우유", dateOfManufacture: strawBerryDate, manufacturer: "매일우유")
-        let coke = Soda(sodium: 120, brand: "펩시", capacity: 350, price: 1500, name: "다이어트콜라", dateOfManufacture: cokeDate, manufacturer: "펩시")
-        let topCoffee = Coffee(caffeine: 300, brand: "맥심", capacity: 500, price: 2000, name: "TOP커피", dateOfManufacture: topCoffeeDate, manufacturer: "맥심")
-        
+        let strawberryMilk = StrawberryMilk(flavor: "light", fat: 0.2, brand: "매일우유", capacity: 125, price: 1200, name: "유기농딸기우유", dateOfManufacture: strawBerryDate, manufacturer: "매일우유")
+        let coke = Coke(calorie: 300, sodium: 120, brand: "코카콜라", capacity: 350, price: 1500, name: "다이어트콜라", dateOfManufacture: cokeDate, manufacturer: "펩시")
+        let topCoffee = TOP(hot: true, caffeine: 400, brand: "맥심", capacity: 500, price: 2000, name: "TOP커피", dateOfManufacture: topCoffeeDate, manufacturer: "맥심")
+
         var beverage = [Beverage]()
         beverage.append(strawberryMilk)
         beverage.append(coke)
         beverage.append(topCoffee)
-        
         return beverage
     }
     
