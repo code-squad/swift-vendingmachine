a506bd07c7ba7ba50bb41bcf44381d2cf4c584e9
oingbong <oingbong@gmail.com>
2018-09-23 16:00:21 +0900

    Divide main logic with add Inventory class

---
+++ VendingMachine/VendingMachine/Inventory.swift
@@ -0,0 +1,32 @@
+//
+//  Inventory.swift
+//  VendingMachine
+//
+//  Created by oingbong on 2018. 9. 23..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Inventory {
+    private var beverages: [[Beverage]]
+    
+    init() {
+        var beverages = [[Beverage]]()
+        let numberOfBeverageType = Int.random(in: 4...8)
+        for _ in 1...numberOfBeverageType {
+            let numberPerItem = Int.random(in: 1...9)
+            let beverage = RandomBeverage.random(select: numberPerItem)
+            beverages.append(beverage)
+        }
+        self.beverages = beverages
+    }
+    
+    public func inventoryInformation() {
+        for beverages in self.beverages {
+            for beverage in beverages {
+                print(beverage.description)
+            }
+        }
+    }
+}
--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,31 +9,9 @@
 import Foundation
 
 struct VendingMachine {
-    static var inventory = [[Beverage]]()
-    
     static func run(){
-        inventory = VendingMachine.initInventory()
-        _ = VendingMachine.inventoryInformation()
-    }
-    
-    static func initInventory() -> [[Beverage]] {
-        var beverages = [[Beverage]]()
-        let numberOfBeverageType = Int.random(in: 4...8)
-        for _ in 1...numberOfBeverageType {
-            let numberPerItem = Int.random(in: 1...9)
-            let beverage = RandomBeverage.random(select: numberPerItem)
-            beverages.append(beverage)
-        }
-        
-        return beverages
-    }
-    
-    static func inventoryInformation(){
-        for beverageType in self.inventory {
-            for beverage in beverageType {
-                print(beverage.description)
-            }
-        }
+        let inventory = Inventory.init()
+        inventory.inventoryInformation()
     }
 }
 
