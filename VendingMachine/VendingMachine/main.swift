ef751b8298aa43e4eacd34fad099686b48264a58
oingbong <oingbong@gmail.com>
2018-09-23 22:21:40 +0900

    Edit charge in Menu

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -13,19 +13,21 @@ struct VendingMachine {
     var customer = Customer.sharedBalance
     
     func run() throws {
-        // 메뉴 출력
-        // 1. 잔액 2. 재고 3. 선택목록 리스트
-        let balance = customer.presentBalance()
-        OutputView.printPresentBalance(with: balance)
-        let list = self.inventory.list()
-        OutputView.printInventoryList(with: list)
-        OutputView.printSelectMenu()
-        
-        // 입력값 받기
-        let (type, value) = try InputView.selectMenuType()
-        
-        // 메뉴 선택
-        Menu.select(type: type, with: value)
+        while true {
+            // 메뉴 출력
+            // 1. 잔액 2. 재고 3. 선택목록 리스트
+            let balance = customer.presentBalance()
+            OutputView.printPresentBalance(with: balance)
+            let list = self.inventory.list()
+            OutputView.printInventoryList(with: list)
+            OutputView.printSelectMenu()
+            
+            // 입력값 받기
+            let (type, value) = try InputView.selectMenuType()
+            
+            // 메뉴 선택
+            Menu.select(type: type, with: value)
+        }
     }
 }
 
