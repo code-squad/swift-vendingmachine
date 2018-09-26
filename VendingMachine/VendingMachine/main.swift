6202e703c90193f41c0343dcbfd3b1978a93e4a8
oingbong <oingbong@gmail.com>
2018-09-27 00:11:43 +0900

    Add Stock struct & Divide object , func for UnitTest

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,29 +9,35 @@
 import Foundation
 
 struct Main {
-    var inventory = Inventory.shared
-    var customer = Customer.shared
-    
     public func run() throws {
         while true {
+            let inventory = Inventory.shared
+            let customer = Customer.shared
+            
             // 출력 : 1. 잔액 2. 재고 3. 선택 메뉴
             let balance = customer.presentBalance()
-            let list = self.inventory.list()
+            let list = inventory.list()
             OutputView.printPresentBalance(with: balance)
-            OutputView.printInventoryList(with: list)
+            try OutputView.printInventoryList(with: list)
             OutputView.printSelectMenu()
             
             // 입력값 받기
-            let menuType = try InputView.selectMenuType()
+            let (menuType, value) = try InputView.selectMenuType()
             
             // 메뉴에 따른 실행
             let vendingMachine = VendingMachine()
-            try vendingMachine.excute(with: menuType)
+            try vendingMachine.excute(with: menuType, value: value)
         }
     }
 }
 
 do {
+    // 재고 추가
+    let stock = Stock.prepareStock()
+    let inventory = Inventory.shared
+    inventory.stockUp(with: stock)
+    
+    // 시작
     let main = Main()
     try main.run()
 } catch let error as Errorable {
