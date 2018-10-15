a8892606201738b01a7c537885dba0418d79220c
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-15 13:55:37 +0900

    CustomStringConvertible -> NSObject

--- VendingMachine/VendingMachine/Beverage.swift
+++ VendingMachine/VendingMachine/Beverage.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Beverage: CustomStringConvertible {
+class Beverage: NSObject {
     private var brand: String
     private var volume: Int
     private var price: Int
@@ -20,8 +20,9 @@ class Beverage: CustomStringConvertible {
         return formatter.string(from: date)
     }
     
-    var description: String {
-        return "\(brand), \(volume)ml, \(price)원, \(name), \(displayableDate)"
+    override var description: String {
+        let displayable = convert()
+        return "\(brand), \(volume)ml, \(price)원, \(name), \(displayable)"
     }
     
     init(brand: String, volume: Int, price: Int, name: String, date: Date) {
