8a0d161105c6767cca4ae81e1f064f450e92b82b
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-15 16:54:53 +0900

    Extension for Date Convert

--- VendingMachine/VendingMachine/DateExtension.swift
+++ VendingMachine/VendingMachine/DateExtension.swift
@@ -9,6 +9,22 @@
 import Foundation
 
 extension Date {
+    static func convert(days: Int) -> TimeInterval {
+        return TimeInterval(days * 86400)
+    }
+    
+    static func convert(weeks: Int) -> TimeInterval {
+        return convert(days: 7) * Double(weeks)
+    }
+    
+    static func convert(months: Int) -> TimeInterval {
+        return convert(days: 30) * Double(months)
+    }
+    
+    static func convert(years: Int) -> TimeInterval {
+        return convert(months: 12) * Double(years)
+    }
+    
     var readable: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyyMMdd"
