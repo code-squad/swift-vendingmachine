664cd15e77eda87589f13da82787673580dac48c
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-15 13:29:39 +0900

    display

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,4 +8,10 @@
 
 import Foundation
 
+let milk = Beverage(brand: "서울우유", volume: 200, price: 1000, name: "날마다딸기우유", date: Date(timeIntervalSinceNow: 360000))
+let pepsi = Beverage(brand: "팹시", volume: 350, price: 2000, name: "다이어트콜라", date: Date(timeIntervalSinceNow: 720000))
+let maxim = Beverage(brand: "맥심", volume: 400, price: 3000, name: "TOP아메리카노", date: Date(timeIntervalSinceNow: 250000))
 
+print(milk)
+print(pepsi)
+print(maxim)
