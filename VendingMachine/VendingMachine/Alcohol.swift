4a727830d1353dd502e4f33c7c89fb46940f6c6b
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-15 16:55:13 +0900

    Interfaces for Subclasses

--- VendingMachine/VendingMachine/Alcohol.swift
+++ VendingMachine/VendingMachine/Alcohol.swift
@@ -10,19 +10,28 @@ import Foundation
 
 class Alcohol: Beverage {
     private var alcohol: Double // 알코올 도수
+    var isNonAlcohol: Bool {
+        return alcohol == 0
+    }
     
-    init(alcohol: Double, brand: String, volume: Int, price: Int, name: String, date: Date, expire: Date) {
+    init(alcohol: Double, brand: String, volume: Int, price: Int, name: String, date: Date) {
         self.alcohol = alcohol
-        super.init(brand: brand, volume: volume, price: price, name: name, date: date, expire: expire)
+        super.init(brand: brand, volume: volume, price: price, name: name, date: date)
     }
 }
 
 class Beer: Alcohol {
     private var malt: Double // 맥아 비율
+    override var expire: Date {
+        return Date(timeInterval: Date.convert(years: 1), since: super.expire)
+    }
+    var bitterness: Double {
+        return malt * 100 / 1.23 // 임의식
+    }
     
-    init(malt: Double, alcohol: Double, brand: String, volume: Int, price: Int, name: String, date: Date, expire: Date) {
+    init(malt: Double, alcohol: Double, brand: String, volume: Int, price: Int, name: String, date: Date) {
         self.malt = malt
-        super.init(alcohol: alcohol, brand: brand, volume: volume, price: price, name: name, date: date, expire: expire)
+        super.init(alcohol: alcohol, brand: brand, volume: volume, price: price, name: name, date: date)
     }
 }
 
