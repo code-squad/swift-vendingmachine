bdcb9e6a48ebfb113bb5c024c06619df817087b2
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-18 16:12:44 +0900

    Beverage Bundle for Same Type of Beverages

---
+++ VendingMachine/VendingMachine/BeverageBundle.swift
@@ -0,0 +1,22 @@
+//
+//  BeverageBundle.swift
+//  VendingMachine
+//
+//  Created by 이동건 on 18/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class BeverageBundle {
+    private var beverages: [Beverage]
+    
+    func getBundleTag(_ handler: (Beverage, Int)->(String)) -> String? {
+        guard let first = beverages.first else { return nil }
+        return handler(first, beverages.count)
+    }
+    
+    init(_ beverages: [Beverage]) {
+        self.beverages = beverages
+    }
+}
