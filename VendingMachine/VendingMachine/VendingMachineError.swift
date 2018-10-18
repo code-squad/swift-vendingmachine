0c76f4863934d5992ab77e6c83bcc7bcaa6f9bd6
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-18 13:38:09 +0900

    VendingMachineError

---
+++ VendingMachine/VendingMachine/VendingMachineError.swift
@@ -0,0 +1,14 @@
+//
+//  VendingMachineError.swift
+//  VendingMachine
+//
+//  Created by 이동건 on 18/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+enum VendingMachineError: Error {
+    case wrongInput
+    case outOfBudget
+}
