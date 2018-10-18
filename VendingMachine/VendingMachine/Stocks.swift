f7b460919ca964349da999d9697ebd32f574b503
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-18 14:30:22 +0900

    info, price tags interface in Stocks

--- VendingMachine/VendingMachine/Stocks.swift
+++ VendingMachine/VendingMachine/Stocks.swift
@@ -10,21 +10,39 @@ import Foundation
 
 class Stocks {
     private var list: [Beverage]
-    private var stocks: [[Beverage] : Int] {
+    private var stocks: [[Beverage]] {
         let milks: [Milk] = list.compactMap { $0 as? Milk }
         let alcohols: [Alcohol] = list.compactMap { $0 as? Alcohol }
         let coffees: [Coffee] = list.compactMap { $0 as? Coffee }
+        return [milks, alcohols, coffees]
+    }
+    
+    var info: String {
+        var result = ""
+        stocks.forEach {
+            guard let representation = $0.first else { return }
+            result += " \(representation.summary(isSelectable: false, with: $0.count))"
+        }
+        return result
+    }
+    
+    var priceTags: String {
+        var result = ""
+        stocks.enumerated().forEach {
+            guard let representation = $0.element.first else { return }
+            let prefix = "\($0.offset + 1)) "
+            let info = "\(representation.summary(isSelectable: true, with: $0.element.count))"
+            result += "\((prefix + info))\n"
+        }
+        return result
         
-        return [ milks: milks.count,
-                alcohols : alcohols.count,
-                coffees : coffees.count ]
     }
     
     init(_ list: [Beverage]) {
         self.list = list
     }
     
-    func append(_ beverage: Beverage) -> [[Beverage] : Int] {
+    func append(_ beverage: Beverage) -> [[Beverage]] {
         list.append(beverage)
         return stocks
     }
