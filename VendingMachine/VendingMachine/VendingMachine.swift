84a3fa57d285fba9c995778debfe9071179262b1
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-19 15:07:57 +0900

    Vending Machine

---
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -0,0 +1,50 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by 이동건 on 18/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class VendingMachine {
+    private var stocks: Stocks
+    private var account: Int = 0
+    var availables: [(Beverage,Int)] {
+        return stocks.availables
+    }
+    // 금액을 넣고 잔액을 확인할 수 있는 프로퍼티
+    var transactions: Int {
+        set {
+            account += newValue
+        }
+        get {
+            return account
+        }
+    }
+    
+    init(_ stocks: Stocks) {
+        self.stocks = stocks
+    }
+    
+    func append(_ beverage: Beverage) {
+        stocks.append(beverage)
+    }
+    
+    // 현재 잔액으로 구매할 수 있는 음료 목록
+    func availables(with money: Int) -> [Beverage] {
+        return stocks.availables(with: money)
+    }
+    
+    // 음료수 구매 메소드
+    func buy(at index: Int) throws -> Beverage {
+        do {
+            let order = try stocks.buy(at: index, account)
+            account -= order.price
+            return order.beverage
+        } catch let err {
+            throw err 
+        }
+    }
+}
