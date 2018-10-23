b4a313b27ea203e0d17ff0200df93339f8de7985
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-23 12:09:50 +0900

    Bundle, Bundles

---
+++ VendingMachine/VendingMachine/Bundles.swift
@@ -0,0 +1,32 @@
+//
+//  Bundles.swift
+//  VendingMachine
+//
+//  Created by 이동건 on 23/10/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class Bundles {
+    private var bundles: [Bundle]
+    var count: Int {
+        return bundles.count
+    }
+    
+    init(_ bundles: [Bundle]) {
+        self.bundles = bundles
+    }
+    
+    func get(at index: Int) -> Beverage {
+        return bundles[index - 1].beverage
+    }
+    
+    func display(_ handler: (_ offset: Int, _ beverage: Beverage, _ count: Int)->(String)) -> String {
+        var result = ""
+        bundles.enumerated().forEach {
+            result += handler($0.offset, $0.element.beverage, $0.element.count)
+        }
+        return result
+    }
+}
