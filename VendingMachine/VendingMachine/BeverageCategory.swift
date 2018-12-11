a3ccecfe519344d7df14d182bc48760a229faf93
popsmile <popsmile7@gmail.com>
2018-12-11 17:02:54 +0900

    Create an enumeration BeverageCategory

---
+++ VendingMachine/VendingMachine/BeverageCategory.swift
@@ -0,0 +1,19 @@
+//
+//  BeverageCategory.swift
+//  VendingMachine
+//
+//  Created by 윤지영 on 11/12/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+enum BeverageCategory {
+    case milk
+    case softDrink
+    case coffee
+}
+
+protocol Groupable {
+    var group: BeverageCategory { get }
+}
