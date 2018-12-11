803f2baee5530c28f17f17364c7b1b0374dbc8c3
popsmile <popsmile7@gmail.com>
2018-12-11 17:09:31 +0900

    Modify the values of dateOfManufacture

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,9 +9,10 @@
 import Foundation
 
 func main() {
-    let chocolateMilk = Milk(brand: "남양유업", name: "초코에몽", volume: 180, price: 1000, dateOfManufacture: Date(timeIntervalSinceNow: 5))
-    let sprite = SoftDrink(brand: "스프라이트", name: "스프라이트", volume: 250, price: 1200, dateOfManufacture: Date(timeIntervalSinceNow: 50))
-    let cantata = Coffee(brand: "롯데칠성", name: "칸타타", volume: 275, price: 2200, dateOfManufacture: Date(timeIntervalSinceNow: 30))
+    let aDay: Double = 86400
+    let chocolateMilk = Milk(brand: "남양유업", name: "초코에몽", volume: 180, price: 1000, dateOfManufacture: Date(timeIntervalSinceNow: -2*aDay))
+    let sprite = SoftDrink(brand: "스프라이트", name: "스프라이트", volume: 250, price: 1200, dateOfManufacture: Date(timeIntervalSinceNow: -30*aDay))
+    let cantata = Coffee(brand: "롯데칠성", name: "칸타타", volume: 275, price: 2200, dateOfManufacture: Date(timeIntervalSinceNow: -20*aDay))
     let beverages: [Beverage] = [chocolateMilk, sprite, cantata]
     
     beverages.forEach { beverage in
