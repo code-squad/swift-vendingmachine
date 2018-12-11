ad8fb2df624b60a7d3b6cafee3056858c72102b0
popsmile <popsmile7@gmail.com>
2018-12-11 17:04:24 +0900

    Add some instances of Beverage class to main

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,4 +8,15 @@
 
 import Foundation
 
+func main() {
+    let chocolateMilk = Milk(brand: "남양유업", name: "초코에몽", volume: 180, price: 1000, dateOfManufacture: Date(timeIntervalSinceNow: 5))
+    let sprite = SoftDrink(brand: "스프라이트", name: "스프라이트", volume: 250, price: 1200, dateOfManufacture: Date(timeIntervalSinceNow: 50))
+    let cantata = Coffee(brand: "롯데칠성", name: "칸타타", volume: 275, price: 2200, dateOfManufacture: Date(timeIntervalSinceNow: 30))
+    let beverages: [Beverage] = [chocolateMilk, sprite, cantata]
+    
+    beverages.forEach { beverage in
+        print(beverage)
+    }
+}
 
+main()
