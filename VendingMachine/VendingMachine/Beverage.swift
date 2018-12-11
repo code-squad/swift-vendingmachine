92d11a4cbc52684409d28a722db96bb177abf35f
popsmile <popsmile7@gmail.com>
2018-12-11 17:02:28 +0900

    Create a class named Beverage

---
+++ VendingMachine/VendingMachine/Beverage.swift
@@ -0,0 +1,33 @@
+//
+//  Beverage.swift
+//  VendingMachine
+//
+//  Created by 윤지영 on 11/12/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class Beverage: CustomStringConvertible {
+    private let brand: String
+    private let name: String
+    private let volume: Int
+    private let price: Int
+    private let dateOfManufacture: Date
+
+    init(brand: String, name: String, volume: Int, price: Int, dateOfManufacture: Date) {
+        self.brand = brand
+        self.name = name
+        self.volume = volume
+        self.price = price
+        self.dateOfManufacture = dateOfManufacture
+    }
+
+    var description: String {
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "yyyyMMdd"
+        let date = dateFormat.string(from: dateOfManufacture)
+        return "\(brand), \(name), \(volume)ml, \(price)원, \(date)"
+    }
+
+}
