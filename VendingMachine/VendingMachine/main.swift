f385abfd1ce1c0e2aad84d2a48264e0d668653e9
popsmile <popsmile7@gmail.com>
2018-12-13 16:43:18 +0900

    Add a super class to the class Beverage
    Change the protocol CustomStringConvertible to the inheritance of NSObject to use the property named className

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -10,13 +10,13 @@ import Foundation
 
 func main() {
     let aDay: Double = 86400
-    let chocolateMilk = Milk(brand: "남양유업", name: "초코에몽", volume: 180, price: 1000, dateOfManufacture: Date(timeIntervalSinceNow: -2*aDay))
-    let sprite = SoftDrink(brand: "스프라이트", name: "스프라이트", volume: 250, price: 1200, dateOfManufacture: Date(timeIntervalSinceNow: -30*aDay))
-    let cantata = Coffee(brand: "롯데칠성", name: "칸타타", volume: 275, price: 2200, dateOfManufacture: Date(timeIntervalSinceNow: -20*aDay))
+    let chocolateMilk = ChocolateMilk(brand: "남양유업", name: "초코에몽", volume: 180, price: 1000, dateOfManufacture: Date(timeIntervalSinceNow: -2*aDay))
+    let sprite = Sprite(volume: 250, price: 1200, dateOfManufacture: Date(timeIntervalSinceNow: -30*aDay), package: .can)
+    let cantata = Cantata(name: "칸타타 더치블랙", volume: 275, price: 2200, dateOfManufacture: Date(timeIntervalSinceNow: -20*aDay), ice: false)
     let beverages: [Beverage] = [chocolateMilk, sprite, cantata]
     
     beverages.forEach { beverage in
-        print(beverage)
+        print("\(beverage.className) - \(beverage)")
     }
 }
 
