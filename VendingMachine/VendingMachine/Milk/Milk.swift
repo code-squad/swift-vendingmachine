ed4bd96d01f9c158061d7d78d6bb56f0181048a6
popsmile <popsmile7@gmail.com>
2018-12-13 16:37:04 +0900

    Add some own properties and methods to the class Milk

--- VendingMachine/VendingMachine/Milk/Milk.swift
+++ VendingMachine/VendingMachine/Milk/Milk.swift
@@ -9,9 +9,39 @@
 import Foundation
 
 class Milk: Beverage, Groupable {
+    private let flavor: String?
+    private let package: BeveragePackage
+    private let useByDate: Double = 10
+    private let expirationDate: Date
+
+    init(brand: String, name: String, volume: Int, price: Int, dateOfManufacture: Date, flavor: String? = nil, package: BeveragePackage) {
+        self.flavor = flavor
+        self.package = package
+        self.expirationDate = dateOfManufacture.addingDayInterval(useByDate)
+        super.init(brand: brand, name: name, volume: volume, price: price, dateOfManufacture: dateOfManufacture)
+    }
+
+    convenience init(brand: String, name: String, volume: Int, price: Int, dateOfManufacture: Date, flavor: String? = nil) {
+        self.init(
+            brand: brand,
+            name: name,
+            volume: volume,
+            price: price,
+            dateOfManufacture: dateOfManufacture,
+            flavor: flavor,
+            package: BeveragePackage.paper)
+    }
 
     var group: BeverageCategory {
         return .milk
+    }
+
+    func hasFlavor() -> Bool {
+        return flavor != nil
+    }
+
+    func validate(when date:Date) -> Bool {
+        return date < expirationDate
     }
 
 }
