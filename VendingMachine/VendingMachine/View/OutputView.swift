860cd4fd6a9d140c801ce786c17bf7c7362de18f
popsmile <popsmile7@gmail.com>
2018-12-14 14:49:55 +0900

    Create structs named InputView and OutputView

---
+++ VendingMachine/VendingMachine/View/OutputView.swift
@@ -0,0 +1,13 @@
+//
+//  OutputView.swift
+//  VendingMachine
+//
+//  Created by 윤지영 on 14/12/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+
+}
