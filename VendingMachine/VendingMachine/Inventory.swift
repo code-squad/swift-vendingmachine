fc028cf838c9ede953a3a285cd337cd2e8d3ad2f
popsmile <popsmile7@gmail.com>
2018-12-14 14:48:38 +0900

    Create a class named Inventory

---
+++ VendingMachine/VendingMachine/Inventory.swift
@@ -0,0 +1,18 @@
+//
+//  Inventory.swift
+//  VendingMachine
+//
+//  Created by 윤지영 on 14/12/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class Inventory {
+    private let beverages: [Beverage]
+
+    init() {
+        self.beverages = []
+    }
+
+}
