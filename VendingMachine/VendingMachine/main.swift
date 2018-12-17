1f6d6551fffc4626732c19e74e30b81e92dcc5fb
hngfu <djsl1018@naver.com>
2018-12-17 18:06:46 +0900

    Date를 init(before day:) 확장

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 func main() {
-    let milk = Beverage(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date())
+    let milk = Beverage(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date(before: 1))
     
-    let cola = Beverage(brand: "코카콜라", size: 185, price: 1300, name: "코카콜라", openDate: Date())
+    let cola = Beverage(brand: "코카콜라", size: 185, price: 1300, name: "코카콜라", openDate: Date(before: 4))
     
-    let starbucksCoffee = Beverage(brand: "동서식품", size: 200, price: 1400, name: "스타벅스 더블샷 에스프레소 & 크림", openDate: Date())
+    let starbucksCoffee = Beverage(brand: "동서식품", size: 200, price: 1400, name: "스타벅스 더블샷 에스프레소 & 크림", openDate: Date(before: 6))
     
     print(milk)
     print(cola)
