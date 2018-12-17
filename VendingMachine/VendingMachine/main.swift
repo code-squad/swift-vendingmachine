683b4bfcd7e94c6af19e7176db3194eea57ffad7
hngfu <djsl1018@naver.com>
2018-12-17 17:57:53 +0900

    Date에 toString() 확장

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,14 +8,12 @@
 
 import Foundation
 
-let oneDay: TimeInterval = 86400
-
 func main() {
-    let milk = Beverage(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date(timeIntervalSinceNow: oneDay * 4))
+    let milk = Beverage(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date())
     
-    let cola = Beverage(brand: "코카콜라", size: 185, price: 1300, name: "코카콜라", openDate: Date(timeIntervalSinceNow: oneDay * 287))
+    let cola = Beverage(brand: "코카콜라", size: 185, price: 1300, name: "코카콜라", openDate: Date())
     
-    let starbucksCoffee = Beverage(brand: "동서식품", size: 200, price: 1400, name: "스타벅스 더블샷 에스프레소 & 크림", openDate: Date(timeIntervalSinceNow: oneDay * 365))
+    let starbucksCoffee = Beverage(brand: "동서식품", size: 200, price: 1400, name: "스타벅스 더블샷 에스프레소 & 크림", openDate: Date())
     
     print(milk)
     print(cola)
