bb29cbaa041cd3e607e3722e6791ac46c07fabc1
hngfu <djsl1018@naver.com>
2018-12-17 19:09:44 +0900

    음료 객체 생성시 제조일자는 지정하도록 수정

--- VendingMachine/VendingMachine/Milks/MandarineMilk.swift
+++ VendingMachine/VendingMachine/Milks/MandarineMilk.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class MandarineMilk: Milk {
-    init() {
-        super.init(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date(before: 3))
+    init(openDate: Date) {
+        super.init(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: openDate)
     }
 }
