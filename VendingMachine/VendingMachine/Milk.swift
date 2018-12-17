afbdde898c2d4d99555c03d625a06707230f8ed3
hngfu <djsl1018@naver.com>
2018-12-17 18:07:42 +0900

    Milk생성

---
+++ VendingMachine/VendingMachine/Milk.swift
@@ -0,0 +1,13 @@
+//
+//  Milk.swift
+//  VendingMachine
+//
+//  Created by 조재흥 on 18. 12. 17..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class Milk: Beverage {
+    
+}
