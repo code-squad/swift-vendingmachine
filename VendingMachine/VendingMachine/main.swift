bf7a4c503f7189ce53bdf3a0f940378290b3ab88
hngfu <djsl1018@naver.com>
2018-12-17 16:14:44 +0900

    코카콜라 추가

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -13,7 +13,10 @@ let oneDay: TimeInterval = 86400
 func main() {
     let milk = Beverage(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date(timeIntervalSinceNow: oneDay * 4))
     
+    let cola = Beverage(brand: "코카콜라", size: 185, price: 1300, name: "코카콜라", openDate: Date(timeIntervalSinceNow: oneDay * 287))
+    
     print(milk)
+    print(cola)
 }
 
 main()
