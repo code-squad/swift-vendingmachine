cd99641114c4e45f7163f0dde994de72c14701fc
hngfu <djsl1018@naver.com>
2018-12-17 18:31:49 +0900

    main()수정

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,15 +9,13 @@
 import Foundation
 
 func main() {
-    let milk = Beverage(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date(before: 1))
+    let mandarineMilk = MandarineMilk()
+    let cocaCola = CocaCola()
+    let starbucksDoubleShot = StarbucksDoubleShot()
     
-    let cola = Beverage(brand: "코카콜라", size: 185, price: 1300, name: "코카콜라", openDate: Date(before: 4))
-    
-    let starbucksCoffee = Beverage(brand: "동서식품", size: 200, price: 1400, name: "스타벅스 더블샷 에스프레소 & 크림", openDate: Date(before: 6))
-    
-    print(milk)
-    print(cola)
-    print(starbucksCoffee)
+    print(mandarineMilk)
+    print(cocaCola)
+    print(starbucksDoubleShot)
 }
 
 main()
