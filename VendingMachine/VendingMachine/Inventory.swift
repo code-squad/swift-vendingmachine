89156346c979b6b338fb77500161f977d884ec98
popsmile <popsmile7@gmail.com>
2018-12-17 18:42:55 +0900

    Implement methods of VendingMachine using the class Inventory

--- VendingMachine/VendingMachine/Inventory.swift
+++ VendingMachine/VendingMachine/Inventory.swift
@@ -9,34 +9,41 @@
 import Foundation
 
 class Inventory {
-    private var beverages: [Beverage]
+    private var list: [ObjectIdentifier: Pack]
 
-    init() {
-        self.beverages = []
+    init(list: [ObjectIdentifier: Pack]) {
+        self.list = list
     }
 
     //    특정 상품 인스턴스를 넘겨서 재고를 추가하는 메소드
-    func add(_ beverage: Beverage) {
-        beverages.append(beverage)
-    }
-
-    //    전체 상품 재고를 (사전으로 표현하는) 종류별로 리턴하는 메소드
-    func countNumberOfEachBeverage() -> [Beverage: Int] {
-        var numberOfEachBeverage: [Beverage: Int] = [:]
-        for beverage in beverages {
-            numberOfEachBeverage[beverage] = numberOfEachBeverage[beverage] ?? 0 + 1
+    func add(beverage: Beverage) {
+        let beverageType = ObjectIdentifier(type(of: beverage))
+        if let pack = list[beverageType] {
+            pack.add(beverage: beverage)
+            return
         }
-        return numberOfEachBeverage
+        list[beverageType] = Pack(beverages: [beverage])
     }
 
     //    현재 금액으로 구매가능한 음료수 목록을 리턴하는 메소드
-    func getListOfBuyable(for cash: Int) -> [Beverage] {
-        var beveragesBuyable: [Beverage] = []
-        for beverage in beverages {
-            guard beverage.isBuyable(for: cash) else { continue }
-            beveragesBuyable.append(beverage)
-        }
-        return beveragesBuyable
+    func getListBuyable(with money: Int) -> [Pack] {
+        return list.values.filter { $0.isBuyable(with: money) }
     }
+
+    private func findID(of pack: Pack) -> ObjectIdentifier? {
+        for item in list {
+            if item.value == pack { return item.key }
+        }
+        return nil
+    }
+
+    //    음료수를 구매하는 메소드
+    func remove(selected pack: Pack) -> Beverage? {
+        guard let id = findID(of: pack) else { return nil }
+        return list[id]?.removeOne()
+    }
+    //    전체 상품 재고를 (사전으로 표현하는) 종류별로 리턴하는 메소드
+    //    유통기한이 지난 재고만 리턴하는 메소드
+    //    따뜻한 음료만 리턴하는 메소드
 
 }
--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -9,5 +9,42 @@
 import Foundation
 
 struct VendingMachine {
+    private var balance: Int
+    private var inventory: Inventory
 
+    init(beginningBalance: Int = 0, initialInventory: Inventory) {
+        self.balance = beginningBalance
+        self.inventory = initialInventory
+    }
+
+//    자판기 금액을 원하는 금액만큼 올리는 메소드
+    mutating func insert(money: Int) -> Bool {
+        guard money > 0 else { return false }
+        balance += money
+        return true
+    }
+
+//    잔액을 확인하는 메소드
+    func showBalance(with show: (Int) -> Void) {
+        show(balance)
+    }
+
+    func add(beverage: Beverage) {
+        inventory.add(beverage: beverage)
+    }
+
+    func getListBuyable() -> [Pack] {
+        return inventory.getListBuyable(with: balance)
+    }
+
+    func buy(beverage pack: Pack) throws -> Beverage {
+        guard let beverage = inventory.remove(selected: pack) else { throw VendingMachineError.outOfStock }
+        return beverage
+    }
+//    시작이후 구매 상품 이력을 배열로 리턴하는 메소드
+
+}
+
+enum VendingMachineError: Error {
+    case outOfStock
 }
