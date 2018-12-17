a3f34ae7562cae5bef2ccf033dff7640aee68c5d
hngfu <djsl1018@naver.com>
2018-12-17 16:12:35 +0900

    '하루'만큼의 초를 나타내는 oneDay를 추가

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,8 +8,12 @@
 
 import Foundation
 
+let oneDay: TimeInterval = 86400
+
 func main() {
-    let milk = Beverage(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date(timeIntervalSinceNow: 86400))
+    let milk = Beverage(brand: "빙그레", size: 240, price: 1200, name: "귤맛 우유", openDate: Date(timeIntervalSinceNow: oneDay * 4))
+    
+    print(milk)
 }
 
 main()
