33857afdceb70d12078c0b757a792882af61f041
hngfu <djsl1018@naver.com>
2018-12-18 17:50:13 +0900

    VendingMachine 추가

---
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -0,0 +1,13 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by 조재흥 on 18. 12. 18..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct VendingMachine {
+    
+}
