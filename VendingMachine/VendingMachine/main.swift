b5ddfef8a44aedfa84f0643ca530853335be6e6d
hngfu <djsl1018@naver.com>
2018-12-18 15:30:23 +0900

    StarbucksDoubleShot 추가

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -11,7 +11,7 @@ import Foundation
 func main() {
     let mandarineMilk = MandarineMilk(openDate: Date(before: 2))
     let cocaCola = CarbonatedDrink(openDate: Date(before: 4))
-    let starbucksDoubleShot = Coffee(openDate: Date(before: 6))
+    let starbucksDoubleShot = StarbucksDoubleShot(openDate: Date(before: 6))
     
     print(mandarineMilk)
     print(cocaCola)
