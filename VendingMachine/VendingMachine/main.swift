df271ebbed4c826538207f11f4c6b10c9c981509
hngfu <djsl1018@naver.com>
2018-12-18 16:29:45 +0900

    음료 객체의 인스턴스를 종류별로 만들어서 출력

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -10,12 +10,18 @@ import Foundation
 
 func main() {
     let mandarineMilk = MandarineMilk(openDate: Date(before: 2))
-    let cocaCola = CocaCola(openDate: Date(before: 4))
+    let lactoseFreeMilk = LactoseFreeMilk(openDate: Date(before: 3))
     let starbucksDoubleShot = StarbucksDoubleShot(openDate: Date(before: 6))
+    let topTheBlack = TOPTheBlack(openDate: Date(before: 7))
+    let cocaCola = CocaCola(openDate: Date(before: 4))
+    let cocaColaZero = CocaColaZero(openDate: Date(before: 9))
     
     print(mandarineMilk)
-    print(cocaCola)
+    print(lactoseFreeMilk)
     print(starbucksDoubleShot)
+    print(topTheBlack)
+    print(cocaCola)
+    print(cocaColaZero)
 }
 
 main()
