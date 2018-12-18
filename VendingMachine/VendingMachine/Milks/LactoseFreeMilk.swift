c4b3a7fd487767063d59c09995dd1a017ed7ec0a
hngfu <djsl1018@naver.com>
2018-12-18 14:12:28 +0900

    LactoseFreeMilk생성

---
+++ VendingMachine/VendingMachine/Milks/LactoseFreeMilk.swift
@@ -0,0 +1,15 @@
+//
+//  LactoseFreeMilk.swift
+//  VendingMachine
+//
+//  Created by 조재흥 on 18. 12. 18..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class LactoseFreeMilk: Milk {
+    init() {
+        super.init(brand: "매일유업", size: 190, price: 1100, name: "소화가 잘되는 우유", openDate: Date(before: 2))
+    }
+}
