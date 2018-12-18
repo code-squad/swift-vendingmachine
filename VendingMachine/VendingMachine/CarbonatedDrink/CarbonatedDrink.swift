1ed01e54c45463024d2da8068bb44e9973a35371
hngfu <djsl1018@naver.com>
2018-12-18 16:07:09 +0900

    CarbonatedDrink에 isLowCalorie() 추가

--- VendingMachine/VendingMachine/CarbonatedDrink/CarbonatedDrink.swift
+++ VendingMachine/VendingMachine/CarbonatedDrink/CarbonatedDrink.swift
@@ -10,8 +10,13 @@ import Foundation
 
 class CarbonatedDrink: Beverage {
     private let calorie: Int
+    
     init(calorie: Int, brand: String, size: Int, price: Int, name: String, openDate: Date) {
         self.calorie = calorie
         super.init(brand: brand, size: size, price: price, name: name, openDate: openDate)
+    }
+    
+    func isLowCalorie() -> Bool {
+        return self.calorie < 10
     }
 }
