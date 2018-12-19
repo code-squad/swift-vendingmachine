62b5917299ce6d371015e72853fb7d52fefb2b82
hngfu <djsl1018@naver.com>
2018-12-19 14:09:19 +0900

    ProductLine 추가

---
+++ VendingMachine/VendingMachine/ProductLine.swift
@@ -0,0 +1,13 @@
+//
+//  ProductLine.swift
+//  VendingMachine
+//
+//  Created by 조재흥 on 18. 12. 19..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct ProductLine {
+    private var products: [Beverage] = []
+}
