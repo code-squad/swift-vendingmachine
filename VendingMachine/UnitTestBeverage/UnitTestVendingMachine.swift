190e60d5bd4bc567ca72f7eba1511acc465966c4
hngfu <djsl1018@naver.com>
2018-12-19 14:55:16 +0900

    UnitTest setUp()에 자판기 인스턴스 생성, 음료 인스턴스 넣어줌

--- VendingMachine/UnitTestBeverage/UnitTestVendingMachine.swift
+++ VendingMachine/UnitTestBeverage/UnitTestVendingMachine.swift
@@ -12,6 +12,25 @@ class UnitTestVendingMachine: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        var VM = VendingMachine()
+        VM.insert(money: 1100)
+        
+        let mandarineMilk = MandarineMilk(openDate: Date(before: 2))
+        let lactoseFreeMilk = LactoseFreeMilk(openDate: Date(before: 3))
+        let starbucksDoubleShot = StarbucksDoubleShot(openDate: Date(before: 6))
+        let topTheBlack = TOPTheBlack(openDate: Date(before: 7))
+        let cocaCola = CocaCola(openDate: Date(before: 4))
+        let cocaColaZero = CocaColaZero(openDate: Date(before: 9))
+        
+        VM.add(product: mandarineMilk)
+        VM.add(product: lactoseFreeMilk)
+        VM.add(product: lactoseFreeMilk)
+        VM.add(product: cocaCola)
+        VM.add(product: cocaCola)
+        VM.add(product: cocaCola)
+        VM.add(product: starbucksDoubleShot)
+        VM.add(product: starbucksDoubleShot)
     }
 
     override func tearDown() {
