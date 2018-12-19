a7757810c99dff177262968c29c19218104e297a
hngfu <djsl1018@naver.com>
2018-12-19 14:06:25 +0900

    VendingMachine에 add(product:) 추가

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -10,9 +10,13 @@ import Foundation
 
 struct VendingMachine {
     private var balance: Int = 0
+    private var products: [String:[Beverage]] = [:]
     
     mutating func insert(money: Int) {
         self.balance += money
     }
     
+    mutating func add(product: Beverage) {
+        products["\(type(of:product))"] = [product]
+    }
 }
