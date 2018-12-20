b22caa80a9935b1b929030828c230659b9f19d02
hngfu <djsl1018@naver.com>
2018-12-20 17:45:54 +0900

    InputView 추가

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,19 +9,34 @@
 import Foundation
 
 func main() {
-    let mandarineMilk = MandarineMilk(openDate: Date(before: 2))
-    let lactoseFreeMilk = LactoseFreeMilk(openDate: Date(before: 3))
+    var vendingMachine = initializeVendingMachine()
+    
+    
+    
+}
+
+func initializeVendingMachine() -> VendingMachine {
+    var VM = VendingMachine()
+    
+    let mandarineMilk = MandarineMilk(openDate: Date(before: 13))
+    let lactoseFreeMilk = LactoseFreeMilk(openDate: Date(before: 20))
     let starbucksDoubleShot = StarbucksDoubleShot(openDate: Date(before: 6))
     let topTheBlack = TOPTheBlack(openDate: Date(before: 7))
     let cocaCola = CocaCola(openDate: Date(before: 4))
     let cocaColaZero = CocaColaZero(openDate: Date(before: 9))
     
-    print(mandarineMilk)
-    print(lactoseFreeMilk)
-    print(starbucksDoubleShot)
-    print(topTheBlack)
-    print(cocaCola)
-    print(cocaColaZero)
+    VM.add(product: mandarineMilk)
+    VM.add(product: lactoseFreeMilk)
+    VM.add(product: lactoseFreeMilk)
+    VM.add(product: cocaCola)
+    VM.add(product: cocaCola)
+    VM.add(product: cocaCola)
+    VM.add(product: starbucksDoubleShot)
+    VM.add(product: starbucksDoubleShot)
+    VM.add(product: topTheBlack)
+    VM.add(product: cocaColaZero)
+    
+    return VM
 }
 
 main()
