7938f02af1a981cc0ec3ebc15701e556aa6d5308
popsmile <popsmile7@gmail.com>
2018-12-21 02:15:41 +0900

    Separate the large methods of Excutable into smaller roles

--- VendingMachine/VendingMachine/Executable.swift
+++ VendingMachine/VendingMachine/Executable.swift
@@ -19,21 +19,62 @@ struct ManagerMode: Executable {
         self.vendingMachine = vendingMachine
     }
 
+    private func readMenu() throws -> ManagerMenuItem? {
+        OutputView.start(vendingMachine)
+        let input = InputView.readInput()
+        if MenuController.finish(keyword: input) { return nil }
+        return try MenuController.readManagerMenu(from: input)
+    }
+
+    private func removeExpiredBeverages() {
+        let expired = vendingMachine.removeExpiredBeverages()
+        OutputView.showListRemoved(beverages: expired)
+    }
+
+    private func readValue() throws -> Int {
+        let input = InputView.readInput()
+        guard let number = Int(input) else { throw MenuError.notInt }
+        return number - 1
+    }
+
+    private func complete(menu: ManagerMenuItem) -> Bool {
+        switch menu {
+        case .addBeverage, .removeBeverage:
+            OutputView.askValue(of: menu)
+            return false
+        case .removeExpiredBeverages:
+            removeExpiredBeverages()
+        case .readHistory:
+            OutputView.showHistory(of: vendingMachine)
+        }
+        return true
+    }
+
+    private func addBeverage(of number: Int) throws {
+        guard vendingMachine.add(beverage: number) else { throw VendingMachineError.notExistPack }
+        OutputView.showAddition(of: number)
+    }
+
+    private func removeBeverage(of number: Int) throws {
+        guard let beverage = vendingMachine.remove(beverage: number) else { throw VendingMachineError.cannotRemove }
+        OutputView.showRemoval(of: beverage)
+    }
+
     mutating func run() {
         OutputView.clear()
         while(true) {
             do {
-                OutputView.start(vendingMachine)
-                let input = InputView.readInput()
-                if MenuController.finish(keyword: input) { return }
-                let menu = try MenuController.readManagerMenu(from: input)
-
-                guard OutputView.proceed(menu: menu, of: vendingMachine) else { continue }
-                let numberSelected = InputView.readInput()
-                guard let number = Int(numberSelected) else { throw MenuError.notInt }
-                let selected = number - 1
-
-                try OutputView.operate(menu: menu, of: vendingMachine, with: selected)
+                guard let menu = try readMenu() else { return }
+                if complete(menu: menu) { continue }
+                let value = try readValue()
+                switch menu {
+                case .addBeverage:
+                    try addBeverage(of: value)
+                case .removeBeverage:
+                    try removeBeverage(of: value)
+                default:
+                    continue
+                }
             }  catch let error as MenuError {
                 OutputView.showMessage(of: error)
             }  catch let error as VendingMachineError {
@@ -53,28 +94,40 @@ struct ConsumerMode: Executable {
         self.vendingMachine = vendingMachine
     }
 
+    private func readMenu() throws -> (item: ConsumerMenuItem, value: Int)? {
+        if vendingMachine.isEmpty() { throw VendingMachineError.outOfStock }
+        OutputView.start(vendingMachine)
+        let input = InputView.readInput()
+        if MenuController.finish(keyword: input) { return nil }
+        return try MenuController.readConsumerMenu(from: input)
+    }
+
+    mutating private func insert(money: Int) -> Bool {
+        guard vendingMachine.insert(money: money) else { return false }
+        OutputView.showInsertion(of: money)
+        return true
+    }
+
+    mutating private func purchase(beverage: Int) throws -> Bool {
+        let list = vendingMachine.getListBuyable()
+        let number = beverage - 1
+        guard number < list.count else { throw MenuError.outOfList }
+        let pack = list[number]
+        guard let beverage = vendingMachine.buy(beverage: pack) else { return false }
+        OutputView.showPurchase(of: beverage)
+        return true
+    }
+
     mutating func run() {
         OutputView.clear()
         while(true) {
             do {
-                if vendingMachine.isEmpty() { throw VendingMachineError.outOfStock }
-                OutputView.start(vendingMachine)
-                let input = InputView.readInput()
-                if MenuController.finish(keyword: input) { return }
-                let menu = try MenuController.readConsumerMenu(from: input)
-
+                guard let menu = try readMenu() else { return }
                 switch menu.item {
                 case .insertCoin:
-                    let moneyInserted = menu.value
-                    guard vendingMachine.insert(money: moneyInserted) else { continue }
-                    OutputView.showInsertion(of: moneyInserted)
+                    guard insert(money: menu.value) else { continue }
                 case .purchaseBeverage:
-                    let listBuyable = vendingMachine.getListBuyable()
-                    let beverageSelected = menu.value - 1
-                    guard beverageSelected < listBuyable.count else { throw MenuError.outOfList }
-                    let packSelected = listBuyable[beverageSelected]
-                    guard let beveragePurchased = vendingMachine.buy(beverage: packSelected) else { continue }
-                    OutputView.showPurchase(of: beveragePurchased)
+                    guard try purchase(beverage: menu.value) else { continue }
                 }
             } catch let error as MenuError {
                 OutputView.showMessage(of: error)
