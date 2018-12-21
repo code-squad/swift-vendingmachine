57fe79e585a1c29c91893dcf9ce3c5f0d6141d42
hngfu <djsl1018@naver.com>
2018-12-21 11:00:30 +0900

    ValidChecker 생성

---
+++ VendingMachine/VendingMachine/ValidChecker.swift
@@ -0,0 +1,13 @@
+//
+//  ValidCheck.swift
+//  VendingMachine
+//
+//  Created by 조재흥 on 18. 12. 21..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct ValidChecker {
+    
+}
