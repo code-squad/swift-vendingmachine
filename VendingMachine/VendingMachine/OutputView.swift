4afd9b55fdc3799d0adb3688af760249c1f7961b
hngfu <djsl1018@naver.com>
2018-12-21 16:34:44 +0900

    OutputView 생성, show(result:)생성

---
+++ VendingMachine/VendingMachine/OutputView.swift
@@ -0,0 +1,15 @@
+//
+//  OutputView.swift
+//  VendingMachine
+//
+//  Created by 조재흥 on 18. 12. 21..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    func show(result: String) {
+        print(result)
+    }
+}
