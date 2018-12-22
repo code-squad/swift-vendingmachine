29729f0dd6232247c1e8c29c670fc0c309531b05
hngfu <djsl1018@naver.com>
2018-12-22 19:32:08 +0900

    BeverageInfo 생성

---
+++ VendingMachine/VendingMachine/BeverageInfo.swift
@@ -0,0 +1,15 @@
+//
+//  BeverageInfo.swift
+//  VendingMachine
+//
+//  Created by 조재흥 on 18. 12. 22..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct BeverageInfo {
+    private let name: String
+    private let price: Int
+    private let BeverageCount: Int
+}
