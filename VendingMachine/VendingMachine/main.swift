b2374a33be4f387ce2f7a8e7cadc6aadc1ce134e
Dongmin.yoon <ehdals8965@naver.com>
2018-12-28 14:32:40 +0900

    remove not using Class and Add Kind Enum Type

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -11,10 +11,10 @@ import Foundation
 
 func main() {
     var drinks: [Beverage] = []
-    drinks.append(ChocoMilk(name: "서울우유", volume: 200, price: 1000, brand: "낢마다초코우유", date: "20170610"))
-    drinks.append(ChocoMilk(name: "서울우유", volume: 200, price: 1000, brand: "낢마다초코우유", date: "20171012"))
-    drinks.append(Cider(name: "칠성사이다", volume: 350, price: 2000, brand: "다이어트사이다", date: "20171005"))
-    drinks.append(Cantata(name: "맥심", volume: 350, price: 3000, brand: "Cantata마끼아또", date: "20171010"))
+    drinks.append(Milk(name: "서울우유", volume: 200, price: 1000, brand: "낢마다초코우유", date: "20170610", milkKind: .choco))
+    drinks.append(Milk(name: "서울우유", volume: 200, price: 1000, brand: "낢마다초코우유", date: "20171012", milkKind: .banana))
+    drinks.append(CarbonatedDrink(name: "칠성사이다", volume: 350, price: 2000, brand: "다이어트사이다", date: "20171005", drinkKind: .cider))
+    drinks.append(Coffee(name: "맥심", volume: 350, price: 3000, brand: "Cantata마끼아또", date: "20171010", coffeeKind: .cantata))
 
     for drink in drinks {
         print(drink)
