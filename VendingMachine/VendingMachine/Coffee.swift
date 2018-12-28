54b23a591d3fbc7559f969f0401d914b127c3641
Dongmin.yoon <ehdals8965@naver.com>
2018-12-28 16:01:13 +0900

    Add 2 level Drink property

--- VendingMachine/VendingMachine/Coffee.swift
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -8,17 +8,11 @@
 
 import Foundation
 
-enum CoffeeKind {
-    case cantata
-    case top
-    case georgia
-}
-
 class Coffee: Beverage {
-    var kind: CoffeeKind
+    var caffeineAmount: Int
     
-    init(name: String, volume: Int, price: Int, brand: String, date: String, coffeeKind: CoffeeKind) {
-        self.kind = coffeeKind
+    init(name: String, volume: Int, price: Int, brand: String, date: String, caffeineAmount: Int) {
+        self.caffeineAmount = caffeineAmount
         super.init(name: name, volume: volume, price: price, brand: brand, date: date)
     }
 }
--- VendingMachine/VendingMachine/Milk.swift
+++ VendingMachine/VendingMachine/Milk.swift
@@ -8,17 +8,12 @@
 
 import Foundation
 
-enum MilkKind {
-    case choco
-    case strawberry
-    case banana
-}
-
+// Fat -> 저지방 우유일 경우 False, 고지방 우유일 경우 True
 class Milk: Beverage {
-    var kind: MilkKind
-    
-    init(name: String, volume: Int, price: Int, brand: String, date: String, milkKind: MilkKind) {
-        self.kind = milkKind
+    var fat: Bool
+
+    init(name: String, volume: Int, price: Int, brand: String, date: String, fat: Bool) {
+        self.fat = fat
         super.init(name: name, volume: volume, price: price, brand: brand, date: date)
     }
 }
