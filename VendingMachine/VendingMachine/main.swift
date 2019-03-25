dfaa42e40b0406109c3021fcf5f3a19a3124cb2f
conyconydev <conyconydev@gmail.com>
2019-03-25 15:02:14 +0900

    Milk class 변경 , print 추가

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,7 +8,9 @@
 
 import Foundation
 
+let chocolateMilk = Milk.init()
 
 //let cantataCoffee = Beverage.init(brand: "롯데칠성음료", volume: 275, price: 2000, name: "칸타타 아메리카노", manufacturedData: Date())
 //let CocaColaSoda = Beverage.init(brand: "코카콜라", volume: 355, price: 1500, name: "코카콜라", manufacturedData: Date())
 
+print(chocolateMilk)
