99865a39053c614d8596ac7bb1dca53283882f0c
conyconydev <conyconydev@gmail.com>
2019-03-31 14:13:52 +0900

    Unit Test code , Coffee :  유통기한 테스트 추가

--- VendingMachine/VendingMachine/Coffee/StarbucksCoffee.swift
+++ VendingMachine/VendingMachine/Coffee/StarbucksCoffee.swift
@@ -10,12 +10,18 @@ import Foundation
 
 class StarbucksCoffee: Coffee {
     
-    private let coffeePackage: CoffeePackage
+    private let coffeePackage: CoffeePackage?
     
     init() {
         self.coffeePackage = .glass
         super.init(brand: "동서식품", volume: 281, price: 3000, name: "스타벅스 모카", manufacturedDate:Date.changeString(beforeDate: "20180915"), caffeine: 30)
     }
     
+    func isPackage() -> CoffeePackage? {
+        guard let coffeePackage = coffeePackage else {
+            return nil
+        }
+        return coffeePackage
+    }
     
 }
