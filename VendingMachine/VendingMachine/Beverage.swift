99220875a23c586176fe898c339cfd67eaac7478
김지나 <jinagold.k@gmail.com>
2019-04-01 20:29:47 +0900

    음료 클래스 파일 생성
    상위 클래스 Beverage 생성
    하위 클래스 Milk, Soda, Coffee 생성

---
+++ VendingMachine/VendingMachine/Beverage.swift
@@ -0,0 +1,47 @@
+//
+//  Beverage.swift
+//  VendingMachine
+//
+//  Created by 김지나 on 01/04/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+// CustomStringConvertible : 모델 객채를 화면이다 디버깅 코드에 표시할 때, 텍스트적인 표현을 생성할 때 사용
+// 특정 타입을 String으로 표현
+class Beverage: CustomStringConvertible {
+    private let brand: String
+    private let capacity: Int
+    private let price: Int
+    private let productName: String
+    private let dateOfManufacture: Date
+    
+    init(brand: String, capacity: Int, price: Int, productName: String, dateOfManufacture: Date) {
+        self.brand = brand
+        self.capacity = capacity
+        self.price = price
+        self.productName = productName
+        self.dateOfManufacture = dateOfManufacture
+    }
+    
+    var description: String {
+        return "\(brand), \(capacity)ml, \(price)원, \(productName), \(dateOfManufacture)"
+    }
+}
+
+class Milk: Beverage {
+    override init(brand: String, capacity: Int, price: Int, productName: String, dateOfManufacture: Date) {
+        super.init(brand: brand, capacity: capacity, price: price, productName: productName, dateOfManufacture: dateOfManufacture)
+    }
+}
+class Soda: Beverage {
+    override init(brand: String, capacity: Int, price: Int, productName: String, dateOfManufacture: Date) {
+        super.init(brand: brand, capacity: capacity, price: price, productName: productName, dateOfManufacture: dateOfManufacture)
+    }
+}
+class Coffee: Beverage {
+    override init(brand: String, capacity: Int, price: Int, productName: String, dateOfManufacture: Date) {
+        super.init(brand: brand, capacity: capacity, price: price, productName: productName, dateOfManufacture: dateOfManufacture)
+    }
+}
