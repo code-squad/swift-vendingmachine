c2d54797ed40374455f1762cf88566b6d5bacf7e
김지나 <jinagold.k@gmail.com>
2019-04-04 21:35:14 +0900

    각 하위 클래스별로 속성 추가

---
+++ VendingMachine/VendingMachine/Beverage/Beverage.swift
@@ -0,0 +1,74 @@
+//
+//  Beverage.swift
+//  VendingMachine
+//
+//  Created by 김지나 on 01/04/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+// CustomStringConvertible : 모델 객채를 화면이나 디버깅 코드에 표시할 때, 텍스트적인 표현을 생성할 때 사용
+// 특정 타입을 String으로 표현
+class Beverage: CustomStringConvertible {
+    private let brand: String
+    private let capacity: Int
+    private let price: Int
+    private let productName: String
+    private let dateOfManufacture: Date
+    
+    init(brand: String, capacity: Int, price: Int, productName: String, dateOfManufacture: Date = Date()) {
+        self.brand = brand
+        self.capacity = capacity
+        self.price = price
+        self.productName = productName
+        self.dateOfManufacture = dateOfManufacture
+        
+    }
+    
+    // Date()의 결과를 원하는 형태로 출력되도록 포맷
+    var description: String {
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "yyyyMMdd"
+        let date = dateFormat.string(from: dateOfManufacture)
+        
+        return "\(brand), \(capacity)ml, \(price)원, \(productName), \(date)"
+    }
+}
+
+class Milk: Beverage {
+    private let grade: String
+    
+    init(grade: String, brand: String, capacity: Int, price: Int, productName: String, dateOfManufacture: Date = Date()) {
+        self.grade = grade
+        super.init(brand: brand, capacity: capacity, price: price, productName: productName, dateOfManufacture: dateOfManufacture)
+    }
+    
+    func isGradeA() -> Bool {
+        return grade == "A"
+    }
+}
+class Soda: Beverage {
+    private let calorie: Int
+    
+    init(calorie: Int, brand: String, capacity: Int, price: Int, productName: String, dateOfManufacture: Date = Date()) {
+        self.calorie = calorie
+        super.init(brand: brand, capacity: capacity, price: price, productName: productName, dateOfManufacture: dateOfManufacture)
+    }
+    
+    func isHigh() -> Bool {
+        return calorie > 400
+    }
+}
+class Coffee: Beverage {
+    private let temperature: String
+    
+    init(temperature: String, brand: String, capacity: Int, price: Int, productName: String, dateOfManufacture: Date = Date()) {
+        self.temperature = temperature
+        super.init(brand: brand, capacity: capacity, price: price, productName: productName, dateOfManufacture: dateOfManufacture)
+    }
+    
+    func isHot() -> Bool {
+        return temperature == "Hot"
+    }
+}
