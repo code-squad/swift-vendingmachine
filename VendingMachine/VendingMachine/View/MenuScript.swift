35d044121257e0349e5bea54b2c98c7c5a9a56ce
conyconydev <conyconydev@gmail.com>
2019-04-09 23:15:24 +0900

    메뉴에 대한 출력을 클로저로 구현하였으며, error 처리를 위해 throws 사용

--- VendingMachine/VendingMachine/View/MenuScript.swift
+++ VendingMachine/VendingMachine/View/MenuScript.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+
+protocol MessagePrintable {
+    var message: String { get }
+}
+
+enum MenuScript: Int, CaseIterable {
+    case returnChange = 0, addMoney = 1, buyBeverage
+    
+    var menuScript: String{
+        switch self {
+            case .returnChange:
+                return "잔돈반환종료하기"
+            case .addMoney:
+                return "금액추가"
+            case .buyBeverage:
+                return "음료구매"
+        }
+    }
+}
+
+enum MenuError: Error, MessagePrintable {
+    
+    case invalidForm
+    case noNumberGoods
+    case notMenu
+    case notInt
+    case notNegativeNumber
+    case notDefine
+    
+    private var manual: String {
+        let sample = "ex) 10,000원 투입 시 --> 1 10000\n     1번음료 구매 시 --> 2 1 \n     잔돈 반환 종료는 --> 0 "
+        
+        switch self {
+        case .invalidForm:
+            return "메뉴와 값 사이에 띄워서 입력해주세요.\n \(sample)"
+        case .noNumberGoods:
+            return "입력한 번호의 음료가 없습니다."
+        case .notMenu:
+            return "지원하는 메뉴를 입력해주세요."
+        case .notInt:
+            return "숫자로 입력해주세요.\n \(sample)"
+        case .notNegativeNumber:
+            return "숫자는 양수로 입력해주세요."
+        case .notDefine:
+            return "알수 없는 오류입니다."
+        }
+    }
+    
+    var message: String {
+        return "잘못 입력하셨습니다. \(self.manual)"
+    }
+    
+}
+
