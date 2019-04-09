8588da11e7aa1637ef55dd3f6f0180b3788b7a46
conyconydev <conyconydev@gmail.com>
2019-04-09 23:15:24 +0900

    메뉴에 대한 출력을 클로저로 구현하였으며, error 처리를 위해 throws 사용

--- VendingMachine/VendingMachine/View/Menu.swift
+++ VendingMachine/VendingMachine/View/Menu.swift
@@ -8,3 +8,36 @@
 
 import Foundation
 
+struct Menu {
+    
+    static func readMenu(input: String) throws -> (details: MenuScript, value: Int) {
+        let menuSelected = input.split(separator: " ")
+        
+        guard let firstScript = Int(menuSelected[0]) else {
+            throw MenuError.notInt
+        }
+        
+        guard let menu = MenuScript(rawValue: firstScript) else {
+            throw MenuError.notMenu
+        }
+
+        if Int(menuSelected[0]) == 0 {
+            return (menu,0)
+        }
+        
+        guard menuSelected.count == 2 else {
+            throw MenuError.invalidForm
+        }
+        
+        guard let value = Int(menuSelected[1]) else {
+            throw MenuError.notInt
+        }
+        
+        guard value > 0 else {
+            throw MenuError.notNegativeNumber
+        }
+        
+        return (menu, value)
+    }
+    
+}
