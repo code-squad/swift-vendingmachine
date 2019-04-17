45375d40a37810d5a7b818c94ca1e36eb4206693
김지나 <jinagold.k@gmail.com>
2019-04-17 18:13:35 +0900

    음료 객체 생성

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 
-let strawberry = Milk(brand: "빙그레", capacity: 240, price: 1000, productName: "딸기우유")
-let sprite = Soda(brand: "cocacola", capacity: 250, price: 500, productName: "스프라이트")
-let kanu = Coffee(brand: "맥심", capacity: 150, price: 1000, productName: "카누")
+let strawberry = Strawberry(grade: "A", brand: "빙그레", volume: 240, price: 1000, productName: "딸기우유")
+let cola = CocaCola(calorie: 100, brand: "cocacola", volume: 250, price: 500, productName: "코카콜라")
+let kanu = Kanu(temperature: "Hot", brand: "맥심", volume: 150, price: 1000, productName: "카누")
 
 print(strawberry)
-print(sprite)
+print(cola)
 print(kanu)
