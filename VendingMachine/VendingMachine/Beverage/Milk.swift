bb250f65baebb44769f4974286de6d035c3dfbdd
김지나 <jinagold.k@gmail.com>
2019-04-18 14:53:46 +0900

    우유의 등급 enum 생성

--- VendingMachine/VendingMachine/Beverage/Milk.swift
+++ VendingMachine/VendingMachine/Beverage/Milk.swift
@@ -8,21 +8,27 @@
 
 import Foundation
 
+enum MilkGrade {
+    case A
+    case B
+    case C
+}
+
 class Milk: Beverage {
-    private let grade: String
+    private let grade: MilkGrade
     
-    init(grade: String, brand: String, volume: Int, price: Int, productName: String, dateOfManufacture: Date = Date()) {
+    init(grade: MilkGrade, brand: String, volume: Int, price: Int, productName: String, dateOfManufacture: Date = Date()) {
         self.grade = grade
         super.init(brand: brand, volume: volume, price: price, productName: productName, dateOfManufacture: dateOfManufacture, expirationDate: dateOfManufacture + 432000)
     }
     
     func isGradeA() -> Bool {
-        return grade == "A"
+        return grade == MilkGrade.A
     }
 }
 
 class Strawberry: Milk {
-    override init(grade: String, brand: String, volume: Int, price: Int, productName: String, dateOfManufacture: Date = Date()) {
+    override init(grade: MilkGrade, brand: String, volume: Int, price: Int, productName: String, dateOfManufacture: Date = Date()) {
         super.init(grade: grade, brand: brand, volume: volume, price: price, productName: productName, dateOfManufacture: dateOfManufacture)
     }
 }
