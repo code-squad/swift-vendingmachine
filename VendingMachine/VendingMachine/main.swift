69d46c1508ebf87e44d39165074c83eca439431d
conyconydev <conyconydev@gmail.com>
2019-05-02 14:17:36 +0900

    update: 사용자 출력부분을 수정

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,61 +8,14 @@
 
 import Foundation
 
-func main() {
-    // motion: 자판기 작동을 위한 변수
-    var motion = true
-    let beverages = [Sprite(), Sprite(), Sprite(), CocaCola(), CocaCola(), CocaCola(), CocaCola(),ChocolateMilk(), ChocolateMilk(), BananaMilk(),CantataCoffee(), CantataCoffee(), CantataCoffee(), StarbucksCoffee(),StarbucksCoffee()]
-    
-    var vendingMachine = VendingMachine(startMoney: 0, list: Inventory(list: [:]))
-
-    beverages.forEach {
-        beverage in vendingMachine.add(beverage: beverage)
-    }
-    
-    while(motion == true) {
-        do {
-            try OutputView.start(vendingMachine)
-            let data = InputView.readInput()
-            let menu = try Menu.readCustomerMenu(input: data)
-            
-            switch menu.details {
-                case .returnChange:
-                    OutputView.moneyReturn(vendingMachine)
-                    motion = false
-                case .addMoney:
-                    let moneyInserted = menu.value
-                
-                    guard vendingMachine.isPut(cash: moneyInserted) else { continue }
-                    OutputView.showInsertion(money: moneyInserted)
-                case .buyBeverage:
-                    let listBuyable = vendingMachine.buyAvailableList()
-                    
-                    let selectedBeverage = menu.value - 1
-                    guard selectedBeverage < listBuyable.count else { throw MenuError.noNumberGoods }
-                    
-                    let selectedGoods = listBuyable[selectedBeverage]
-                    guard let beveragePurchas = vendingMachine.buyBeverage(package: selectedGoods) else { continue }
-                    
-                    OutputView.showPurchase(beverage: beveragePurchas)
-                
-            }
-            } catch let meg as MenuError {
-                OutputView.showMessage(error : meg)
-            } catch {
-                OutputView.showMessage(error: MenuError.notDefine)
-            }
-    }
-    
-}
-
-//main()
-
 func selectMode(vendingMachine: VendingMachine) -> SelectMode? {
     while true {
         do {
             OutputView.selectMode()
             let inputData = InputView.readInput()
-            if Menu.moveHighStep(input: inputData) { return nil }
+            if Menu.moveHighStep(input: inputData){
+                return nil
+            }
             let userPick = try Menu.readUserMode(input: inputData)
             switch userPick {
             case .manager:
@@ -78,8 +31,7 @@ func selectMode(vendingMachine: VendingMachine) -> SelectMode? {
     }
 }
 
-func testMain() {
-    var motion = true
+func main() {
     let beverages = [Sprite(), Sprite(), Sprite(), CocaCola(), CocaCola(), CocaCola(), CocaCola(),ChocolateMilk(), ChocolateMilk(), BananaMilk(),BananaMilk(),CantataCoffee(), CantataCoffee(), CantataCoffee(), StarbucksCoffee(),StarbucksCoffee()]
     
     var vendingMachine = VendingMachine(startMoney: 0, list: Inventory(list: [:]))
@@ -88,10 +40,10 @@ func testMain() {
         beverage in vendingMachine.add(beverage: beverage)
     }
     
-    while motion {
+    while true {
         guard var userPick = selectMode(vendingMachine: vendingMachine) else { return }
         userPick.perform()
     }
 }
 
-testMain()
+main()
