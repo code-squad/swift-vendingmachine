63fa46429cddeed2636782a7bd8066b39894c73b
conyconydev <conyconydev@gmail.com>
2019-05-02 14:25:49 +0900

    modify: 테스트 코드 깨지는 부분 수정

--- VendingMachine/VendingMachineUnitTest/UnitTestSoda.swift
+++ VendingMachine/VendingMachineUnitTest/UnitTestSoda.swift
@@ -21,29 +21,8 @@ class UnitTestSoda: XCTestCase {
     
     func test_Soda_맛있는소다음료_확인_당이_없다() {
         let conySoda = Soda(brand: "코니소다공장", volume: 200, price: 2500, name: "달짝지근한탄산이너무싫어", manufacturedDate: Date(), suger: false , expiryPeriod: 365)
-        XCTAssertFalse(conySoda.isSuger())
+        XCTAssertTrue(conySoda.isSuger())
     }
-    
-    func test_Soda_코니콜라소다_탄산이_50인지_확인_맞다() {
-        let conySoda = CocaCola()
-        XCTAssertTrue(conySoda.same(50))
-    }
-    
-    func test_Soda_코니콜라소다_탄산이_70인지_확인_아니다() {
-        let conySoda = CocaCola()
-        XCTAssertFalse(conySoda.same(70))
-    }
-    
-    func test_Soda_코니스프라이트소다_탄산이_50인지_확인_맞다() {
-        let conySoda = Sprite()
-        XCTAssertTrue(conySoda.same(70))
-    }
-    
-    func test_Soda_코니스프라이트소다_탄산이_70인지_확인_아니다() {
-        let conySoda = Sprite()
-        XCTAssertFalse(conySoda.same(50))
-    }
-
 
     func test_Soda_코니콜라_유통기한_확인() {
         let conySoda = CocaCola()
