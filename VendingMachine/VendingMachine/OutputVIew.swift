10a11ae5c1990e777d10d11dd4c2d338c099f985
김지나 <jinagold.k@gmail.com>
2019-05-10 15:35:38 +0900

    OutputView 생성
    1. 현재 투입금액에 대한 문구 출력해주는 메소드(currentStatus) 추가
    2.  메뉴 문구 출력해주는 메소드(menu) 추가
    3. 현재 보유중인 음료 정보 출력해주는 메소드(beverageList) 추가
    4. 사용자의 입력에 오류 발생 시 보유줄 에러 enum errorMessage 추가

---
+++ VendingMachine/VendingMachine/OutputVIew.swift
@@ -0,0 +1,35 @@
+//
+//  OutputVIew.swift
+//  VendingMachine
+//
+//  Created by 김지나 on 22/04/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+/// 에러메시지
+enum errorMessage: String {
+    case unselect = "원하는 번호를 선택해주세요"
+}
+
+/// 출력
+struct OutputView {
+    static func currentStatus(_ money: Int) {
+        print("현재 투입한 금액이 \(money)원입니다. 다음과 같은 음료가 있습니다.")
+    }
+    
+    static func menu() {
+        print("""
+             1. 금액추가
+             2. 음료구매
+             """)
+    }
+    
+    // 현재 보유 중인 음료수 내역
+    func beverageList(_ beverage:[Beverage]) {
+        for drinks in 0 ..< beverage.count {
+            print("\(drinks + 1)) \(beverage[drinks].beverageName) / \(beverage[drinks].beveragePrice)원 / \(beverage.count)개")
+        }
+    }
+}
