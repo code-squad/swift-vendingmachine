df3d4592cc698e442cc4fbd501a8e8ccc0d43518
김지나 <jinagold.k@gmail.com>
2019-05-14 12:00:32 +0900

    수정된 inventory메소드 반영

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,9 +9,10 @@
 import Foundation
 
 var vendingMachine = VendingMachine()
+vendingMachine.inventory()
 while(true) {
     OutputView.currentStatus(vendingMachine.balance()) // 현재 투입한 금액이 0원입니다. 다음과 같은 음료가 있습니다.
-    OutputView().beverageList(vendingMachine.drinks())
+    OutputView().beverageList(vendingMachine) 
     OutputView.menu() // 1. 금액추가 2. 음료구매
     let input = InputView().selectMenu() // 메뉴를 선택하도록 입력 받는다.
     
@@ -19,4 +20,3 @@ while(true) {
         vendingMachine.insert(money: input)
     }
 }
-
