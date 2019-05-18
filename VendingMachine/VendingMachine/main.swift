4e2ddd38fea632c0ae4a9e9cd7463cd81873a668
김지나 <jinagold.k@gmail.com>
2019-05-19 00:36:23 +0900

    메인 수정
    1. 메인 실행 방법 변경 while -> function
    2. 에러 검사 후 출력 부 추가

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -10,13 +10,25 @@ import Foundation
 
 var vendingMachine = VendingMachine()
 vendingMachine.inventory()
-while(true) {
+
+func main() {
     OutputView.currentStatus(vendingMachine.balance()) // 현재 투입한 금액이 0원입니다. 다음과 같은 음료가 있습니다.
-    OutputView().beverageList(vendingMachine) 
+    OutputView().beverageList(vendingMachine)
     OutputView.menu() // 1. 금액추가 2. 음료구매
     let input = InputView().selectMenu() // 메뉴를 선택하도록 입력 받는다.
     
-    if input.first == "1" {
-        vendingMachine.insert(money: input)
+    OutputView().printError(inCorrect(input))
+
+    switch input.first {
+        case "1": vendingMachine.insert(money: input)
+        case "2":
+            guard notEnoughBalance(of: vendingMachine, input) == .notError else { return OutputView().printError(notEnoughBalance(of: vendingMachine, input)) }
+            vendingMachine.sell(beverage: input)
+        default: break
     }
+    
+}
+while true {
+    main()
+
 }
