e57bba8c2cdd06985b911df9e91002e1f446a9cb
김지나 <jinagold.k@gmail.com>
2019-05-19 15:05:30 +0900

    선택한 음료의 구매 가능한 재고가 있는지 확인하는 메소드(ourOfStock) 생성

--- VendingMachine/VendingMachine/Error.swift
+++ VendingMachine/VendingMachine/Error.swift
@@ -9,7 +9,6 @@
 import Foundation
 
 enum Error: String {
-
     case inCorrect = "❌ 메뉴를 확인해주세요"
     case notEnoughBalance = "❌ 잔액이 부족합니다."
     case outOfStock = "❌ 재고가 부족합니다."
@@ -18,7 +17,6 @@ enum Error: String {
     func message() -> String {
         return self.rawValue
     }
-
 }
 
 // 올바른 메뉴 선택 확인
@@ -28,7 +26,7 @@ func inCorrect(_ input: String) -> Error {
     return .notError
 }
 
-// 잔액 확인
+// 현재의 잔액으로 선택한 음료의 구매 가능 여부 확인
 func notEnoughBalance(of machine: VendingMachine, _ input: String) -> Error {
     let balance = machine.balance()
     let status = machine.currentBeverageStatus()
@@ -41,5 +39,14 @@ func notEnoughBalance(of machine: VendingMachine, _ input: String) -> Error {
     return .notError
 }
 
-// 재고 확인
-// 메뉴 제대로 입력 안됬을 때
+// 선택한 음료의 구매 가능한 재고의 존재 여부
+func outOfStock(machine: VendingMachine, _ input: String) -> Error {
+    let status = machine.currentBeverageStatus()
+    let selectBeverage = Int(input.dropFirst(2))! - 1
+    for drinks in status {
+        if drinks.key == selectBeverage {
+            guard status[drinks.key]!.2 != 0 else { return .outOfStock }
+        }
+    }
+    return .notError
+}
