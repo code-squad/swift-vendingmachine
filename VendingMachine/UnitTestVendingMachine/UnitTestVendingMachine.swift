fb93136446787456a0d49534e351fd5adcc0bc49
김지나 <jinagold.k@gmail.com>
2019-05-20 19:14:21 +0900

    Unit test VendgingMachine

--- VendingMachine/UnitTestVendingMachine/UnitTestVendingMachine.swift
+++ VendingMachine/UnitTestVendingMachine/UnitTestVendingMachine.swift
@@ -9,46 +9,79 @@
 import XCTest
 
 class UnitTestVendingMachine: XCTestCase {
-    func testValidate() {
-        let date = Date()
-        XCTAssertTrue(Beverage.validate(expireDate:date + 432000))
+    var vendingMachine: VendingMachine!
+    
+    override func setUp() {
+        vendingMachine = VendingMachine()
+        vendingMachine.inventory()
     }
     
-    func testPassValidate() {
-        let date = Date()
-        XCTAssertFalse(Beverage.validate(expireDate:date - 432000))
+    /// 동작
+    // 돈 넣기
+    func testIncreaseBalance() {
+        vendingMachine.insert(money: "1 1000")
+        XCTAssertEqual(vendingMachine.balance(),1000)
     }
     
-    ///Milk
-    func testMilkGradeA() {
-        let milk = Milk(grade: .A, brand: "빙그레", volume: 240, price: 1000, productName: "빙그레딸기우유")
-        XCTAssertTrue(milk.isGradeA())
+    // 돈 더 넣기
+    func testAddBalance() {
+        vendingMachine.insert(money: "1 1000")
+        vendingMachine.insert(money: "1 1000")
+        XCTAssertEqual(vendingMachine.balance(),2000)
     }
     
-    func testMilkGradeB() {
-        let milk = Milk(grade: .B, brand: "오뚜기", volume: 240, price: 1000, productName: "딸기우유")
-        XCTAssertFalse(milk.isGradeA())
+    // 음료 하나 구매 후 재고 하나 줄이기
+    func testDecreaseBeverageCount() {
+        vendingMachine.insert(money: "1 1000")
+        vendingMachine.sell(beverage: "2 2")
+        XCTAssertEqual(vendingMachine.currentBeverageStatus()[1]!.2,9)
     }
     
-    ///Soda
-    func testCalorieIsHigh() {
-        let soda = Soda(calorie: 500, brand: "cocacola", volume: 300, price: 2000, productName: "코카콜라")
-        XCTAssertTrue(soda.isHigh())
+    // 돈 넣고 음료 구매 후 잔액 차감하기
+    func testDecreaseBalacne() {
+        vendingMachine.insert(money: "1 1000")
+        vendingMachine.sell(beverage: "2 2")
+        XCTAssertEqual(vendingMachine.balance(),0)
     }
     
-    func testCalorieIsNotHigh() {
-        let soda = Soda(calorie: 200, brand: "cocacola", volume: 300, price: 2000, productName: "제로코카콜라")
-        XCTAssertFalse(soda.isHigh())
+    /// 에러
+    // 메뉴 입력 확인
+    func testIncorrectNotError() {
+        let error = incorrect("1 1000")
+        XCTAssertEqual(error, .notError)
     }
     
-    ///Coffee
-    func testHotCoffee() {
-        let coffee = Coffee(ice: false, brand: "맥심", volume: 150, price: 1000, productName: "카누")
-        XCTAssertTrue(coffee.isHot())
+    func testIncorrectError() {
+        let error = incorrect("3 1000")
+        XCTAssertEqual(error, .incorrect)
     }
     
-    func testIceCoffee() {
-        let coffee = Coffee(ice: true, brand: "맥심", volume: 150, price: 1000, productName: "카누")
-        XCTAssertFalse(coffee.isHot())
+    // 현재 잔액으로 음료 구매
+    func testEnoughBalanceError() {
+        vendingMachine.insert(money: "1 10000")
+        let error = notEnoughBalance(of: vendingMachine, "2 2")
+        XCTAssertEqual(error, .notError)
+    }
+    
+    func testNotEnoughBalanceError() {
+        //vendingMachine.insert(money: "1 100")
+        // 잔액 = 0
+        let error = notEnoughBalance(of: vendingMachine, "2 2")
+        XCTAssertEqual(error, .notEnoughBalance)
+    }
+    
+    // 음료의 재고 확인
+    func testCanBuy() {
+        let error = outOfStock(machine: vendingMachine, "2 2")
+        XCTAssertEqual(error, .notError)
+    }
+    
+    func testCannotBuy() {
+        vendingMachine.insert(money: "1 10000")
+        for _ in 0 ... 9 {
+            vendingMachine.sell(beverage: "2 2")
+        }
+        let error = outOfStock(machine: vendingMachine, "2 2")
+        XCTAssertEqual(error, .outOfStock)
     }
 }
