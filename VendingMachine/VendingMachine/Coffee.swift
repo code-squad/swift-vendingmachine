cebe943167d68cb3625826c998a9e6250745b89e
kiljh <kiljh@me.com>
2019-06-18 16:55:46 +0900

    생성 - Coffee 클래스 생성

---
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee.swift
+//  VendingMachine
+//
+//  Created by joon-ho kil on 6/18/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coffee {
+    var hot: Bool
+    
+    func isHot() -> Bool {
+        return hot
+    }
+}
