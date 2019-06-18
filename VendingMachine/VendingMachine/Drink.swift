dc15521d8be060fdf2bf9adcf133b0a4ad7d210f
kiljh <kiljh@me.com>
2019-06-18 15:46:32 +0900

    생성 - 음료수들의 부모 클래스 생성

--- VendingMachine/VendingMachine/Drink.swift
+++ VendingMachine/VendingMachine/Drink.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class Drink: CustomStringConvertible {
+    private let brand: String
+    private let ml: Int
+    private var price: Int
+    private let name: String
+    private let productDate: Date
+    
+    var description: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        
+        return "\(brand), \(ml)ml, \(price)원, \(name), \(dateFormatter.string(from: productDate))"
+    }
+    
+    init(brand: String, ml: Int, price: Int, name: String, productDate: String) {
+        self.brand = brand
+        self.ml = ml
+        self.price = price
+        self.name = name
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        
+        if let date = dateFormatter.date(from: productDate) {
+            self.productDate = date
+        } else {
+            self.productDate = Date.init()
+        }
+    }
+}
