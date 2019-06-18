c40b50d7387300ee7352f9aba013b2d828620002
kiljh <kiljh@me.com>
2019-06-18 16:11:36 +0900

    수정 - name 제외한 이니셜라이저로 수정

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 func main() {
-    let bananaMilk1 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, name: "빙그레 바나나우유", productDate: "20190301")
-    let bananaMilk2 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, name: "빙그레 바나나우유", productDate: "20190405")
-    let fanta = Fanta(brand: "롯데", ml: 350, price: 2000, name: "오렌지 환타", productDate: "20190505")
-    let top = TOP(brand: "맥심", ml: 400, price: 3000, name: "TOP아메리카노", productDate: "20190606")
+    let bananaMilk1 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190301")
+    let bananaMilk2 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405")
+    let fanta = Fanta(brand: "롯데", ml: 350, price: 2000, productDate: "20190505")
+    let top = TOP(brand: "맥심", ml: 400, price: 3000, productDate: "20190606")
     
     print(bananaMilk1)
     print(bananaMilk2)
