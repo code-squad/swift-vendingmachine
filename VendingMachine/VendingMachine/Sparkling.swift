c1ca7d17084b23d538a6ae693082b74ac6edb32c
kiljh <kiljh@me.com>
2019-06-18 16:49:14 +0900

    생성 - 탄산음료 클래스 생성

---
+++ VendingMachine/VendingMachine/Sparkling.swift
@@ -0,0 +1,14 @@
+//
+//  Sparkling.swift
+//  VendingMachine
+//
+//  Created by joon-ho kil on 6/18/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Sparkling: Drink {
+    isLowCalorie()
+    
+}
