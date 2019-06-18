831068286af33efe586b272ce81ab245b5c309e5
kiljh <kiljh@me.com>
2019-06-18 17:05:18 +0900

    수정 - 출력할 때 앞에 상수명 추가

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,15 +9,15 @@
 import Foundation
 
 func main() {
-    let bananaMilk1 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190301", farmCode: 5)
-    let bananaMilk2 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405", farmCode: 3)
+    let bananaMilk = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190301", farmCode: 5)
+    let strawberryMilk = StrawberryMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405", farmCode: 3)
     let fanta = Fanta(brand: "롯데", ml: 350, price: 2000, productDate: "20190505")
     let top = TOP(brand: "맥심", ml: 400, price: 3000, productDate: "20190606", hot: false)
     
-    print(bananaMilk1)
-    print(bananaMilk2)
-    print(fanta)
-    print(top)
+    print("bananaMilk - \(bananaMilk)")
+    print("strawberryMilk - \(strawberryMilk)")
+    print("fanta - \(fanta)")
+    print("top - \(top)")
 }
 
 main()
