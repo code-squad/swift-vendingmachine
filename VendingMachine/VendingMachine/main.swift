046427bd8ce68bb3de9c0df8122e9858a83f6978
kiljh <kiljh@me.com>
2019-06-18 16:43:05 +0900

    수정 - 농장코드 추가에 의한 수정

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 func main() {
-    let bananaMilk1 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190301")
-    let bananaMilk2 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405")
+    let bananaMilk1 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190301", farmCode: 5)
+    let bananaMilk2 = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405", farmCode: 3)
     let fanta = Fanta(brand: "롯데", ml: 350, price: 2000, productDate: "20190505")
     let top = TOP(brand: "맥심", ml: 400, price: 3000, productDate: "20190606")
     
