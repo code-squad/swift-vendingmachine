0a57af99d3ca0fd27aa8e03fd539fc3f40d00c19
kiljh <kiljh@me.com>
2019-06-19 12:27:28 +0900

    추가 - TOP 에 속성 추가

--- VendingMachine/VendingMachine/TOP.swift
+++ VendingMachine/VendingMachine/TOP.swift
@@ -9,7 +9,15 @@
 import Foundation
 
 class TOP: Coffee {
+    private var advertisingModel: String
+    
     init(brand: String, ml: Int, price: Int, productDate: String, hot: Bool, expirationDate: String) {
+        advertisingModel = "원빈"
+        
         super.init(brand: brand, ml: ml, price: price, name: "TOP아메리카노", productDate: productDate, hot: hot, barcode: Barcode.upc(60, 70, 80, 100), expirationDate: expirationDate)
+    }
+    
+    func isAdvertisingModel(_ name: String) -> Bool {
+        return advertisingModel == name
     }
 }
