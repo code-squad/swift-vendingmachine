bfde26b033fcee10b5b57424a87e444e30f38ab1
kiljoonho <kiljh@me.com>
2019-06-19 16:39:18 +0900

    추가 - 핫식스, 펩시콜라 생성

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -13,11 +13,9 @@ func main() {
     let strawberryMilk = StrawberryMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405", farmCode: 3, expirationDate: "20190925")
     let fanta = Fanta(brand: "롯데", ml: 350, price: 2000, productDate: "20190505", orangeIncense: 0.7, expirationDate: "20190921")
     let top = TOP(brand: "맥심", ml: 400, price: 3000, productDate: "20190606", hot: false, expirationDate: "20190920")
+    let hot6 = Hot6(brand: "롯데", ml: 240, price: 1000, productDate: "20190529", expirationDate: "20191029")
+    let pepsiCoke = PepsiCoke(brand: "펩시", ml: 255, price: 1200, productDate: "20190610", expirationDate: "20191010")
     
-    print("bananaMilk - \(bananaMilk)")
-    print("strawberryMilk - \(strawberryMilk)")
-    print("fanta - \(fanta)")
-    print("top - \(top)")
 }
 
 main()
