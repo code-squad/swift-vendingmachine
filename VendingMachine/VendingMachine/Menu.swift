5ca4353e9ef6df271dc1acc5cfb762929e110bc6
kiljoonho <kiljh@me.com>
2019-06-19 19:28:07 +0900

    생성 - Menu enum

---
+++ VendingMachine/VendingMachine/Menu.swift
@@ -0,0 +1,14 @@
+//
+//  Menu.swift
+//  VendingMachine
+//
+//  Created by joon-ho kil on 6/19/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum Menu: Int {
+    case insertCoin = 1
+    case buyDrink = 2
+}
