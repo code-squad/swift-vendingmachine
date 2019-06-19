81287b5032316dca3aad4c8661b3b8886090bdb4
kiljoonho <kiljh@me.com>
2019-06-19 19:29:13 +0900

    추가 - 자판기 메서드들 테스트 구현

--- VendingMachine/UnitTestVendingmachine/UnitTestVendingmachine.swift
+++ VendingMachine/UnitTestVendingmachine/UnitTestVendingmachine.swift
@@ -77,11 +77,125 @@ class UnitTestVendingmachine: XCTestCase {
         XCTAssertFalse(strawberryMilk.isLessthanStrawberryContainPercent(0.4))
     }
     
-    func testVendingMachineInsertCoint () {
+    func testInsertCoint () {
         XCTAssertEqual(vendingMachine.getBalance(), 0)
         vendingMachine.insertCoint(500)
         XCTAssertEqual(vendingMachine.getBalance(), 500)
         vendingMachine.insertCoint(1000)
         XCTAssertEqual(vendingMachine.getBalance(), 1500)
+    }
+    
+    func testGetStockList () {
+        let milkAmount = 20
+        let fantaAmount = 10
+        let topAmount = 40
+        let hot6Amount = 6
+        let pepsiCokeAmount = 30
+        
+        let stockListCompared = [bananaMilk: milkAmount, strawberryMilk: milkAmount, fanta: fantaAmount, top: topAmount, hot6: hot6Amount, pepsiCoke: pepsiCokeAmount]
+        
+        XCTAssertNotEqual(vendingMachine.getStockList(), stockListCompared)
+        
+        vendingMachine.supply(bananaMilk, amount: milkAmount)
+        vendingMachine.supply(strawberryMilk, amount: milkAmount)
+        
+        XCTAssertEqual(vendingMachine.getStockList(), [bananaMilk: milkAmount, strawberryMilk: milkAmount])
+        XCTAssertNotEqual(vendingMachine.getStockList(), stockListCompared)
+        
+        vendingMachine.supply(fanta, amount: fantaAmount)
+        vendingMachine.supply(top, amount: topAmount)
+        vendingMachine.supply(hot6, amount: hot6Amount)
+        vendingMachine.supply(pepsiCoke, amount: pepsiCokeAmount)
+    
+        XCTAssertEqual(vendingMachine.getStockList(), stockListCompared)
+    }
+    
+    func testGetBuyableDrinkList () {
+        XCTAssertEqual(vendingMachine.getBuyableDrinkList(), [])
+        
+        vendingMachine.insertCoint(1300)
+        
+        XCTAssertEqual(vendingMachine.getBuyableDrinkList(), [])
+        
+        vendingMachine.supply(bananaMilk, amount: 20)
+        vendingMachine.supply(strawberryMilk, amount: 20)
+        
+        XCTAssertEqual(vendingMachine.getBuyableDrinkList(), [bananaMilk, strawberryMilk])
+        
+        vendingMachine.supply(fanta, amount: 10)
+        vendingMachine.supply(top, amount: 40)
+        vendingMachine.supply(hot6, amount: 6)
+        vendingMachine.supply(pepsiCoke, amount: 30)
+        
+        XCTAssertEqual(vendingMachine.getBuyableDrinkList(), [bananaMilk, strawberryMilk, hot6, pepsiCoke])
+        
+        vendingMachine.insertCoint(800)
+        
+        XCTAssertEqual(vendingMachine.getBuyableDrinkList(), [bananaMilk, strawberryMilk, fanta, hot6, pepsiCoke])
+        
+         vendingMachine.insertCoint(1000)
+
+        XCTAssertEqual(vendingMachine.getBuyableDrinkList(), [bananaMilk, strawberryMilk, fanta, top, hot6, pepsiCoke])
+    }
+    
+    func testBuy () {
+        XCTAssertThrowsError(try vendingMachine.buy(bananaMilk))
+        
+        vendingMachine.supply(bananaMilk, amount: 20)
+        vendingMachine.supply(strawberryMilk, amount: 20)
+        
+        XCTAssertThrowsError(try vendingMachine.buy(bananaMilk))
+        
+        vendingMachine.insertCoint(2000)
+        
+        XCTAssertEqual(vendingMachine.getStockList(), [bananaMilk: 20, strawberryMilk: 20])
+        
+        XCTAssertNoThrow(try vendingMachine.buy(bananaMilk))
+        
+        XCTAssertEqual(vendingMachine.getStockList(), [bananaMilk: 19, strawberryMilk: 20])
+    }
+    
+    func testGetBalance () {
+        vendingMachine.supply(bananaMilk, amount: 20)
+        vendingMachine.supply(strawberryMilk, amount: 20)
+        
+        XCTAssertEqual(vendingMachine.getBalance(), 0)
+        
+        vendingMachine.insertCoint(2000)
+        XCTAssertEqual(vendingMachine.getBalance(), 2000)
+        
+        XCTAssertNoThrow(try vendingMachine.buy(bananaMilk))
+        XCTAssertEqual(vendingMachine.getBalance(), 700)
+    }
+    
+    func testGetExpiredDrinkList () {
+        vendingMachine.supply(bananaMilk, amount: 20)
+        vendingMachine.supply(strawberryMilk, amount: 20)
+        vendingMachine.supply(pepsiCoke, amount: 30)
+        
+        XCTAssertEqual(vendingMachine.getExpiredDrinkList() , [bananaMilk, strawberryMilk])
+    }
+    
+    func testGetHotDrinkList () {
+        vendingMachine.supply(top, amount: 20)
+        vendingMachine.supply(topHot, amount: 20)
+        
+        XCTAssertEqual(vendingMachine.getHotDrinkList() , [topHot])
+    }
+    
+    func testGetSellList () {
+        vendingMachine.supply(top, amount: 20)
+        vendingMachine.supply(topHot, amount: 20)
+        vendingMachine.supply(bananaMilk, amount: 20)
+        
+        vendingMachine.insertCoint(10000)
+        
+        XCTAssertNoThrow(try vendingMachine.buy(topHot))
+        XCTAssertNoThrow(try vendingMachine.buy(topHot))
+        
+        XCTAssertEqual(vendingMachine.getSellList(), [topHot,topHot])
+        
+        XCTAssertNoThrow(try vendingMachine.buy(bananaMilk))
+        XCTAssertEqual(vendingMachine.getSellList(), [topHot,topHot,bananaMilk])
     }
 }
