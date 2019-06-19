e7a4333a0fe560b3e1b6f85b35a470e677887517
kiljoonho <kiljh@me.com>
2019-06-19 19:44:29 +0900

    수정 - InputView 구조체 완료

--- VendingMachine/VendingMachine/InputView.swift
+++ VendingMachine/VendingMachine/InputView.swift
@@ -9,24 +9,23 @@
 import Foundation
 
 struct InputView {
-    private func printMenuInputGuidance (vendingMachine: VendingMachine) {
+    private func printMenuInputGuidance (_ vendingMachine: VendingMachine) {
         print("현재 투입한 금액이 \(vendingMachine.getBalance())원입니다. 다음과 같은 음료가 있습니다.")
-        print()
-        print("자판기 메뉴를 선택하세요.")
-        print("1. 금액 넣기") // 자판기 금액을 원하는 금액만큼 올리는 메소드
+        print("=> ", terminator: "")
+        let drinks = vendingMachine.getStockList()
+        for (name, amount) in drinks {
+            print("\(name)(\(amount)) ", terminator: "")
+        }
         
+        print("\n1. 금액추가")
+        print("2. 음료구매")
         print("0. 종료")
     }
     
-    현재 투입한 금액이 0원입니다. 다음과 같은 음료가 있습니다.
-    => 딸기우유(3개) 바나나우유(2개) 팹시콜라(5개) TOP커피(3개) 핫식스(4개)
-    1. 금액추가
-    2. 음료구매
-    
-    func readInput () -> String {
+    func readInput (_ vendingMachine: VendingMachine) -> String {
         let menu: String
         
-        printMenuInputGuidance()
+        printMenuInputGuidance(vendingMachine)
         print("> ", terminator: "")
         menu = readLine() ?? "0"
         
