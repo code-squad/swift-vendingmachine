9de8c391526ff1a0bd7e3f7996ab30ad52e40f0c
김지나 <jinagold.k@gmail.com>
2019-06-20 20:45:11 +0900

    수정
    1. 복잡한 currentBeverage를 Inventory구조체를 가지도록 함
    2. 음료 구매 즉시 내역 출력 해주는 구문 삭제
    3. 특정 음료 구분하는 notValiDate, hotBeverage 메소드 리턴 타입 변경 [String] -> [Beverage]

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -18,7 +18,7 @@ struct VendingMachine {
                 TOP(flavor: "라떼", ice: false, brand: "맥심", volume: 200, price: 1400, productName: "TOP카페라떼")]
     }
     private var currentBalance: Int
-    private var currentBeverage = [Int:(String,Int,Int)]()
+    private var currentBeverage = [Inventory]()
     private var purchase = [String]()
     
     init(currentBalance: Int = 0) {
@@ -42,69 +42,58 @@ struct VendingMachine {
     
 
     /// 음료 내역
-    // beverage 배열을 이용해서 [종류개수:(음료명,가격,재고)] 형식의 재고상태를 보여주는 딕셔너리를 만듦
     mutating func inventory() {
-        for drinks in 0 ..< beverage.count {
-            self.currentBeverage.updateValue((beverage[drinks].beverageName,beverage[drinks].beveragePrice,10), forKey: drinks)
+        for i in beverage {
+            self.currentBeverage.append(Inventory(beverageName: i.beverageName, beveragaPrice: i.beveragePrice, beverageCount: 10))
         }
     }
     // 현재 음료 현황 리턴
-    func currentBeverageStatus() -> [Int:(String,Int,Int)] {
+    func currentBeverageStatus() -> [Inventory] {
         return currentBeverage
     }
     
     
     /// 음료수 구매
     // 구매한 음료 처리
-    mutating func sell(beverage: String) {
-        let selectBeverageNum = Int(beverage.dropFirst(2))! - 1
-        for inner in currentBeverage {
-            if inner.key == selectBeverageNum {
-                subtract(from: inner.key)
-                purchaseList(from: inner.key)
-                deduct(money: inner.key)
-                OutputView().printPurchase(productName: currentBeverage[inner.key]!.0, price: currentBeverage[inner.key]!.1)
+    mutating func sell(beverageNumber: Int) {
+        for inner in 0 ..< currentBeverage.count {
+            if beverageNumber == inner {
+                subtract(from: inner)
+                purchaseList(from: inner)
+                deduct(money: inner)
             }
         }
     }
     
     // 시작이후 구매 상품 이력을 배열로 리턴하는 메소드
     private mutating func purchaseList(from productNumber: Int) {
-        self.purchase.append(currentBeverage[productNumber]!.0)
+        self.purchase.append(currentBeverage[productNumber].beverageName)
     }
 
     // 재고 마이너스
     private mutating func subtract(from inventory: Int) {
-        self.currentBeverage[inventory]!.2 -= 1
+        self.currentBeverage[inventory].beverageCount -= 1
     }
     
     // 잔액 마이너스
     private mutating func deduct(money: Int) {
-        self.currentBalance = currentBalance - currentBeverage[money]!.1
+        self.currentBalance = currentBalance - currentBeverage[money].beveragaPrice
     }
     
     
     /// 특정 음료 구분
     // 유통기한이 지난 재고만 리턴하는 메소드
-    private func notValidDate() -> [String] {
-        var pastExpiration = [String]()
-        for date in beverage{
-            if date.validate() == false {
-                pastExpiration.append(date.beverageName)
-            }
-        }
-        return pastExpiration
+    private func notValidDate() -> [Beverage] {
+        return beverage.filter{ $0.validate() == false }
     }
 
     // 따뜻한 음료만 리턴하는 메소드
-    private func hotBeverage() -> [String] {
-        var hot = [String]()
-        for drinks in beverage {
-            guard let coffee = drinks as? Coffee else {return [""]}
-            if coffee.isHot() == true {
-                hot.append(drinks.beverageName)
-            }
+    private func hotBeverage() -> [Beverage] {
+        let coffee = beverage.filter{
+            let drink = $0 as? Coffee
+                if drink?.isHot() == true { return true }
+            return false
         }
-        return hot
+        return coffee
     }
 }
