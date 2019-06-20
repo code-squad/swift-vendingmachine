86e5b5b18c5c9fa939df59802f5a6db1d79507d9
김지나 <jinagold.k@gmail.com>
2019-06-20 20:34:36 +0900

    UnitTest

--- VendingMachine/UnitTestVendingMachine/UnitTestVendingMachine.swift
+++ VendingMachine/UnitTestVendingMachine/UnitTestVendingMachine.swift
@@ -19,69 +19,80 @@ class UnitTestVendingMachine: XCTestCase {
     /// 동작
     // 돈 넣기
     func testIncreaseBalance() {
-        vendingMachine.insert(money: "1 1000")
+        vendingMachine.insert(money: 1000)
         XCTAssertEqual(vendingMachine.balance(),1000)
     }
     
     // 돈 더 넣기
     func testAddBalance() {
-        vendingMachine.insert(money: "1 1000")
-        vendingMachine.insert(money: "1 1000")
+        vendingMachine.insert(money: 1000)
+        vendingMachine.insert(money: 1000)
         XCTAssertEqual(vendingMachine.balance(),2000)
     }
     
     // 음료 하나 구매 후 재고 하나 줄이기
     func testDecreaseBeverageCount() {
-        vendingMachine.insert(money: "1 1000")
-        vendingMachine.sell(beverage: "2 2")
-        XCTAssertEqual(vendingMachine.currentBeverageStatus()[1]!.2,9)
+        vendingMachine.insert(money: 1000)
+        vendingMachine.sell(beverageNumber: 2)
+        let drink = vendingMachine.currentBeverageStatus()
+        XCTAssertEqual(drink[2].beverageCount,9)
     }
     
     // 돈 넣고 음료 구매 후 잔액 차감하기
     func testDecreaseBalacne() {
-        vendingMachine.insert(money: "1 1000")
-        vendingMachine.sell(beverage: "2 2")
+        vendingMachine.insert(money: 2000)
+        vendingMachine.sell(beverageNumber: 2)
         XCTAssertEqual(vendingMachine.balance(),0)
     }
     
     /// 에러
     // 메뉴 입력 확인
-    func testIncorrectNotError() {
-        let error = incorrect("1 1000")
-        XCTAssertEqual(error, .notError)
-    }
-    
     func testIncorrectError() {
-        let error = incorrect("3 1000")
-        XCTAssertEqual(error, .incorrect)
+        do {
+            let _ = try incorrect("3 1000")
+        } catch let error {
+            XCTFail("\(error as! InputError)")
+        }
     }
     
     // 현재 잔액으로 음료 구매
-    func testEnoughBalanceError() {
-        vendingMachine.insert(money: "1 10000")
-        let error = notEnoughBalance(of: vendingMachine, "2 2")
-        XCTAssertEqual(error, .notError)
+    func testEnoughBalance() {
+        vendingMachine.insert(money: 10000)
+        do {
+            let _ = try CheckAvailability(of: vendingMachine, 2)
+        } catch let error {
+            XCTFail("\(error as! InputError)")
+        }
     }
     
     func testNotEnoughBalanceError() {
-        //vendingMachine.insert(money: "1 100")
-        // 잔액 = 0
-        let error = notEnoughBalance(of: vendingMachine, "2 2")
-        XCTAssertEqual(error, .notEnoughBalance)
+        vendingMachine.insert(money: 100)
+        do {
+            let _ = try CheckAvailability(of: vendingMachine, 2)
+        } catch let error {
+            XCTFail("\(error as! InputError)")
+        }
     }
     
     // 음료의 재고 확인
     func testCanBuy() {
-        let error = outOfStock(machine: vendingMachine, "2 2")
-        XCTAssertEqual(error, .notError)
+        vendingMachine.insert(money: 2000)
+        do {
+            let _ = try CheckAvailability(of: vendingMachine, 2)
+        } catch let error {
+            XCTFail("\(error as! InputError)")
+        }
     }
     
     func testCannotBuy() {
-        vendingMachine.insert(money: "1 10000")
+        vendingMachine.insert(money: 10000)
         for _ in 0 ... 9 {
-            vendingMachine.sell(beverage: "2 2")
+            vendingMachine.sell(beverageNumber: 2)
         }
-        let error = outOfStock(machine: vendingMachine, "2 2")
-        XCTAssertEqual(error, .outOfStock)
+        do {
+            let _ = try CheckAvailability(of: vendingMachine, 2)
+        } catch let error {
+            XCTFail("\(error as! InputError)")
+        }
     }
 }
