58398464152ff0a625f2c42acc7966809a596d8e
kiljoonho <kiljh@me.com>
2019-06-20 10:46:41 +0900

    수정 - OutputView 구현

--- VendingMachine/VendingMachine/OutputView.swift
+++ VendingMachine/VendingMachine/OutputView.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+struct OutputView {
+    static func printMenuInputGuidance (_ vendingMachine: VendingMachine) {
+        print("현재 투입한 금액이 \(vendingMachine.getBalance())원입니다. 다음과 같은 음료가 있습니다.")
+        print("=> ", terminator: "")
+        let drinks = vendingMachine.getStockList()
+        for (name, amount) in drinks {
+            print("\(name)(\(amount)) ", terminator: "")
+        }
+        
+        print("\n1. 금액추가")
+        print("2. 음료구매")
+        print("0. 종료")
+        
+        print("> ", terminator: "")
+    }
+    
+    static func printBuyableDrinkList (_ vendingMachine: VendingMachine) throws {
+        let buyableDrinkList = vendingMachine.getBuyableDrinkList()
+        if buyableDrinkList.count == 0 {
+            throw BuyError.NotEnoughBalance
+        }
+        
+        for (index, drink) in buyableDrinkList.enumerated() {
+            print("\(index+1)) \(drink) \(drink.getPrice())원")
+        }
+    }
+}
