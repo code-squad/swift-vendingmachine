dc172a97026e0efaf756c570d6980ddc90c2a3e5
김지나 <jinagold.k@gmail.com>
2019-06-20 20:46:32 +0900

    수정
    1. 단순 출력 메소드 static으로 변경
    2. 불필요한 구문 삭제

--- VendingMachine/VendingMachine/OutputVIew.swift
+++ VendingMachine/VendingMachine/OutputVIew.swift
@@ -10,11 +10,11 @@ import Foundation
 
 /// 출력
 struct OutputView {
-    func currentStatus(_ money: Int) {
+    static func currentStatus(_ money: Int) {
         print("현재 투입한 금액이 \(money)원입니다. 다음과 같은 음료가 있습니다.")
     }
     
-    func menu() {
+    static func menu() {
         print("""
              1. 금액추가
              2. 음료구매
@@ -24,21 +24,21 @@ struct OutputView {
     // 현재 보유 중인 음료수 내역
     func beverageList(_ beverage:VendingMachine) {
         let drinks = beverage.currentBeverageStatus()
-        let sortedDrinks = drinks.keys.sorted(by: <)
-        for inner in sortedDrinks {
-            print("\(inner + 1)) \(drinks[inner]!.0) / \(drinks[inner]!.1)원 / \(drinks[inner]!.2)개")
+        for inner in 0 ..< drinks.count {
+            print("\(inner + 1)) \(drinks[inner].beverageName) \(drinks[inner].beveragaPrice)원 \(drinks[inner].beverageCount)개")
         }
     }
     
     // 음료 구매 후 출력문
-    func printPurchase(productName: String, price: Int) {
+    static func printPurchase(productName: String, price: Int) {
         print("\(productName)를 구매하셨습니다. \(price)원을 차감합니다.\n")
     }
     
     // 에러 출력
-    func printError(_ error: Error) {
-        if error != .notError {
+    func printError(_ error: InputError) {
             print(error.message())
-        }
     }
 }
+
+
+
