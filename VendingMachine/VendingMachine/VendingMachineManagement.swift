af378fcca2e99767aaa35264816d13c674dbfc29
kiljoonho <kiljh@me.com>
2019-06-22 00:41:13 +0900

    추가 - 메뉴 리턴 메서드 추가

--- VendingMachine/VendingMachine/VendingMachineManagement.swift
+++ VendingMachine/VendingMachine/VendingMachineManagement.swift
@@ -12,19 +12,19 @@ struct VendingMachineManagement {
     private var stock = [Drink]()
     
     /// 자판기를 초기화하는 메소드
-    func initVendingMachine() -> VendingMachine {
+    private func initVendingMachine() -> VendingMachine {
         return VendingMachine(stock: stock)
     }
     
     /// 특정 상품 인스턴스를 넘겨서 재고를 추가하는 메소드
-    mutating func supply(_ product: Drink, amount: Int) {
+    private mutating func supply(_ product: Drink, amount: Int) {
         for _ in 1...amount {
             stock.append(product)
         }
     }
     
     /// 전체 상품 재고를 (사전으로 표현하는) 종류별로 리턴하는 메소드
-    func getStockList () -> Dictionary<Drink, Int> {
+    private func getStockList () -> Dictionary<Drink, Int> {
         var stockList = Dictionary<Drink, Int>()
         
         for drink in stock {
@@ -44,7 +44,7 @@ struct VendingMachineManagement {
     }
     
     /// 유통기한이 지난 재고만 리턴하는 메소드
-    func getExpiredDrinkList () -> [Drink] {
+    private func getExpiredDrinkList () -> [Drink] {
         var expiredDrinks = stock.filter() { (drink: Drink) -> Bool in
             return !drink.validate()
         }
@@ -55,7 +55,7 @@ struct VendingMachineManagement {
     }
     
     /// 따뜻한 음료만 리턴하는 메소드
-    func getHotDrinkList () -> [Drink] {
+    private func getHotDrinkList () -> [Drink] {
         var hotDrinks = stock.filter() { (drink: Drink) -> Bool in
             let coffee = drink as! Coffee
             return coffee.isHot()
@@ -64,6 +64,22 @@ struct VendingMachineManagement {
         hotDrinks.removeDuplicates()
         
         return hotDrinks
+    }
+    
+    /// 메뉴를 실행하는 메소드
+    func executeMenu (_menu: ManagementMenu) {
+        
+    }
+    
+    /// 메뉴를 리턴하는 메소드
+    func getMenu () -> Dictionary<Int, String> {
+        var menu = Dictionary<Int, String>()
+        
+        for managementMenu in ManagementMenu.allCases {
+            menu[managementMenu.rawValue] = managementMenu.localizedDescription
+        }
+        
+        return menu
     }
 }
 
