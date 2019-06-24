a2f59477c9f64bbf864c43d0a0e1dde7a4b231da
kiljoonho <kiljh@me.com>
2019-06-24 15:16:35 +0900

    제거 - 모든 메서드에 private 제거
    private 을 붙이고 실행하는 메소드로 내부에서 호출하려고 했지만 다시 외부에서 실행하도록 구성 변경

--- VendingMachine/VendingMachine/VendingMachineManagement.swift
+++ VendingMachine/VendingMachine/VendingMachineManagement.swift
@@ -12,19 +12,26 @@ struct VendingMachineManagement {
     private var stock = [Drink]()
     
     /// 자판기를 초기화하는 메소드
-    private func initVendingMachine() -> VendingMachine {
+    func initVendingMachine() -> VendingMachine {
         return VendingMachine(stock: stock)
     }
     
     /// 특정 상품 인스턴스를 넘겨서 재고를 추가하는 메소드
-    private mutating func supply(_ product: Drink, amount: Int) {
+    mutating func supply(_ index: Int, amount: Int) {
+        let bananaMilk = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190301", farmCode: 5, expirationDate: "20190930")
+        let strawberryMilk = StrawberryMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405", farmCode: 3, expirationDate: "20190925")
+        let fanta = Fanta(brand: "롯데", ml: 350, price: 2000, productDate: "20190505", orangeIncense: 0.7, expirationDate: "20190921")
+        let top = TOP(brand: "맥심", ml: 400, price: 3000, productDate: "20190606", hot: false, expirationDate: "20190920")
+        let hot6 = Hot6(brand: "롯데", ml: 240, price: 1000, productDate: "20190529", expirationDate: "20191029")
+        let pepsiCoke = PepsiCoke(brand: "펩시", ml: 255, price: 1200, productDate: "20190610", expirationDate: "20191010")
+        
         for _ in 1...amount {
             stock.append(product)
         }
     }
     
     /// 전체 상품 재고를 (사전으로 표현하는) 종류별로 리턴하는 메소드
-    private func getStockList () -> Dictionary<Drink, Int> {
+    func getStockList () -> Dictionary<Drink, Int> {
         var stockList = Dictionary<Drink, Int>()
         
         for drink in stock {
@@ -35,7 +42,7 @@ struct VendingMachineManagement {
         return stockList
     }
     
-    private func getStockCount (_ drink: Drink, _ stockList: Dictionary<Drink, Int>) -> Int {
+    func getStockCount (_ drink: Drink, _ stockList: Dictionary<Drink, Int>) -> Int {
         if let stockCount = stockList[drink] {
             return stockCount + 1
         }
@@ -44,7 +51,7 @@ struct VendingMachineManagement {
     }
     
     /// 유통기한이 지난 재고만 리턴하는 메소드
-    private func getExpiredDrinkList () -> [Drink] {
+    func getExpiredDrinkList () -> [Drink] {
         var expiredDrinks = stock.filter() { (drink: Drink) -> Bool in
             return !drink.validate()
         }
@@ -55,7 +62,7 @@ struct VendingMachineManagement {
     }
     
     /// 따뜻한 음료만 리턴하는 메소드
-    private func getHotDrinkList () -> [Drink] {
+    func getHotDrinkList () -> [Drink] {
         var hotDrinks = stock.filter() { (drink: Drink) -> Bool in
             let coffee = drink as! Coffee
             return coffee.isHot()
@@ -64,11 +71,6 @@ struct VendingMachineManagement {
         hotDrinks.removeDuplicates()
         
         return hotDrinks
-    }
-    
-    /// 메뉴를 실행하는 메소드
-    func executeMenu (_menu: ManagementMenu) {
-        
     }
     
     /// 메뉴를 리턴하는 메소드
