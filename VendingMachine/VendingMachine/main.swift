256528a489667c122f391c5867369ad8e6fa40b7
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-24 18:56:17 +0900

    beverages에 음료 인스턴스를 생성하고 출력하도록 함

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -8,4 +8,12 @@
 
 import Foundation
 
+var beverages = [
+    Beverage(brand: "서울 우유", packageSize: 200, price: 1000, name: "딸기우유", dateOfManufacture: Date()),
+    Beverage(brand: "팹시", packageSize: 350, price: 2000, name: "다이어트 콜라", dateOfManufacture: Date()),
+    Beverage(brand: "맥심", packageSize: 400, price: 3000, name: "TOP 아메리카노", dateOfManufacture: Date()),
+]
 
+for beverage in beverages {
+    print(beverage)
+}
