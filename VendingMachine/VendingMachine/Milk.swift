9195181a59cb0eb2f7182a15a79fe3425e87ca20
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-25 12:13:32 +0900

    Milk 클래스 작성

---
+++ VendingMachine/VendingMachine/Milk.swift
@@ -0,0 +1,18 @@
+//
+//  Milk.swift
+//  VendingMachine
+//
+//  Created by 김성현 on 25/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Milk: Beverage {
+    var milkFactoryIdentifier: String
+    
+    init(brand: String, packageSize: Int, price: Int, name: String, milkFactoryIdentifier: String, dateOfManufacture: Date) {
+        self.milkFactoryIdentifier = milkFactoryIdentifier
+        super.init(brand: brand, packageSize: packageSize, price: price, name: name, dateOfManufacture: dateOfManufacture)
+    }
+}
