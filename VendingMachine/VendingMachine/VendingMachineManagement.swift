eab6d52a91d40cb5a55bb7de988f7dcb102863f7
kiljoonho <kiljh@me.com>
2019-06-25 12:33:08 +0900

    구현 - 자판기 관리자 인터페이스 구현

--- VendingMachine/VendingMachine/VendingMachineManagement.swift
+++ VendingMachine/VendingMachine/VendingMachineManagement.swift
@@ -8,96 +8,11 @@
 
 import Foundation
 
-struct VendingMachineForManagement {
-    private var stock = [Drink]()
-    let bananaMilk: BananaMilk
-    let strawberryMilk: StrawberryMilk
-    let fanta: Fanta
-    let top: TOP
-    let hot6: Hot6
-    let pepsiCoke: PepsiCoke
-    let drinks: [Drink]
-    
-    init() {
-        bananaMilk = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190301", farmCode: 5, expirationDate: "20190930")
-        strawberryMilk = StrawberryMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405", farmCode: 3, expirationDate: "20190925")
-        fanta = Fanta(brand: "롯데", ml: 350, price: 2000, productDate: "20190505", orangeIncense: 0.7, expirationDate: "20190921")
-        top = TOP(brand: "맥심", ml: 400, price: 3000, productDate: "20190606", hot: false, expirationDate: "20190920")
-        hot6 = Hot6(brand: "롯데", ml: 240, price: 1000, productDate: "20190529", expirationDate: "20191029")
-        pepsiCoke = PepsiCoke(brand: "펩시", ml: 255, price: 1200, productDate: "20190610", expirationDate: "20191010")
-        drinks = [bananaMilk, strawberryMilk, fanta, top, hot6, pepsiCoke]
-        
-    }
-    
-    /// 자판기를 초기화하는 메소드
-    func initVendingMachine() -> VendingMachineForUser {
-        return VendingMachineForUser(stock: stock)
-    }
-    
-    /// 특정 상품 인스턴스를 넘겨서 재고를 추가하는 메소드
-    mutating func supply(_ index: Int, amount: Int) {
-        for _ in 1...amount {
-            stock.append(drinks[index])
-        }
-    }
-    
-    /// 사용 가능한 음료수 종류를 리턴하는 메소드
-    func getAbleDrinks () -> [Drink] {
-        return drinks
-    }
-    
-    /// 전체 상품 재고를 (사전으로 표현하는) 종류별로 리턴하는 메소드
-    func getStockList () -> Dictionary<Drink, Int> {
-        var stockList = Dictionary<Drink, Int>()
-        
-        for drink in stock {
-            let stockCount = getStockCount(drink, stockList)
-            stockList[drink] = stockCount
-        }
-        
-        return stockList
-    }
-    
-    func getStockCount (_ drink: Drink, _ stockList: Dictionary<Drink, Int>) -> Int {
-        if let stockCount = stockList[drink] {
-            return stockCount + 1
-        }
-        
-        return 1
-    }
-    
-    /// 유통기한이 지난 재고만 리턴하는 메소드
-    func getExpiredDrinkList () -> [Drink] {
-        var expiredDrinks = stock.filter() { (drink: Drink) -> Bool in
-            return !drink.validate()
-        }
-        
-        expiredDrinks.removeDuplicates()
-        
-        return expiredDrinks
-    }
-    
-    /// 따뜻한 음료만 리턴하는 메소드
-    func getHotDrinkList () -> [Drink] {
-        var hotDrinks = stock.filter() { (drink: Drink) -> Bool in
-            let coffee = drink as! Coffee
-            return coffee.isHot()
-        }
-        
-        hotDrinks.removeDuplicates()
-        
-        return hotDrinks
-    }
-    
-    /// 메뉴를 리턴하는 메소드
-    func getMenu () -> Dictionary<Int, String> {
-        var menu = Dictionary<Int, String>()
-        
-        for managementMenu in ManagementMenu.allCases {
-            menu[managementMenu.rawValue] = managementMenu.localizedDescription
-        }
-        
-        return menu
-    }
+protocol VendingMachineManagementable {
+    mutating func supply(_ index: Int, amount: Int)
+    func getAbleDrinks () -> [Drink]
+    func getStockList () -> Dictionary<Drink, Int>
+    func getExpiredDrinkList () -> [Drink]
+    func getHotDrinkList () -> [Drink]
+    func getMenu () -> Dictionary<Int, String>
 }
-
