295ee29bd1b903819739dc05a89e601e16c37595
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-27 18:18:20 +0900

    main 명령문 작성

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+let beverages = [
+    StrawberryMilk(milkFactoryIdentifier: "어느목장1", dateOfManufacture: Date(year: 2019, month: .june, day: 26)!),
+    Cola(dateOfManufacture: Date(year: 2019, month: .june, day: 15)!),
+    LemonlimeDrink(dateOfManufacture: Date(year: 2018, month: .february, day: 5)!),
+    TOPCoffee(dateOfManufacture: Date(year: 2019, month: .january, day: 21)!)
+]
+
+for beverage in beverages {
+    print(beverage)
+}
+
+
