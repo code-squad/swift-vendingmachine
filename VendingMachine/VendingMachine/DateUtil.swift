312591b4e39f1c1f0a8ceb54035fccf3910fb821
gukjin jang <jgj455@gmail.com>
2019-07-01 16:31:53 +0900

    Add: DateUtil 구조체 생성 및 main() 구현

---
+++ VendingMachine/VendingMachine/DateUtil.swift
@@ -0,0 +1,26 @@
+//
+//  DateUtil.swift
+//  VendingMachine
+//
+//  Created by jang gukjin on 01/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct DateUtil {
+    static private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        
+        return dateFormatter
+    }()
+    
+    static func convertDate(of text: String) -> Date{
+        return dateFormatter.date(from: text)!
+    }
+    
+    static func convertText(of date: Date) -> String{
+        return dateFormatter.string(from: date)
+    }
+}
