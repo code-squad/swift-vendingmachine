8277d05d375e3e82524c3b824f853be485170da0
박준희 <junibibi04@gmail.com>
2019-07-02 19:00:08 +0900

    askNumber, insertCoins 작성

--- VendingMachine/VendingMachine/InputController.swift
+++ VendingMachine/VendingMachine/InputController.swift
@@ -1,27 +1,43 @@
 import Foundation
 
 enum InputError: Error {
-    case notNumber
     case invalidSelection
 }
 
 class InputController {
     
     var inputView = InputView()
+    var outputView = OutputView()
+    
+    var machine = VendingMachine()
     
     func askForChoice(options: [String], question: String) -> Result<String, InputError> {
         inputView.show(question)
         inputView.showOptions(options)
         
-        let input = inputView.ask("숫자")
-        guard let selection = Int(input) else {
-            return .failure(InputError.notNumber)
-        }
+        let selection = askNumber("숫자")
         
         guard (1...options.count).contains(selection) else {
             return .failure(InputError.invalidSelection)
         }
         return .success(options[selection - 1])
+    }
+    
+    func askNumber(_ target: String) -> Int {
+        while true {
+            let input = inputView.ask(target)
+            guard let number = Int(input) else {
+                inputView.show("숫자가 아님, 다시 입력.")
+                continue
+            }
+            return number
+        }
+    }
+    
+    func insertCoins() {
+        inputView.show("넣을 코인의 수량을 입력하세요.")
+        let coins = askNumber("수량")
+        machine.insertCoins(coins)
     }
     
 }
