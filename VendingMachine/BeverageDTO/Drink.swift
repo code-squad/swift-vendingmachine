65c61b56ef8e679f005e15324588a9892cf5bda9
hwj0623 <hwj0623@naver.com>
2019-07-02 22:03:21 +0900

    STEP 12 (6) Drink(level1) 객체 생성

---
+++ VendingMachine/BeverageDTO/Drink.swift
@@ -0,0 +1,41 @@
+//
+//  Drink.swift
+//  VendingMachine
+//
+//  Created by hw on 02/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Drink {
+    private let brand: String
+    private let quantity: Int
+    private let price: Int
+    private let drinkName: String
+    private let manufactureDate: Date
+    
+    init(brand: String, quantity: Int, price: Int, name: String, date: Date){
+        self.brand = brand
+        self.quantity = quantity
+        self.price = price
+        self.drinkName = name
+        self.manufactureDate = Date.init()
+    }
+}
+
+extension Drink: Drinkable {
+    func display(printFormat: (String) -> Void) {
+        printFormat(self.description)
+    }
+    
+    var description: String {
+        var result = [String]()
+        result.append(self.brand)
+        result.append(String.init(format: "%d\(Units.millilter)", self.quantity))
+        result.append(String.init(format: "%d\(Units.won)", self.price))
+        result.append(self.drinkName)
+        result.append(CustomDateFormatter.convertDateToString(self.manufactureDate))
+        return result.joined(separator: ", ")
+    }
+}
