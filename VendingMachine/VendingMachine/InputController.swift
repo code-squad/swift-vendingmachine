f1d5c1031a35ce59855cd9c340581dc2addd17bb
Sunghyun Kim <sunghyun_k@icloud.com>
2019-07-02 16:09:41 +0900

    askForChoice 작성

--- VendingMachine/VendingMachine/InputController.swift
+++ VendingMachine/VendingMachine/InputController.swift
@@ -1,5 +1,22 @@
 import Foundation
 
 class InputController {
-    <#code#>
+    
+    var inputView = InputView()
+    
+    func askForChoice(options: [String], question: String) -> String {
+        inputView.show(question)
+        inputView.showOptions(options)
+        while true {
+            let input = inputView.ask("숫자")
+            guard let selection = Int(input) else {
+                continue
+            }
+            guard (1...options.count).contains(selection) else {
+                continue
+            }
+            return options[selection - 1]
+        }
+    }
+    
 }
