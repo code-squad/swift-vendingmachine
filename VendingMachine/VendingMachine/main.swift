707209073053246389685dc5d40b92487b878a9d
hwj0623 <hwj0623@naver.com>
2019-07-04 15:55:43 +0900

    main에서 음료수 인스턴스 생성 로직 수정

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -25,18 +25,18 @@ class Main {
         var result = [Drinkable]()
 
         var inputDate = CustomDateFormatter.convertStringToDate(year: 2017, month: 10, day: 09)
-        let seoulStrawberry = Drink(brand: "서울우유", quantity: 200, price: 1000, name: "날마다딸기우유", date: inputDate)
+        let seoulStrawberry = StrawberryMilk(brand: "서울우유", quantity: 200, price: 1000, name: "날마다딸기우유", date: inputDate)
         inputDate = CustomDateFormatter.convertStringToDate(year: 2017, month: 10, day: 12)
-        let seoulStrawberry2 = Drink(brand: "서울우유", quantity: 200, price: 1000, name: "날마다딸기우유", date: inputDate)
+        let seoulStrawberry2 = StrawberryMilk(brand: "서울우유", quantity: 200, price: 1000, name: "날마다딸기우유", date: inputDate)
         inputDate = CustomDateFormatter.convertStringToDate(year: 2017, month: 10, day: 05)
-        let pepsi = Drink(brand: "펩시", quantity: 350, price: 2000, name: "다이어트콜라", date: inputDate)
+        let zeroPepsi = Coke(brand: "펩시", quantity: 350, price: 2000, name: "다이어트콜라", date: inputDate, isSugar: false)
         inputDate = CustomDateFormatter.convertStringToDate(year: 2017, month: 10, day: 10)
-        let top = Drink(brand: "맥심", quantity: 400, price: 3000, name: "TOP아메리카노", date: inputDate)
+        let topAmericano = TOP.init(brand: "맥심", quantity: 400, price: 3000, name: "TOP아메리카노", date: inputDate, coffeine: 120.0, origin: .arabica, type: .americano)
 
         result.append(seoulStrawberry)
         result.append(seoulStrawberry2)
-        result.append(pepsi)
-        result.append(top)
+        result.append(zeroPepsi)
+        result.append(topAmericano)
         return result
     }
 }
