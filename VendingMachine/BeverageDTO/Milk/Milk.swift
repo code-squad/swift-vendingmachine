24df0c3fb66dc471cede23d4129e553cb98e59f3
hwj0623 <hwj0623@naver.com>
2019-07-05 16:40:13 +0900

    STEP 12-2 (4) 우유 및 하위 객체 수정

--- VendingMachine/BeverageDTO/Milk/Milk.swift
+++ VendingMachine/BeverageDTO/Milk/Milk.swift
@@ -11,16 +11,17 @@ import Foundation
 /// 저지방 유무에 대한 프로퍼티 추가
 class Milk: Drink {
     private var isLowFat : Bool
-    private (set) var farmCode : Int?
+    private (set) var farmCode : FarmCode?
     
-    init(brand: String, quantity: Int, price: Int, name: String, date: Date, isLowFat: Bool = true) {
+    init(brand: String, quantity: Int, price: Int, name: String, date: Date, isLowFat: Bool = false, farmCode: FarmCode? = nil) {
         self.isLowFat = isLowFat
+        self.farmCode = farmCode
         super.init(brand: brand, quantity: quantity, price: price, name: name, date: date)
     }
     
-    func hasFarmCode() -> Int? {
+    func hasFarmCode() -> FarmCode {
         guard let code = self.farmCode else{
-            return -1
+            return FarmCode.none
         }
         return code
     }
