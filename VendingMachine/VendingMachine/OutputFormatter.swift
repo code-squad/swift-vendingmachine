e64b45d31c000aa464ea468295592470620f3009
Sunghyun Kim <sunghyun_k@icloud.com>
2019-07-06 12:25:21 +0900

    itemsWithPrice 수정

--- VendingMachine/VendingMachine/OutputFormatter.swift
+++ VendingMachine/VendingMachine/OutputFormatter.swift
@@ -2,12 +2,17 @@ import Foundation
 
 class OutputFormatter {
     
-    func itemsWithPrice(inventory: [String: Item]) -> [String] {
-        var items = [String]()
-        inventory.forEach { (name, item) in
-            items.append("\(name) \(item.price)코인 (\(item.count)개)")
+    func itemsWithPrice(inventory: [String: Beverages]) -> [String] {
+        
+        return inventory.map { (name, beverages) -> String in
+            let priceStr: String
+            if let price = beverages.price {
+                priceStr = "\(price)"
+            } else {
+                priceStr = "알수 없음"
+            }
+            return "\(name) \(priceStr)"
         }
-        return items
     }
     
 }
