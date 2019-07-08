369ac157d707a5a1aaceba2487d90a9ed197f75b
hwj0623 <hwj0623@naver.com>
2019-07-08 13:59:43 +0900

    (3) 칼로리 및 온도 프로퍼티와 메서드 추가

--- VendingMachine/BeverageDTO/Drink.swift
+++ VendingMachine/BeverageDTO/Drink.swift
@@ -8,31 +8,66 @@
 
 import Foundation
 
-class Drink: Drinkable{
+public typealias CaloryElements = (carbon: Int, protein: Int, fat: Int )
+
+class Drink{
+  
     private let brand: String
     private var quantity: Int
     private var price: Int
     private var drinkName: String
     private let validDate: Date
+    private (set) var temperature: Double
     
-    init(brand: String, quantity: Int, price: Int, name: String, date: Date = Date.init()){
+    private let caloryElements : CaloryElements
+
+    
+    var caloriesInfo : Int {
+        get {
+            return caloryElements.fat*9 + caloryElements.carbon*4 + caloryElements.protein*4
+        }
+    }
+    
+    convenience init (brand: String, quantity: Int, price: Int, name: String, date: Date = Date.init(), temperature: Double = 4.0){
+        let calorySet = CaloryElements(carbon: 0, protein: 0, fat: 0 )
+        self.init(brand: brand, quantity: quantity, price: price, name: name, date: date, caloryElements: calorySet, temp: temperature)
+    }
+    
+    init(brand: String, quantity: Int, price: Int, name: String, date: Date = Date.init(), caloryElements: CaloryElements, temp temperature: Double){
         self.brand = brand
         self.quantity = quantity
         self.price = price
         self.drinkName = name
         self.validDate = date
+        self.caloryElements = caloryElements
+        self.temperature = temperature
     }
-    func display(printFormat: (Drinkable) -> Void) {
-        printFormat(self)
-    }
+    
     var date : Date {
         get {
             return validDate
         }
     }
+}
+
+extension Drink : Drinkable {
+    
+    func display(printFormat: (Drinkable) -> Void) {
+        printFormat(self)
+    }
+    
     func validate(with date:Date) -> Bool {
         let isValid = self.date > date ? true : false
         return isValid
+    }
+    func isLowCalories() -> Bool {
+        let isLow = self.caloriesInfo < 50 ? true : false
+        return isLow
+    }
+    
+    func isHot() -> Bool {
+        let isHot = self.temperature > 20 ? true : false
+        return isHot
     }
 }
 
