72fcdef89ab5ff8098604c20cada6abfb6edfe42
gukjin jang <jgj455@gmail.com>
2019-07-08 21:22:36 +0900

    Add: InputView 구조체 생성

---
+++ VendingMachine/View/OutputView.swift
@@ -0,0 +1,13 @@
+//
+//  OutputView.swift
+//  VendingMachine
+//
+//  Created by jang gukjin on 08/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    
+}
