78c2f792b6dd55a5f48265bf921f5a8067cb1538
hwj0623 <hwj0623@naver.com>
2019-07-10 16:09:06 +0900

    (11) 메인 클래스에서 더미 데이터 생성 - 단위테스트 파일 생성

--- VendingMachine/UnitTestVendingMachine/UnitTestDrinkObject.swift
+++ VendingMachine/UnitTestVendingMachine/UnitTestDrinkObject.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class UnitTestVendingMachine: XCTestCase {
+class UnitTestDrinkObject: XCTestCase {
 
     override func setUp() {
 
---
+++ VendingMachine/UnitTestVendingMachine/UnitTestVendingMachineLogic.swift
@@ -0,0 +1,23 @@
+//
+//  UnitTestVendingMachineLogic.swift
+//  UnitTestVendingMachine
+//
+//  Created by hw on 10/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestVendingMachineLogic: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+    func testExample() {
+       
+    }
+
+    
+
+}
