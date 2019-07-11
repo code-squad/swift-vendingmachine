b841c2a3c7a3f653ac2a3b480a3cbf8bfd4acc13
Sunghyun Kim <sunghyun_k@icloud.com>
2019-07-11 16:04:28 +0900

    관리자 모드 메시지 분리함

--- VendingMachine/VendingMachine/VendingMachineController.swift
+++ VendingMachine/VendingMachine/VendingMachineController.swift
@@ -8,33 +8,40 @@ class VendingMachineController {
             static let insertCoins = "코인 추가"
             static let vendItem = "음료 구매"
             
-            static let addItem = "음료 추가"
-            static let removeItem = "음료 제거"
+            
             
             static let modes: [VendingMachine.Mode: String] = [
                 .user: "사용자 모드",
                 .administrator: "관리자 모드"
             ]
-            static let manualCreate = "수동으로 생성하기"
-            static let selectInOptions = "옵션에서 선택하기"
             
+            struct Administrator {
+                static let addItem = "음료 추가"
+                static let removeItem = "음료 제거"
+                
+                static let manualCreate = "수동으로 생성하기"
+                static let selectInOptions = "옵션에서 선택하기"
+            }
         }
         
         struct Question {
             static let enterCoinsToDeposit = "넣을 코인의 수량을 입력하세요."
             static let amount = "수량"
+            
             static let askChangeToAdministrator = "관리자 모드로 변경하시겠습니까?"
             static let enterY = "동의하면 y 입력"
             
-            static let enterItemNameToRemove = "지울 제품 이름 입력"
-            static let itemName = "음료 이름"
-            static let selectCreateOption = "음료 생성 방법을 선택하세요."
-            
-            static func enter(target: String) -> String {
-                return "\(target)을(를) 입력하세요."
-            }
-            static func choose(target: String) -> String {
-                return "\(target)을(를) 선택하세요."
+            struct Administrator {
+                static let enterItemNameToRemove = "지울 제품 이름 입력"
+                static let itemName = "음료 이름"
+                static let selectCreateOption = "음료 생성 방법을 선택하세요."
+                
+                static func enter(target: String) -> String {
+                    return "\(target)을(를) 입력하세요."
+                }
+                static func choose(target: String) -> String {
+                    return "\(target)을(를) 선택하세요."
+                }
             }
         }
         
@@ -49,16 +56,6 @@ class VendingMachineController {
                 return "현재 모드: \(Message.Option.modes[mode]!)"
             }
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
     }
     
@@ -108,8 +105,8 @@ class VendingMachineController {
         outputView.showInventory(machine.inventory)
         
         let functions = [
-            Message.Option.addItem: addItem,
-            Message.Option.removeItem: removeItem
+            Message.Option.Administrator.addItem: addItem,
+            Message.Option.Administrator.removeItem: removeItem
         ]
         
         let function = inputView.askForChoice(options: functions.map { $0.value }, showingBy: functions.map { $0.key })
@@ -156,8 +153,8 @@ class VendingMachineController {
 /// 관리자 모드 메소드
 extension VendingMachineController: Administrator {
     func removeItem() {
-        inputView.show(Message.Question.enterItemNameToRemove)
-        let name = inputView.ask(Message.Question.itemName)
+        inputView.show(Message.Question.Administrator.enterItemNameToRemove)
+        let name = inputView.ask(Message.Question.Administrator.itemName)
         let removedItem: Beverage
         do {
             removedItem = try machine.remove(itemNamed: name)
@@ -185,11 +182,11 @@ extension VendingMachineController: BeverageMakable {
     func makeBeverage() -> Beverage {
         
         let functions = [
-            Message.Option.manualCreate: makeBeverageManual,
-            Message.Option.selectInOptions: makeBeverageInOptions
+            Message.Option.Administrator.manualCreate: makeBeverageManual,
+            Message.Option.Administrator.selectInOptions: makeBeverageInOptions
         ]
         
-        inputView.show(Message.Question.selectCreateOption)
+        inputView.show(Message.Question.Administrator.selectCreateOption)
         let makeBeverage = inputView.askForChoice(options: functions.map { $0.value }, showingBy: functions.map { $0.key })
         
         return makeBeverage()
@@ -197,25 +194,25 @@ extension VendingMachineController: BeverageMakable {
     
     
     func makeBeverageManual() -> Beverage {
-        inputView.show(Message.Question.enter(target: "이름"))
+        inputView.show(Message.Question.Administrator.enter(target: "이름"))
         let name = inputView.ask("이름")
         
-        inputView.show(Message.Question.enter(target: "가격"))
+        inputView.show(Message.Question.Administrator.enter(target: "가격"))
         let price = inputView.askNumber("가격")
         
-        inputView.show(Message.Question.enter(target: "용량"))
+        inputView.show(Message.Question.Administrator.enter(target: "용량"))
         let packageSize = inputView.askNumber("용량")
         
-        inputView.show(Message.Question.enter(target: "브랜드"))
+        inputView.show(Message.Question.Administrator.enter(target: "브랜드"))
         let brand = inputView.ask("브랜드")
         
-        inputView.show(Message.Question.enter(target: "유효기간 일 수"))
+        inputView.show(Message.Question.Administrator.enter(target: "유효기간 일 수"))
         let expirationPeriod = inputView.askDays("유효기간")
         
-        inputView.show(Message.Question.enter(target: "음료의 온도"))
+        inputView.show(Message.Question.Administrator.enter(target: "음료의 온도"))
         let beverageTemperature = inputView.askNumber("온도")
         
-        inputView.show(Message.Question.enter(target: "생산 날짜"))
+        inputView.show(Message.Question.Administrator.enter(target: "생산 날짜"))
         inputView.show("(입력 형태: yyyyMMdd)")
         let dateOfManufacture = inputView.askDate("생산 날짜")
         
@@ -230,28 +227,28 @@ extension VendingMachineController: BeverageMakable {
         ]
         
         let make = inputView.askForChoice(options: beverageOptions.map { $0.value }, showingBy: beverageOptions.map { $0.key })
-        inputView.show(Message.Question.enter(target: "생산 날짜"))
+        inputView.show(Message.Question.Administrator.enter(target: "생산 날짜"))
         let dateOfManufacture = inputView.askDate("생산 날짜")
         return make(dateOfManufacture)
     }
     
     func makeMilk(dateOfManufacture: Date) -> Milk {
-        inputView.show(Message.Question.choose(target: "우유의 종류"))
+        inputView.show(Message.Question.Administrator.choose(target: "우유의 종류"))
         let option = inputView.askForChoice(options: [BeverageMaker.MilkOption.chocolate, .strawberry], showingBy: ["초코 우유", "딸기 우유"])
         
-        inputView.show(Message.Question.enter(target: "우유 목장"))
+        inputView.show(Message.Question.Administrator.enter(target: "우유 목장"))
         let milkFactory = inputView.ask("목장")
         return maker.makeMilk(option: option, dateOfManufacture: dateOfManufacture, milkFactory: milkFactory)
     }
     
     func makeSoftDrink(dateOfManufacture: Date) -> SoftDrink {
-        inputView.show(Message.Question.choose(target: "탄산 음료의 종류"))
+        inputView.show(Message.Question.Administrator.choose(target: "탄산 음료의 종류"))
         let option = inputView.askForChoice(options: [BeverageMaker.SoftDrinkOption.cola, .lemonLimeDrink], showingBy: ["콜라", "사이다"])
         return maker.makeSoftDrink(option: option, dateOfManufacture: dateOfManufacture)
     }
     
     func makeCoffee(dateOfManufacture: Date) -> Coffee {
-        inputView.show(Message.Question.choose(target: "커피의 종류"))
+        inputView.show(Message.Question.Administrator.choose(target: "커피의 종류"))
         let option = inputView.askForChoice(options: [BeverageMaker.CoffeeOption.cantataCoffee, .topCoffee], showingBy: ["칸타타", "TOP"])
         return maker.makeCoffee(option: option, dateOfManufacture: dateOfManufacture)
     }
