9fc97c14b88f62a6e0fe0372556b919716733f47
Sunghyun Kim <sunghyun_k@icloud.com>
2019-07-11 14:54:04 +0900

    접근 제어 수정

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -2,11 +2,7 @@ import Foundation
 
 func main() {
     var controller = VendingMachineController()
-    var beverageMaker = BeverageMaker()
     
-    controller.machine.toggleAdministratorMode()
-    try! controller.machine.addItems(beverageMaker.exampleBeverages)
-    controller.machine.toggleAdministratorMode()
     
     controller.askToSwitchMode()
     
