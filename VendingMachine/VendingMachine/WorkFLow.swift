b6cf81d1661877a85b18ef1b787db40accee1069
gukjin jang <jgj455@gmail.com>
2019-07-13 01:36:17 +0900

    Modify: main() 구현 및 마이너한 오류 수정

--- VendingMachine/VendingMachine/WorkFLow.swift
+++ VendingMachine/VendingMachine/WorkFLow.swift
@@ -17,47 +17,50 @@ struct WorkFlow {
     
     /// 입력받은 값에 따라 자판기의 기능을 실행하는 메소드
     mutating func selectMenu( vendingMachine: VendingMachine, of answers: [String]) -> String {
+        let itemsSet = vendingMachine.getItems().set()
         var result = ""
+        
         switch mode {
-        case .Admin: adminWorkFlow(vendingMachine: vendingMachine, of: answers)
-        default: result = userWorkFlow(vendingMachine: vendingMachine, of: answers)
+        case .Admin: result = adminWorkFlow(vendingMachine: vendingMachine, of: answers, itemsSet: itemsSet)
+        case .User: result = userWorkFlow(vendingMachine: vendingMachine, of: answers, itemsSet: itemsSet)
+        default: result = ""
         }
         return result
     }
     
     /// 관리자 모드일때 입력받은 값에 따라 자판기의 기능을 실행하는 메소드
-    func adminWorkFlow(vendingMachine: VendingMachine, of answers: [String]) -> String {
-        let menuAnswer: Int = Int(answers[0]) ?? 0
-        var additionalAnswer: Int = 0
-        if answers.count > 1 {
-            additionalAnswer = Int(answers[1]) ?? 0
-        }
-        let itemsSet = vendingMachine.getItems().set()
+    func adminWorkFlow(vendingMachine: VendingMachine, of answers: [String], itemsSet: [Product]) -> String {
+        let answer = convertEachAnswers(of: answers)
         var result = ""
         
-        switch menuAnswer {
-        case 1: vendingMachine.stockUp(of: itemsSet[additionalAnswer-1], count: 1)
-        case 2: vendingMachine.takeOutStock(of: itemsSet[additionalAnswer-1], count: 1)
+        switch answer.first {
+        case 1: vendingMachine.stockUp(of: itemsSet[answer.second-1], count: 1)
+        case 2: vendingMachine.takeOutStock(of: itemsSet[answer.second-1], count: 1)
         default : result = "\(vendingMachine.isItemsPastExpirationDate())\n"
         }
         return result
     }
     
     /// 사용자 모드일때 입력받은 값에 따라 자판기의 기능을 실행하는 메소드
-    func userWorkFlow(vendingMachine: VendingMachine, of answers: [String]) -> String {
+    func userWorkFlow(vendingMachine: VendingMachine, of answers: [String], itemsSet: [Product]) -> String {
+        let answer = convertEachAnswers(of: answers)
+        var result: String = ""
+    
+        switch answer.first {
+        case 1: vendingMachine.pay(of: answer.second)
+        case 2: result = vendingMachine.purchase(of: itemsSet[answer.second-1])+"\n"
+        default : result = "\(vendingMachine.isPurchaseDetails())\n"
+        }
+        return result
+    }
+    
+    /// [String]을 [Int]로 변환하는 함수
+    func convertEachAnswers(of answers: [String]) -> (first: Int, second: Int) {
         let menuAnswer: Int = Int(answers[0]) ?? 0
         var additionalAnswer: Int = 0
         if answers.count > 1 {
             additionalAnswer = Int(answers[1]) ?? 0
         }
-        let itemsSet = vendingMachine.getItems().set()
-        var result: String = ""
-    
-        switch menuAnswer {
-        case 1: vendingMachine.pay(of: additionalAnswer)
-        case 2: result = vendingMachine.purchase(of: itemsSet[additionalAnswer-1])+"\n"
-        default : result = "\(vendingMachine.isPurchaseDetails())\n"
-        }
-        return result
+        return (first: menuAnswer, second: additionalAnswer)
     }
 }
--- VendingMachine/View/InputView.swift
+++ VendingMachine/View/InputView.swift
@@ -12,7 +12,7 @@ struct InputView {
     /// 안내문구를 출력하고 값을 입력받는 메소드
     static func readInput() -> MenuMent {
         var answer: String = ""
-        var menu = MenuMent(of: "User")
+        var menu = MenuMent(of: "Quit")
         while true {
             print("""
                 자판기를 시작합니다. (0을 입력하면 종료)
@@ -25,6 +25,9 @@ struct InputView {
                 menu = MenuMent(of: "admin")
                 break
             } else if answer == "2" {
+                menu = MenuMent(of: "user")
+                break
+            } else if answer == "0" {
                 break
             } else {
                 print("범위 내의 숫자를 입력해주세요")
