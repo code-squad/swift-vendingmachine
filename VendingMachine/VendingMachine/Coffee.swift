1c9e974bdb71276e6df1e658942ffb4de7658dff
Daheen Lee <allwhite.dev@gmail.com>
2019-07-31 23:27:42 +0900

    Add Coffee class inherited from Beverage class

---
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -0,0 +1,39 @@
+//
+//  Coffee.swift
+//  VendingMachine
+//
+//  Created by Daheen Lee on 31/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Beverage {
+    enum Variation: String, CustomStringConvertible {
+        case americano = "아메리카노"
+        case latte     = "라떼"
+        case cappuccino = "카푸치노"
+        case espresso  = "에스프레소"
+        case coldBrew  = "콜드브루"
+        
+        var description: String {
+            return self.rawValue
+        }
+    }
+    
+    private let variation: Variation
+    
+    init(variation: Variation,
+         manufacturer: String,
+         dateOfManufacture: String,
+         brand: String,
+         capacity: Int,
+         unit: UnitVolume,
+         price: Int) {
+        self.variation = variation
+        let compositeName = "\(variation)"
+        super.init(manufacturer: manufacturer, dateOfManufacture: dateOfManufacture, brand: brand, name: compositeName, capacity: capacity, unit: unit, price: price)
+    }
+    
+}
+
