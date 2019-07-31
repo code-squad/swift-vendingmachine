0ed854b3b83d420c3e2f86bdb366050db60a7f33
Daheen Lee <allwhite.dev@gmail.com>
2019-07-31 23:45:29 +0900

    Add Soda class inherited from Beverage class with hasSparkle property

---
+++ VendingMachine/VendingMachine/Soda.swift
@@ -0,0 +1,13 @@
+//
+//  Soda.swift
+//  VendingMachine
+//
+//  Created by Daheen Lee on 31/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Soda: Beverage {
+    static let hasSparkle = true
+}
