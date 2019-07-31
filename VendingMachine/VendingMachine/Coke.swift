69ff104a33a454023ed1c5ce3cdb50c063f917d6
Daheen Lee <allwhite.dev@gmail.com>
2019-07-31 23:46:02 +0900

    Add Coke class inherited from Soda class

---
+++ VendingMachine/VendingMachine/Coke.swift
@@ -0,0 +1,29 @@
+//
+//  Coke.swift
+//  VendingMachine
+//
+//  Created by Daheen Lee on 31/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coke: Soda {
+    enum Variant: String, CustomStringConvertible {
+        case diet = "다이어트"
+        case zero = "제로"
+        case regular = ""
+        
+        var description: String {
+            return "\(self.rawValue)"
+        }
+    }
+    
+    private let variant: Variant
+    
+    init(variant: Variant, manufacturer: String, dateOfManufacture: String, brand: String, capacity: Int, unit: UnitVolume, price: Int) {
+        self.variant = variant
+        let compositeName = "\(variant)"
+        super.init(manufacturer: manufacturer, dateOfManufacture: dateOfManufacture, brand: brand, name: compositeName, capacity: capacity, unit: unit, price: price)
+    }
+}
