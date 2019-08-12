7423387fd5db6dc909e9368f531f32977a76c2de
cmindy <minjidev@naver.com>
2019-08-12 16:02:47 +0900

    feat: Add Coffee class

---
+++ VendingMachine/VendingMachine/Coffee.swift
@@ -0,0 +1,15 @@
+//
+//  Coffee.swift
+//  VendingMachine
+//
+//  Created by CHOMINJI on 12/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Beverage {
+    override init(brand: String, capacity: Int, price: Int, name: String, dateOfManufactured: Date) {
+        super.init(brand: brand, capacity: capacity, price: price, name: name, dateOfManufactured: dateOfManufactured)
+    }
+}
