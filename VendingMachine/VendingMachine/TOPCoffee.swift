fb54fff982ba17a315a60c1dfccf09806fd995c8
cmindy <minjidev@naver.com>
2019-08-12 21:13:35 +0900

    feat: Add TOPCoffee class

---
+++ VendingMachine/VendingMachine/TOPCoffee.swift
@@ -0,0 +1,19 @@
+//
+//  TOPCoffee.swift
+//  VendingMachine
+//
+//  Created by CHOMINJI on 12/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class TOPCoffee: Coffee {
+    let caffeineContent: Int
+    
+    init(caffeineContent: Int) {
+        self.caffeineContent = caffeineContent
+        
+        super.init(brand: "맥심커피", capacity: 400, price: 3000, name: "TOP아메리카노", dateOfManufactured: Date("20190812"), temperature: 10)
+    }
+}
