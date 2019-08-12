910eea858d75910f805247a1616442b9fead84ec
cmindy <minjidev@naver.com>
2019-08-12 20:56:59 +0900

    feat: Add ChocolateMilk class

---
+++ VendingMachine/VendingMachine/ChocolateMilk.swift
@@ -0,0 +1,26 @@
+//
+//  ChocolateMilk.swift
+//  VendingMachine
+//
+//  Created by CHOMINJI on 12/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class ChocolateMilk: Milk {
+    private let chocolateContent: Int
+    
+    init(chocolateContent: Int) {
+        self.chocolateContent = chocolateContent
+        
+        super.init(
+            brand: "매일유업",
+            capacity: 235,
+            price: 2000,
+            name: "허쉬초콜렛드링크",
+            dateOfManufactured: Date("20190811"),
+            dateOfExpiration: Date("20190819")
+        )
+    }
+}
