fb367465deac061be45318e40863feee1eaa5c79
cmindy <minjidev@naver.com>
2019-08-12 20:50:27 +0900

    feat: Add StrawberryMilk class

---
+++ VendingMachine/VendingMachine/StrawberryMilk.swift
@@ -0,0 +1,25 @@
+//
+//  StrawberryMilk.swift
+//  VendingMachine
+//
+//  Created by CHOMINJI on 12/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class StrawberryMilk: Milk {
+    let strawberryContent: Int
+    
+    init(strawberryContent: Int) {
+        self.strawberryContent = strawberryContent
+        super.init(
+            brand: "서울우유",
+            capacity: 200,
+            price: 1000,
+            name: "날마다딸기우유",
+            dateOfManufactured: Date("20190812"),
+            dateOfExpiration: Date("20190820")
+        )
+    }
+}
