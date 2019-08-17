e0a9f056a7000409f81371f7a6632dc7847696d7
cmindy <minjidev@naver.com>
2019-08-17 21:00:38 +0900

    feat: Create OutputView

---
+++ VendingMachine/VendingMachine/OutputView.swift
@@ -0,0 +1,13 @@
+//
+//  OutputView.swift
+//  VendingMachine
+//
+//  Created by CHOMINJI on 17/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    
+}
