f771aaf441e4a2bf1b8529281422a9236db3e630
cmindy <minjidev@naver.com>
2019-08-22 15:08:32 +0900

    feat: Add implement of vending machine methods

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -9,25 +9,43 @@
 import Foundation
 
 class VendingMachine {
-    private(set) var balance = 0
+    private var balance = 0
     var stock: [Beverage: Int] = [:]
     
+    func showBalance(with completion: (Int) -> Void) {
+        completion(balance)
+    }
+    
+    func showStock(with completion: ([(name: String, price: Int, value: Int)]) -> Void) {
+        let names = stock.map { ($0.key.name, $0.key.price, $0.value) }
+        completion(names)
+    }
+    
     /// 자판기 금액을 원하는 금액만큼 올린다.
-    func putMoney(amount money: Int) {
+    func insertMoney(amount: Int) {
+        balance += amount
     }
     
     ///특정 상품 인스턴스를 넘겨서 재고를 추가한다.
-    func addStock(of beverage: Beverage) {
+    func addStock(of beverage: Beverage, count: Int) {
+        stock[beverage, default: 0] += count
     }
     
     /// 현재 금액으로 구매 가능한 음료수 목록을 리턴한다.
-    func fetchPurchaseableList() {
-
+    func fetchPurchaseableList() -> [String] {
+        let beveragePrices = stock.keys
+            .filter { $0.price <= balance }
+            .map { $0.name }
+        return beveragePrices
     }
     
     /// 음료수를 구매한다.
     func purchase(beverage: Beverage) -> Beverage? {
-       
+        if stock[beverage] != nil {
+            stock[beverage]! -= 1
+            return beverage
+        }
+        return nil
     }
     
     /// 잔액을 확인한다.
