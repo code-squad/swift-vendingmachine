4de39a518f6998e98e48047f7befd2ae7aa46311
Daheen Lee <allwhite.dev@gmail.com>
2019-08-26 20:27:48 +0900

    feat: Add VendingMachineUnitTest and test methods

--- VendingMachine/VendingMachineUnitTests/VendingMachineUnitTests.swift
+++ VendingMachine/VendingMachineUnitTests/VendingMachineUnitTests.swift
@@ -9,25 +9,38 @@
 import XCTest
 
 class VendingMachineUnitTests: XCTestCase {
+    let manufacturedDate: String = "20190801"
+    let expirationDate: String = "20190810"
+    
+    var beverage: Beverage!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        beverage = Beverage(manufacturer: "매일우유", dateOfManufactured: manufacturedDate, expirationDate: expirationDate, brand: "우유속에", name: "코코아", capacity: 310, unit: .milliliters, price: 1500)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        beverage = nil
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_validate_method_check_valid_date() {
+        let validDate: Date = Date(string: "20190801")
+        let isOkayToDrink = beverage.validate(with: validDate)
+        
+        XCTAssertTrue(isOkayToDrink, "Test with valid date Failed - Beverage : validate method")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_validate_method_check_invalid_date() {
+        let invalidDate: Date = Date(string: "20190811")
+        let isOkayToDrink = beverage.validate(with: invalidDate)
+        
+        XCTAssertFalse(isOkayToDrink, "Test with invalid date Failed - Beverage : validate method")
     }
-
+    
+    func test_DateConverter() {
+        let dateString = "20190825"
+        let date = DateConverter.toDate(from: dateString)
+        let convertedDate = DateConverter.toString(from: date)
+        XCTAssertEqual(dateString, convertedDate)
+    }
+    
 }
