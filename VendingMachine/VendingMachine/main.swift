e51ee450c0bfa56d4445388dbc343884bc11eb2b
Daheen Lee <allwhite.dev@gmail.com>
2019-08-26 20:51:11 +0900

    Modify main.swift to print Beverage and subclass instances

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,14 +9,22 @@
 import Foundation
 
 var beverages = [Beverage]()
-let chocolateMilk = Milk(flavor: .chocolate, manufacturer: "서울우유", dateOfManufacture: "20190731", brand: "날마다", capacity: 200, unit: .milliliters, price: 2000)
-let bingreBananaMilk = Milk(flavor: .banana, manufacturer: "빙그레", dateOfManufacture: "20190729", brand: "빙그레", capacity: 240, unit: .milliliters, price: 1500)
-let topAmericano = Coffee(variation: .americano, manufacturer: "맥심", dateOfManufacture: "20190710", brand: "TOP", capacity: 200, unit: .milliliters, price: 2500)
-let zeroCoke = Coke(variant: .zero, manufacturer: "한국코카콜라", dateOfManufacture: "20190601", brand: "코카콜라", capacity: 500, unit: .milliliters, price: 1800)
-let pepsiCoke = Coke(variant: .regular, manufacturer: "롯데칠성", dateOfManufacture: "20190601", brand: "펩시콜라", capacity: 500, unit: .milliliters, price: 1500)
-let orangeFanta = Fanta(variant: .orange, dateOfManufacture: "20190530", capacity: 600, unit: .milliliters, price: 3000)
 
-beverages.append(contentsOf: [chocolateMilk, bingreBananaMilk, topAmericano, zeroCoke, pepsiCoke, orangeFanta])
+let dateOfManufactured = "20190731"
+let expirationDate = "20190830"
+
+let chocolateMilk = ChocolateMilk(manufacturer: "서울우유", dateOfManufactured: "20190801", expirationDate: "20190811", brand: "초코꿀단지", capacity: 180, unit: .milliliters, fatContent: 11, chocolateContent: 2, price: 1800)
+let bingreStrawberryMilk = StrawberrrMilk(manufacturer: "빙그레", dateOfManufactured: dateOfManufactured, expirationDate: expirationDate, brand: "빙그레", capacity: 240, unit: .milliliters, fatContent: 13, strawberryContent: 5, price: 1500)
+
+let zeroCoke = Coke(variant: .zero, manufacturer: "한국코카콜라", dateOfManufactured: "20190601", expirationDate: expirationDate, brand: .cocaCola, capacity: 500, unit: .milliliters, price: 1800)
+let pepsiCoke = Coke(variant: .regular, manufacturer: "롯데칠성", dateOfManufactured: "20190601", expirationDate: expirationDate, brand: .pepsiCola, capacity: 500, unit: .milliliters, price: 1500)
+let sprite = Sprite(variant: .regular, manufacturer: "한국코카콜라", dateOfManufactured: dateOfManufactured, expirationDate: expirationDate, capacity: 185, unit: .milliliters, price: 1900)
+
+let cantata = Cantata(variation: .latte, containerType: .can, manufacturer: "롯데칠성음료", dateOfManufactured: dateOfManufactured, expirationDate: expirationDate, capacity: 180, unit: .milliliters, price: 2000)
+let hotCantata = Cantata(variation: .americano, containerType: .can, manufacturer: "롯데칠성음료", dateOfManufactured: dateOfManufactured, expirationDate: expirationDate, capacity: 200, unit: .milliliters, price: 2500, isHot: true)
+
+let hotsix = HotSix(variant: .light, manufacturer: "롯데칠성음료", dateOfManufactured: dateOfManufactured, expirationDate: expirationDate, capacity: 250, unit: .milliliters, price: 1500, caffeineContent: 60)
+beverages.append(contentsOf: [chocolateMilk, bingreStrawberryMilk, zeroCoke, pepsiCoke, sprite, cantata, hotCantata, hotsix])
 for beverage in beverages {
     print(beverage)
 }
