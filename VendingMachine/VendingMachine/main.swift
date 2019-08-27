3ec56e6609572aabff4acf5f7f9dd2bdcc883f8a
kimjieun <zieunv@gmail.com>
2019-08-27 17:51:03 +0900

    클래스의 속성값을 가진 음료 객체들 출력하기

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,3 +9,12 @@
 import Foundation
 
 
+var beverages = [
+    beverage(brand: "pepsi", capacity: 150, price: 900, name: "콜라", manufactureDate: Date(timeIntervalSinceNow: -2*86300)),
+    beverage(brand: "남양", capacity: 130, price: 1300, name: "초코에몽", manufactureDate: Date(timeIntervalSinceNow: -3*86300)),
+    beverage(brand: "델몬트", capacity: 120, price: 1800, name: "오렌지주스", manufactureDate: Date(timeIntervalSinceNow: -5*86300))
+]
+
+for beverage in beverages {
+    print(beverage)
+}
