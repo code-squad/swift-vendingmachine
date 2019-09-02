e3538136141a28c4691c88adff983597dd03199f
cmindy <minjidev@naver.com>
2019-09-02 23:02:33 +0900

    feat: Add logic to Item that represents beverages

--- VendingMachine/VendingMachine/Item.swift
+++ VendingMachine/VendingMachine/Item.swift
@@ -7,3 +7,67 @@
 //
 
 import Foundation
+
+class Item {
+    private var beverages: [Beverage]
+    
+    init(beverages: [Beverage] = []) {
+        self.beverages = beverages
+    }
+    
+    var count: Int {
+        return beverages.count
+    }
+    
+    var isEmpty: Bool {
+        return beverages.isEmpty
+    }
+    
+    func append(beverage: Beverage, count: Int = 0) {
+        beverages.append(beverage)
+    }
+    
+    func remove(beverage: Beverage) {
+        if let index = beverages.firstIndex(of: beverage) {
+            beverages.remove(at: index)
+        }
+    }
+    
+    func fetchFirst() -> Beverage? {
+        return beverages.first
+    }
+    
+    var info: (name: String, price: Int, value: Int)  {
+        let beverage = beverages.first!
+        return (beverage.itemName, beverage.itemPrice, count)
+    }
+}
+
+// MARK: - Filter by conditions
+
+extension Item {
+    /// 재고를 조건에 따라 필터링한 결과를 리턴한다.
+    func filter(by condition: Condition) -> [Beverage] {
+        return beverages.filter { condition.filteringCommand($0) }
+    }
+    
+    enum Condition {
+        case all
+        case hot
+        case expired
+        case purchasable(Int)
+        
+        var filteringCommand: (Beverage) -> Bool {
+            switch self {
+            case .all:
+                return { _ in true }
+            case .hot:
+                return { $0.isHot }
+            case .expired:
+                return { $0.isExpired }
+            case .purchasable(let balance):
+                return { $0.itemPrice <= balance }
+            }
+        }
+    }
+}
