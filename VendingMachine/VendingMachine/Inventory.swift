1f2fa3938c5596bb988616d877ccb947a9a44182
cmindy <minjidev@naver.com>
2019-09-02 23:04:51 +0900

    feat: Modify inventory key to ObjectIdentifier

--- VendingMachine/VendingMachine/Inventory.swift
+++ VendingMachine/VendingMachine/Inventory.swift
@@ -11,84 +11,70 @@ import Foundation
 typealias InventoryInfo = ([(name: String, price: Int, value: Int)]) -> Void
 
 class Inventory {
-    private var inventory: [Beverage: Int] = [:]
-    private var beverages: [Beverage] {
+    private var inventory: [ObjectIdentifier: Item] = [:]
+    private var objectIDs: [ObjectIdentifier] {
         return Array(inventory.keys)
     }
     
-    func append(_ beverage: Beverage, count: Int = 0) {
-        inventory[beverage, default: 0] += count
+    func findItems(key objectID: ObjectIdentifier) -> Item {
+        guard let oldItem = inventory[objectID] else {
+            let newItem = Item()
+            inventory[objectID] = newItem
+            return newItem
+        }
+        return oldItem
     }
     
+    func append(_ beverage: Beverage, count: Int = 0) {
+        let items = findItems(key: beverage.objectID)
+        items.append(beverage: beverage)
+    }
+    
+    
     func purchase(_ beverage: Beverage) {
-        inventory[beverage]? -= 1
+        let items = findItems(key: beverage.objectID)
+        items.remove(beverage: beverage)
+        if items.isEmpty {
+            inventory[beverage.objectID] = nil
+        }
     }
     
     /// 모든 재고 리스트를 보여준다.
+    //    func showAllList(with show: InventoryInfo) {
+    //        let names = inventory.map { ($0.key.itemName, $0.key.itemPrice, $0.value) }
+    //        show(names)
+    //    }
+    //
+    
     func showAllList(with show: InventoryInfo) {
-        let names = inventory.map { ($0.key.itemName, $0.key.itemPrice, $0.value) }
-        show(names)
+        let bundle = inventory.values
+            .filter { !$0.isEmpty }
+            .map { $0.info }
+        show(bundle)
     }
     
-    /// 구매 가능한 재고 리스트를 보여준다.
-    func showPurchasableList(money: Int, with show: InventoryInfo) {
-        let purchasable = fetchPurchasableList(with: money)
-            .map { ($0.key.itemName, $0.key.itemPrice, $0.value) }
-        show(purchasable)
-    }
-    
-    /// 잔액으로 구매 가능한 재고를 리턴한다.
-    private func fetchPurchasableList(with balance: Int) -> [Beverage: Int] {
-        let purchasableList = inventory
-            .filter { $0.key.itemPrice <= balance }
-        return purchasableList
-    }
-    
-    /// 잔액으로 음료가 구매 가능한지 여부를 리턴한다.
-    func canPurchaseBeverage(_ beverage: Beverage, with balance: Int) -> Bool {
-        guard haveInStock(beverage) else {
-            return false
-        }
-        let purchasableList = fetchPurchasableList(with: balance)
-        return purchasableList.keys.contains(beverage)
+    func filter(by condition: Item.Condition) -> [Beverage] {
+        return inventory.values
+            .map { $0.filter(by: condition) }
+            .reduce(into: [Beverage]()) { $0.append(contentsOf: $1) }
     }
     
     /// 음료의 재고가 있는지 없는지 리턴한다.
     private func haveInStock(_ beverage: Beverage) -> Bool {
-        return inventory[beverage] != nil && inventory[beverage]! > 0
+        let items = findItems(key: beverage.objectID)
+        return items.count > 0
     }
     
-    subscript(index: Int) -> Beverage? {
-        get {
-            guard index > -1 && index < beverages.endIndex else {
-                return nil
-            }
-            return beverages[index]
+    func fetchBeverage(at index: Int) -> Beverage? {
+        guard index > -1 && index < objectIDs.endIndex else {
+            return nil
         }
-    }
-}
-
-// MARK: - Filter by conditions
-extension Inventory {
-    /// 재고를 조건에 따라 필터링한 결과를 리턴한다.
-    func filter(by condition: Condition) -> [Beverage] {
-        return beverages.filter { condition.filteringCommand($0) }
-    }
-    
-    enum Condition {
-        case all
-        case hot
-        case expired
+        let id = objectIDs[index]
+        let item = findItems(key: id)
         
-        var filteringCommand: (Beverage) -> Bool {
-            switch self {
-            case .all:
-                return { _ in true }
-            case .hot:
-                return { $0.isHot }
-            case .expired:
-                return { $0.isExpired }
-            }
+        guard let beverage = item.fetchFirst() else {
+            return nil
         }
+        return beverage
     }
 }
