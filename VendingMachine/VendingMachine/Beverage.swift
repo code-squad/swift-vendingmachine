0701d4642ad362b72215581644c4edcf6e49954e
Kroisttt <yago0315@gmail.com>
2019-10-21 17:08:03 +0900

    Beverage 추가

---
+++ VendingMachine/VendingMachine/Beverage.swift
@@ -0,0 +1,25 @@
+//
+//  Beverage.swift
+//  VendingMachine
+//
+//  Created by 이진영 on 2019/10/21.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Beverage {
+    private let brand: String
+    private let capacity: Int
+    private let price: Int
+    private let name: String
+    private let date: Date
+    
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date) {
+        self.brand = brand
+        self.capacity = capacity
+        self.price = price
+        self.name = name
+        self.date = date
+    }
+}
