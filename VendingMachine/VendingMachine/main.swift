f2ae7f0cfada462f84066570c56dcf9a94da5163
Kroisttt <yago0315@gmail.com>
2019-10-21 18:24:06 +0900

    오류 수정

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 let milk = Milk(brand: "서울우유", capacity: 200, price: 1000, name: "날마다 딸기우유", date: Date("20171009"), isLowFat: false)
-let soda = Soda(brand: "펩시", capacity: 350, price: 2000, name: "다이어트 콜라", date: Date("20171005"), isSugarFree: false)
+let soda = Soda(brand: "펩시", capacity: 350, price: 2000, name: "다이어트 콜라", date: Date("20171005"), isSugarFree: true)
 let coffee = Coffee(brand: "맥심", capacity: 400, price: 3000, name: "TOP 아메리카노", date: Date("20171010"), coffeeType: .americano)
 
 print(milk)
