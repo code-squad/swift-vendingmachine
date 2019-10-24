117e25ef0d8f15f84f8775e85c4408b17ffe56bf
Kroisttt <yago0315@gmail.com>
2019-10-24 15:10:45 +0900

    StrawberryMilk 추가

---
+++ VendingMachine/VendingMachine/Class/Milk/StrawberryMilk.swift
@@ -0,0 +1,19 @@
+//
+//  StrawberryMilk.swift
+//  VendingMachine
+//
+//  Created by 이진영 on 2019/10/24.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class StrawberryMilk: Milk {
+    private let strawberryContent: Int
+    
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date, fatContent: Int, strawberryContent: Int) {
+        self.strawberryContent = strawberryContent
+        
+        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date, fatContent: fatContent)
+    }
+}
