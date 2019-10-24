1fbfce446b872f319c0c1e091e60be44ad21b2e2
Kroisttt <yago0315@gmail.com>
2019-10-24 15:13:14 +0900

    ChocolateMilk 추가

---
+++ VendingMachine/VendingMachine/Class/Milk/ChocolateMilk.swift
@@ -0,0 +1,19 @@
+//
+//  ChocolateMilk.swift
+//  VendingMachine
+//
+//  Created by 이진영 on 2019/10/24.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class ChocolateMilk: Milk {
+    private let chocolateContent: Int
+    
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date, fatContent: Int, chocolateContent: Int) {
+        self.chocolateContent = chocolateContent
+        
+        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date, fatContent: fatContent)
+    }
+}
