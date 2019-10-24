f6866f40ba8344d6ca2460e64c5286e06df5de0a
Kroisttt <yago0315@gmail.com>
2019-10-24 18:29:39 +0900

    CoffeeTest 추가

---
+++ VendingMachine/VendingMachineUnitTest/CoffeeTest.swift
@@ -0,0 +1,29 @@
+//
+//  CoffeeTest.swift
+//  VendingMachineUnitTest
+//
+//  Created by 이진영 on 2019/10/24.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import XCTest
+
+class CoffeeTest: XCTestCase {
+    func testAmericano_isNotHot() {
+        let americano = Americano(temperature: 10)
+        
+        XCTAssertFalse(americano.isHot)
+    }
+    
+    func testCafeLatte_isNotHot() {
+        let cafeLatte = CafeLatte()
+        
+        XCTAssertFalse(cafeLatte.isHot)
+    }
+    
+    func testAmericano_isHot() {
+        let americano = Americano(temperature: 70)
+        
+        XCTAssertTrue(americano.isHot)
+    }
+}
