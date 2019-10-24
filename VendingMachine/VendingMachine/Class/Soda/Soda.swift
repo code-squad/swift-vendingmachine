75c3515e4758d0a1f260ab05d60060c5d23aefe4
Kroisttt <yago0315@gmail.com>
2019-10-24 15:26:48 +0900

    Soda 클래스 isSugarFree 연산 프로퍼티로 수정

--- VendingMachine/VendingMachine/Class/Soda/Soda.swift
+++ VendingMachine/VendingMachine/Class/Soda/Soda.swift
@@ -9,11 +9,15 @@
 import Foundation
 
 class Soda: Beverage {
-    private let isSugarFree: Bool
+    private let sugarContent: Int
     
-    init(brand: String, capacity: Int, price: Int, name: String, date: Date, isSugarFree: Bool = false) {
-        self.isSugarFree = isSugarFree
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date, sugarContent: Int) {
+        self.sugarContent = sugarContent
         
         super.init(brand: brand, capacity: capacity, price: price, name: name, date: date)
+    }
+    
+    var isSugarFree: Bool {
+        return sugarContent == 0
     }
 }
