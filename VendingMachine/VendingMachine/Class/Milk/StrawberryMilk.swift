847cea47afe625aa08a1d7384d5f03d9f95b6bf3
Kroisttt <yago0315@gmail.com>
2019-10-24 17:07:41 +0900

    StrawberryMilk 기본 생성 값 추가

--- VendingMachine/VendingMachine/Class/Milk/StrawberryMilk.swift
+++ VendingMachine/VendingMachine/Class/Milk/StrawberryMilk.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 class StrawberryMilk: Milk {
-    private let strawberryContent: Int
+    private let strawberryContent: Double
     
-    init(brand: String, capacity: Int, price: Int, name: String, date: Date, fatContent: Double, strawberryContent: Int) {
+    init(brand: String = "서울우유", capacity: Int = 200, price: Int = 1000, name: String = "딸기우유", date: Date = Date("20191023"), fatContent: Double = 3, strawberryContent: Double = 1) {
         self.strawberryContent = strawberryContent
         
         super.init(brand: brand, capacity: capacity, price: price, name: name, date: date, fatContent: fatContent)
