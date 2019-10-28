d0c65a27f93f25539f5898d1c64fa43171922ab7
Kroisttt <yago0315@gmail.com>
2019-10-28 15:22:06 +0900

    Inventory 추가

---
+++ VendingMachine/VendingMachine/Inventory.swift
@@ -0,0 +1,17 @@
+//
+//  Inventory.swift
+//  VendingMachine
+//
+//  Created by 이진영 on 2019/10/28.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Storable {
+    
+}
+
+struct Inventory: Storable {
+    
+}
