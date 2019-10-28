90083425c8ba43354936293f1ba4907e3baf1f23
Kroisttt <yago0315@gmail.com>
2019-10-28 15:51:42 +0900

    Beverage temperature 프로퍼티 추가

--- VendingMachine/VendingMachine/Beverage/Soda/Coke.swift
+++ VendingMachine/VendingMachine/Beverage/Soda/Coke.swift
@@ -11,10 +11,10 @@ import Foundation
 class Coke: Soda {
     private let caffeineContent: Double
     
-    init(brand: String = "코카콜라", capacity: Int = 210, price: Int = 1100, name: String = "코카콜라", expirationDate: Date = Date(), sugarContent: Double = 23, caffeineContent: Double = 16) {
+    init(brand: String = "코카콜라", capacity: Int = 210, price: Int = 1100, name: String = "코카콜라", expirationDate: Date = Date(), temperature: Int = 10, sugarContent: Double = 23, caffeineContent: Double = 16) {
         self.caffeineContent = caffeineContent
         
-        super.init(brand: brand, capacity: capacity, price: price, name: name, expirationDate: expirationDate, sugarContent: sugarContent)
+        super.init(brand: brand, capacity: capacity, price: price, name: name, expirationDate: expirationDate, temperature: temperature, sugarContent: sugarContent)
     }
 }
 
