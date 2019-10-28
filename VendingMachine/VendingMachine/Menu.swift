002dae64e535e45c0a4e011886d037bec67ce763
Kroisttt <yago0315@gmail.com>
2019-10-28 16:56:26 +0900

    Menu 추가

---
+++ VendingMachine/VendingMachine/Menu.swift
@@ -0,0 +1,23 @@
+//
+//  Menu.swift
+//  VendingMachine
+//
+//  Created by 이진영 on 2019/10/28.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum Menu: Int, CustomStringConvertible, CaseIterable {
+    case insertMoney = 1
+    case purchaseBeverage
+    
+    var description: String {
+        switch self {
+        case .insertMoney:
+            return "\(rawValue). 금액 추가"
+        case .purchaseBeverage:
+            return "\(rawValue). 음료 구매"
+        }
+    }
+}
