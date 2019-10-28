d58b8d9b55408610b14155969bcdd4d430a72b52
Kroisttt <yago0315@gmail.com>
2019-10-28 13:11:01 +0900

    VendingMachine 추가

---
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -0,0 +1,13 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by 이진영 on 2019/10/28.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class VendingMachine {
+    
+}
