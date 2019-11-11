0ec84fcc6625bf8ffdc209f24403f2e2735a5063
Kroisttt <yago0315@gmail.com>
2019-11-11 17:35:48 +0900

    VendingMachineTest 추가
    의도하지 않았던 Coke와 LemonlimeDrink 유통기한 부분 수정

--- VendingMachine/VendingMachine/Beverage/Soda/Coke.swift
+++ VendingMachine/VendingMachine/Beverage/Soda/Coke.swift
@@ -11,7 +11,7 @@ import Foundation
 class Coke: Soda {
     private let caffeineContent: Double
     
-    init(brand: String = "코카콜라", capacity: Int = 210, price: Int = 1100, name: String = "코카콜라", expirationDate: Date = Date(), temperature: Int = 10, sugarContent: Double = 23, caffeineContent: Double = 16) {
+    init(brand: String = "코카콜라", capacity: Int = 210, price: Int = 1100, name: String = "코카콜라", expirationDate: Date = Date("20200101"), temperature: Int = 10, sugarContent: Double = 23, caffeineContent: Double = 16) {
         self.caffeineContent = caffeineContent
         
         super.init(brand: brand, capacity: capacity, price: price, name: name, expirationDate: expirationDate, temperature: temperature, sugarContent: sugarContent)
