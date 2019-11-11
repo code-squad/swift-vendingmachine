8d69038f6d613973f2d8faa7f803b90189f438cc
Kroisttt <yago0315@gmail.com>
2019-11-11 12:48:33 +0900

    VendingMachine 추가

--- VendingMachine/VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine/VendingMachine.swift
@@ -9,5 +9,73 @@
 import Foundation
 
 class VendingMachine {
+    private var balance = 0
+    private var history: [Sellable] = []
+    private var inventory: Storable
     
+    init(inventory: Storable) {
+        self.inventory = inventory
+    }
+    
+    func insert(money: Int) -> Bool {
+        if money > 0 {
+            balance += money
+            
+            return true
+        }
+        
+        return false
+    }
+    
+    func inquireBalance() -> Int {
+        return balance
+    }
+    
+    func purchaseProduct(index: Int) -> Sellable? {
+        let purchasableProduct = fetchPurchasableProducts()
+        guard let beverage = inventory.takeProduct(at: index) else {
+            return nil
+        }
+        
+        if purchasableProduct.contains(where: { $0.objectID == beverage.objectID }) {
+            balance -= beverage.productPrice
+            history.append(beverage)
+            
+            return beverage
+        }
+        
+        return nil
+    }
+    
+    func addStock(_ product: Sellable) {
+        inventory.addStock(product)
+    }
+    
+    func fetchHistory() -> [Sellable] {
+        return history
+    }
+    
+    func fetchPurchasableProducts() -> [Sellable] {
+        return inventory.search(option: .purchasable, balance: balance)
+    }
+    
+    func fetchHotProducts() -> [Sellable] {
+        return inventory.search(option: .hot)
+    }
+    
+    func fetchExpiredProducts() -> [Sellable] {
+        return inventory.search(option: .expired)
+    }
+    
+    func fetchAllProducts() -> [Sellable] {
+        return inventory.search(option: .all)
+    }
+    
+    func showBalance(form: (Int) -> ()) {
+        form(balance)
+    }
+    
+    func showInventory(form: ([(product: Sellable, count: Int)]) -> ()) {
+        inventory.showInventory(form: form)
+    }
 }
