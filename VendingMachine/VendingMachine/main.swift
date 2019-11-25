aeb3e934d66d1ce03c30442af53adbe961470d26
Kroisttt <yago0315@gmail.com>
2019-11-25 19:03:44 +0900

    main() 수정
    ManagerMode와 UserMode에 코드 작성

--- VendingMachine/VendingMachine/main.swift
+++ VendingMachine/VendingMachine/main.swift
@@ -20,62 +20,13 @@ func main() {
         return
     }
     
-    vendingMachineLoop: while true {
-        switch mode {
-        case .managerMode:
-            let managerMode = vendingMachine as VendingMachineManagerable & Printable
-            
-            managerMode.showInventory(form: OutputView.showInventory)
-            
-            let menuInput = InputView.readManagerMenu()
-            guard let menu = ManagerMenu.init(rawValue: menuInput) else {
-                OutputView.show(failMessage: .invalidInputMessage)
-                
-                continue vendingMachineLoop
-            }
-            
-            let input = InputView.readInput()
-            let beverage = managerMode.fetchProduct(at: input)
-            
-            switch menu {
-            case .addStock:
-                managerMode.addStock(beverage)
-            case .removeStock:
-                managerMode.removeStock(beverage)
-            }
-            
-        case .userMode:
-            let userMode = vendingMachine as VendingMachineUserable & Printable
-            
-            userMode.showBalance(form: OutputView.showBalance)
-            userMode.showInventory(form: OutputView.showInventory)
-            
-            let menuInput = InputView.readUserMenu()
-            guard let menu = UserMenu.init(rawValue: menuInput) else {
-                OutputView.show(failMessage: .invalidInputMessage)
-                
-                continue vendingMachineLoop
-            }
-            
-            let input = InputView.readInput()
-            
-            switch menu {
-            case .insertMoney:
-                if !userMode.insert(money: input) {
-                    OutputView.show(failMessage: .invalidInputMessage)
-                    
-                    continue vendingMachineLoop
-                }
-            case .purchaseBeverage:
-                guard let beverage = userMode.purchaseProduct(index: input) else {
-                    OutputView.show(failMessage: .unableToPurchaseMessage)
-                    
-                    continue vendingMachineLoop
-                }
-                
-                OutputView.showPurchase(beverage)
-            }
-        }
+    switch mode {
+    case .managerMode:
+        let managerMode = ManagerMode(vendingMachine: vendingMachine)
+        managerMode.run()
+    case .userMode:
+        let userMode = UserMode(vendingMachine: vendingMachine)
+        userMode.run()
     }
 }
 
